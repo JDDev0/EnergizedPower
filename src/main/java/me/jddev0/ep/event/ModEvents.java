9524fcb1bb9ce15efd052a995c5856fa13df16c7
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-25 19:36:41 +0100

    Update offers for the electrician villager profession

--- src/main/java/me/jddev0/ep/event/ModEvents.java
+++ src/main/java/me/jddev0/ep/event/ModEvents.java
@@ -2,6 +2,7 @@ package me.jddev0.ep.event;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
 import me.jddev0.ep.item.ModItems;
 import me.jddev0.ep.villager.ModVillager;
 import net.minecraft.world.entity.npc.VillagerTrades;
@@ -21,26 +22,71 @@ public class ModEvents {
         if(event.getType() == ModVillager.ELECTRICIAN_PROFESSION.get()) {
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
 
+            //Level 1
             addOffer(trades, 1,
-                    new ItemStack(Items.EMERALD, 12),
-                    new ItemStack(ModItems.ENERGIZED_COPPER_INGOT.get(), 1),
-                    10, 20, .02f);
+                    new ItemStack(Items.COPPER_INGOT, 2),
+                    new ItemStack(Items.EMERALD, 1),
+                    25, 1, .02f);
             addOffer(trades, 1,
                     new ItemStack(ModItems.SILICON.get(), 9),
-                    new ItemStack(Items.EMERALD, 5),
-                    15, 10, .02f);
+                    new ItemStack(Items.EMERALD, 4),
+                    15, 2, .02f);
 
+            //Level 2
             addOffer(trades, 2,
                     new ItemStack(ModItems.BASIC_SOLAR_CELL.get(), 2),
                     new ItemStack(Items.EMERALD, 3),
-                    5, 35, .02f);
+                    5, 5, .02f);
+            addOffer(trades, 2,
+                    new ItemStack(Items.EMERALD, 9),
+                    new ItemStack(ModItems.ENERGIZED_COPPER_INGOT.get(), 1),
+                    10, 8, .02f);
 
-            //TODO add trades (for levels 2 - 5)
+            //Level 3
+            addOffer(trades, 3,
+                    new ItemStack(Items.EMERALD, 6),
+                    new ItemStack(ModBlocks.BASIC_MACHINE_FRAME_ITEM.get(), 1),
+                    new ItemStack(ModBlocks.AUTO_CRAFTER_ITEM.get(), 1),
+                    5, 10, .02f);
+            addOffer(trades, 3,
+                    new ItemStack(Items.EMERALD, 8),
+                    new ItemStack(ModBlocks.BASIC_MACHINE_FRAME_ITEM.get(), 1),
+                    new ItemStack(ModBlocks.COAL_ENGINE_ITEM.get(), 1),
+                    5, 14, .02f);
+
+            //Level 4
+            addOffer(trades, 4,
+                    new ItemStack(Items.EMERALD, 8),
+                    new ItemStack(ModItems.BASIC_SOLAR_CELL.get(), 2),
+                    new ItemStack(ModBlocks.SOLAR_PANEL_ITEM_1.get(), 1),
+                    5, 19, .02f);
+            addOffer(trades, 4,
+                    new ItemStack(Items.EMERALD, 37),
+                    new ItemStack(ModBlocks.ADVANCED_MACHINE_FRAME_ITEM.get(), 1),
+                    new ItemStack(ModBlocks.ENERGIZER_ITEM.get(), 1),
+                    2, 26, .02f);
+
+            //Level 5
+            addOffer(trades, 5,
+                    new ItemStack(Items.EMERALD, 49),
+                    new ItemStack(ModBlocks.ADVANCED_MACHINE_FRAME_ITEM.get(), 1),
+                    new ItemStack(ModBlocks.LIGHTNING_GENERATOR_ITEM.get(), 1),
+                    2, 30, .02f);
+            addOffer(trades, 5,
+                    new ItemStack(Items.EMERALD, 14),
+                    new ItemStack(ModBlocks.SOLAR_PANEL_ITEM_1.get(), 2),
+                    new ItemStack(ModBlocks.SOLAR_PANEL_ITEM_2.get(), 1),
+                    5, 41, .02f);
         }
     }
 
     private static void addOffer(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades, int minLevel,
                                  ItemStack cost, ItemStack result, int maxUses, int xp, float priceMultiplier) {
         trades.get(minLevel).add((trader, rand) -> new MerchantOffer(cost, result, maxUses, xp, priceMultiplier));
+    }
+
+    private static void addOffer(Int2ObjectMap<List<VillagerTrades.ItemListing>> trades, int minLevel,
+                                 ItemStack costA, ItemStack costB, ItemStack result, int maxUses, int xp, float priceMultiplier) {
+        trades.get(minLevel).add((trader, rand) -> new MerchantOffer(costA, costB, result, maxUses, xp, priceMultiplier));
     }
 }
