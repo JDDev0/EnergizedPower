f8a37a3c948d088956baf700825c4d36285d7c74
JDDev0 <37939362+JDDev0@users.noreply.github.com>
2022-12-27 21:55:48 +0100

    Implement charging station

--- src/main/java/me/jddev0/ep/block/entity/ChargingStationBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/ChargingStationBlockEntity.java
@@ -1,21 +1,30 @@
 package me.jddev0.ep.block.entity;
 
+import me.jddev0.ep.block.ChargingStationBlock;
 import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
 import me.jddev0.ep.energy.ReceiveOnlyEnergyStorage;
 import me.jddev0.ep.networking.ModMessages;
 import me.jddev0.ep.networking.packet.EnergySyncS2CPacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.IEnergyStorage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class ChargingStationBlockEntity extends BlockEntity implements EnergyStoragePacketUpdate {
     private final ReceiveOnlyEnergyStorage energyStorage;
@@ -76,7 +85,45 @@ public class ChargingStationBlockEntity extends BlockEntity implements EnergySto
         if(level.isClientSide)
             return;
 
-        //TODO
+        List<Player> players = level.getEntitiesOfClass(Player.class, AABB.of(BoundingBox.fromCorners(
+                new Vec3i(blockPos.getX() - 1, blockPos.getY() - 1, blockPos.getZ() - 1),
+                new Vec3i(blockPos.getX() + 1, blockPos.getY() + 1, blockPos.getZ() + 1))));
+
+        int energyPerTick = Math.min(blockEntity.energyStorage.getMaxReceive(), blockEntity.energyStorage.getEnergy());
+        int energyPerTickLeft = energyPerTick;
+
+        outer:
+        for(Player player:players) {
+            if(player.isDeadOrDying())
+                continue;
+
+            Inventory inventory = player.getInventory();
+            for(int i = 0;i < inventory.getContainerSize();i++) {
+                ItemStack itemStack = inventory.getItem(i);
+
+                LazyOptional<IEnergyStorage> energyStorageLazyOptional = itemStack.getCapability(ForgeCapabilities.ENERGY);
+                if(!energyStorageLazyOptional.isPresent())
+                    continue;
+
+                IEnergyStorage energyStorage = energyStorageLazyOptional.orElse(null);
+                if(!energyStorage.canReceive())
+                    continue;
+
+                energyPerTickLeft -= energyStorage.receiveEnergy(energyPerTickLeft, false);
+                if(energyPerTickLeft == 0)
+                    break outer;
+            }
+        }
+
+        if(energyPerTickLeft == energyPerTick) {
+            if(!level.getBlockState(blockPos).hasProperty(ChargingStationBlock.CHARGING) || level.getBlockState(blockPos).getValue(ChargingStationBlock.CHARGING))
+                level.setBlock(blockPos, state.setValue(ChargingStationBlock.CHARGING, Boolean.FALSE), 3);
+        }else {
+            if(!level.getBlockState(blockPos).hasProperty(ChargingStationBlock.CHARGING) || !level.getBlockState(blockPos).getValue(ChargingStationBlock.CHARGING))
+                level.setBlock(blockPos, state.setValue(ChargingStationBlock.CHARGING, Boolean.TRUE), 3);
+
+            blockEntity.energyStorage.setEnergy(blockEntity.energyStorage.getEnergy() - energyPerTick + energyPerTickLeft);
+        }
     }
 
     @Override
