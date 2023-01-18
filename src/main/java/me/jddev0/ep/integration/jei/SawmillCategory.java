a69ff59849897ba8032b72b7c2b45aa72c4fd434
JDDev0 <jddev0@dorneger.at>
2023-01-18 22:25:00 +0100

    Implement JEI integration

---
+++ src/main/java/me/jddev0/ep/integration/jei/SawmillCategory.java
@@ -0,0 +1,62 @@
+package me.jddev0.ep.integration.jei;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.item.ModItems;
+import me.jddev0.ep.recipe.SawmillRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public class SawmillCategory implements IRecipeCategory<SawmillRecipe> {
+    public static final ResourceLocation UID = new ResourceLocation(EnergizedPowerMod.MODID, "sawmill");
+    public static final RecipeType<SawmillRecipe> TYPE = new RecipeType<>(UID, SawmillRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public SawmillCategory(IGuiHelper helper) {
+        ResourceLocation texture = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/sawmill.png");
+        background = helper.createDrawable(texture, 42, 30, 109, 26);
+
+        icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.CRUSHER_ITEM.get()));
+    }
+
+    @Override
+    public RecipeType<SawmillRecipe> getRecipeType() {
+        return TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("container.energizedpower.sawmill");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder iRecipeLayout, SawmillRecipe recipe, IFocusGroup iFocusGroup) {
+        iRecipeLayout.addSlot(RecipeIngredientRole.INPUT, 1, 5).addIngredients(recipe.getInput());
+
+        iRecipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 65, 5).addItemStack(recipe.getOutput());
+
+        if(recipe.getSawdustAmount() > 0)
+            iRecipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 92, 5).addItemStack(new ItemStack(ModItems.SAWDUST.get(), recipe.getSawdustAmount()));
+    }
+}
