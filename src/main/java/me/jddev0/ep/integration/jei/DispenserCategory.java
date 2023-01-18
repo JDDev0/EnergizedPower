51463b326dade5da3f00acc9ededd5b0fc267f7f
JDDev0 <jddev0@dorneger.at>
2023-01-18 21:36:54 +0100

    Implement JEI integration

---
+++ src/main/java/me/jddev0/ep/integration/jei/DispenserCategory.java
@@ -0,0 +1,73 @@
+package me.jddev0.ep.integration.jei;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public class DispenserCategory implements IRecipeCategory<DispenserCategory.DispenserRecipe> {
+    public static final ResourceLocation UID = new ResourceLocation(EnergizedPowerMod.MODID, "dispenser");
+    public static final RecipeType<DispenserRecipe> TYPE = new RecipeType<>(UID, DispenserRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public DispenserCategory(IGuiHelper helper) {
+        background = helper.createBlankDrawable(100, 25);
+
+        icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Items.DISPENSER));
+    }
+
+    @Override
+    public RecipeType<DispenserCategory.DispenserRecipe> getRecipeType() {
+        return TYPE;
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
+
+    @Override
+    public Class<? extends DispenserRecipe> getRecipeClass() {
+        return DispenserRecipe.class;
+    }
+
+    @Override
+    public Component getTitle() {
+        return new TranslatableComponent("container.dispenser");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder iRecipeLayoutBuilder, DispenserRecipe recipe, IFocusGroup iFocusGroup) {
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 0, 5).addIngredients(recipe.tool());
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 19, 5).addIngredients(recipe.block());
+
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 81, 5).addItemStack(recipe.output());
+    }
+
+    //TODO draw slots
+
+    record DispenserRecipe(Ingredient tool, Ingredient block, ItemStack output) {}
+}
---
+++ src/main/java/me/jddev0/ep/integration/jei/InWorldCategory.java
@@ -0,0 +1,73 @@
+package me.jddev0.ep.integration.jei;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public class InWorldCategory implements IRecipeCategory<InWorldCategory.InWorldRecipe> {
+    public static final ResourceLocation UID = new ResourceLocation(EnergizedPowerMod.MODID, "in_world");
+    public static final RecipeType<InWorldRecipe> TYPE = new RecipeType<>(UID, InWorldRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public InWorldCategory(IGuiHelper helper) {
+        background = helper.createBlankDrawable(100, 25);
+
+        icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Items.GRASS_BLOCK));
+    }
+
+    @Override
+    public RecipeType<InWorldRecipe> getRecipeType() {
+        return TYPE;
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
+
+    @Override
+    public Class<? extends InWorldRecipe> getRecipeClass() {
+        return InWorldRecipe.class;
+    }
+
+    @Override
+    public Component getTitle() {
+        return new TranslatableComponent("recipes.energizedpower.in_world_crafting");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder iRecipeLayoutBuilder, InWorldRecipe recipe, IFocusGroup iFocusGroup) {
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 0, 5).addIngredients(recipe.tool());
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.INPUT, 19, 5).addIngredients(recipe.block());
+
+        iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT, 81, 5).addItemStack(recipe.output());
+    }
+
+    //TODO draw slots
+
+    record InWorldRecipe(Ingredient tool, Ingredient block, ItemStack output) {}
+}
