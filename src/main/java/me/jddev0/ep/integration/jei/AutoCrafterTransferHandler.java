5d3af6bfca0068473f7e16ae7bd33c6fadd9b1a2
JDDev0 <jddev0@dorneger.at>
2023-02-16 23:02:48 +0100

    Add JEI transfer handler for the Auto Crafter

---
+++ src/main/java/me/jddev0/ep/integration/jei/AutoCrafterTransferHandler.java
@@ -0,0 +1,65 @@
+package me.jddev0.ep.integration.jei;
+
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.SetAutoCrafterPatternInputSlotsC2SPacket;
+import me.jddev0.ep.screen.AutoCrafterMenu;
+import me.jddev0.ep.screen.ModMenuTypes;
+import mezz.jei.api.gui.ingredient.IRecipeSlotView;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AutoCrafterTransferHandler implements IRecipeTransferHandler<AutoCrafterMenu, CraftingRecipe> {
+    private final IRecipeTransferHandlerHelper helper;
+
+    public AutoCrafterTransferHandler(IRecipeTransferHandlerHelper helper) {
+        this.helper = helper;
+    }
+
+    @Override
+    public Class<? extends AutoCrafterMenu> getContainerClass() {
+        return AutoCrafterMenu.class;
+    }
+
+    @Override
+    public Optional<MenuType<AutoCrafterMenu>> getMenuType() {
+        return Optional.of(ModMenuTypes.AUTO_CRAFTER_MENU.get());
+    }
+
+    @Override
+    public RecipeType<CraftingRecipe> getRecipeType() {
+        return null;
+    }
+
+    @Override
+    public @Nullable IRecipeTransferError transferRecipe(AutoCrafterMenu container, CraftingRecipe recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
+        if(!recipe.canCraftInDimensions(3, 3))
+            return null;
+
+        if(!doTransfer)
+            return null;
+
+        List<ItemStack> itemStacks = new ArrayList<>(9);
+
+        List<IRecipeSlotView> inputSlots = recipeSlots.getSlotViews(RecipeIngredientRole.INPUT);
+        int len = Math.min(inputSlots.size(), 9);
+        for(int i = 0;i < len;i++)
+            itemStacks.add(inputSlots.get(i).getDisplayedItemStack().orElse(ItemStack.EMPTY).copy());
+
+        ModMessages.sendToServer(new SetAutoCrafterPatternInputSlotsC2SPacket(container.getBlockEntity().getBlockPos(), itemStacks));
+
+        return null;
+    }
+}
--- src/main/java/me/jddev0/ep/integration/jei/EnergizedPowerJEIPlugin.java
+++ src/main/java/me/jddev0/ep/integration/jei/EnergizedPowerJEIPlugin.java
@@ -7,12 +7,10 @@ import me.jddev0.ep.recipe.ChargerRecipe;
 import me.jddev0.ep.recipe.CrusherRecipe;
 import me.jddev0.ep.recipe.EnergizerRecipe;
 import me.jddev0.ep.recipe.SawmillRecipe;
-import me.jddev0.ep.screen.ChargerScreen;
-import me.jddev0.ep.screen.CrusherScreen;
-import me.jddev0.ep.screen.EnergizerScreen;
-import me.jddev0.ep.screen.SawmillScreen;
+import me.jddev0.ep.screen.*;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.registration.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.DispenserScreen;
@@ -46,6 +44,11 @@ public class EnergizedPowerJEIPlugin implements IModPlugin {
         );
     }
 
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+        registration.addRecipeTransferHandler(new AutoCrafterTransferHandler(registration.getTransferHelper()),
+                RecipeTypes.CRAFTING);
+    }
+
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
@@ -68,6 +71,8 @@ public class EnergizedPowerJEIPlugin implements IModPlugin {
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.AUTO_CRAFTER_ITEM.get()), RecipeTypes.CRAFTING);
+
         registration.addRecipeCatalyst(new ItemStack(ModBlocks.CHARGER_ITEM.get()), ChargerCategory.TYPE);
         registration.addRecipeCatalyst(new ItemStack(ModBlocks.CRUSHER_ITEM.get()), CrusherCategory.TYPE);
         registration.addRecipeCatalyst(new ItemStack(ModBlocks.SAWMILL_ITEM.get()), SawmillCategory.TYPE);
@@ -79,6 +84,8 @@ public class EnergizedPowerJEIPlugin implements IModPlugin {
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(AutoCrafterScreen.class, 89, 34, 24, 17, RecipeTypes.CRAFTING);
+
         registration.addRecipeClickArea(ChargerScreen.class, 25, 16, 40, 54, ChargerCategory.TYPE);
         registration.addRecipeClickArea(ChargerScreen.class, 111, 16, 58, 54, ChargerCategory.TYPE);
 
