0e5f1c0060299b5e500d41e8c066cb7cca190b5f
JDDev0 <jddev0@dorneger.at>
2023-02-17 15:46:02 +0100

    Add json field to book pages: "remove"
    (cherry picked from commit 13f39105bb0c03563f444fdac20727041cd0efbe)

--- src/main/java/me/jddev0/ep/loading/EnergizedPowerBookReloadListener.java
+++ src/main/java/me/jddev0/ep/loading/EnergizedPowerBookReloadListener.java
@@ -3,6 +3,7 @@ package me.jddev0.ep.loading;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mojang.datafixers.util.Pair;
 import com.mojang.logging.LogUtils;
 import me.jddev0.ep.screen.EnergizedPowerBookScreen;
 import net.minecraft.network.chat.Component;
@@ -14,10 +15,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.slf4j.Logger;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @OnlyIn(Dist.CLIENT)
@@ -30,7 +28,7 @@ public class EnergizedPowerBookReloadListener extends SimpleJsonResourceReloadLi
 
     @Override
     protected void apply(Map<ResourceLocation, JsonElement> elements, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
-        List<EnergizedPowerBookScreen.PageContent> pages = new LinkedList<>();
+        List<Pair<ResourceLocation, EnergizedPowerBookScreen.PageContent>> pages = new LinkedList<>();
 
         List<Map.Entry<ResourceLocation, JsonElement>> elementEntries = elements.entrySet().stream().
                 sorted(Comparator.comparing(o -> o.getKey().getPath())).
@@ -49,6 +47,40 @@ public class EnergizedPowerBookReloadListener extends SimpleJsonResourceReloadLi
                 }
 
                 JsonObject object = element.getAsJsonObject();
+
+                //Remove page command (Will not be displayed)
+                if(object.has("remove")) {
+                    JsonElement pageToRemoveElement = object.get("remove");
+
+                    if(!pageToRemoveElement.isJsonPrimitive() || !pageToRemoveElement.getAsJsonPrimitive().isString()) {
+                        LOGGER.error(String.format("Failed to load energized power book page '%s' from data pack '%s': remove must be a string primitive",
+                                resourceLocation.getPath(), resourceLocation.getNamespace()));
+
+                        continue;
+                    }
+                    ResourceLocation pageToRemove = ResourceLocation.tryParse(pageToRemoveElement.getAsJsonPrimitive().getAsString());
+                    if(pageToRemove == null)
+                        continue;
+
+                    boolean containsKeyFlag = false;
+                    for(int i = pages.size() - 1;i >= 0;i--) {
+                        if(pages.get(i).getFirst().equals(pageToRemove)) {
+                            containsKeyFlag = true;
+                            pages.remove(i);
+
+                            break;
+                        }
+                    }
+
+                    if(!containsKeyFlag) {
+                        LOGGER.error(String.format("Failed to load energized power book page '%s' from data pack '%s': page to be removed was not found",
+                                resourceLocation.getPath(), resourceLocation.getNamespace()));
+
+                        continue;
+                    }
+
+                    continue;
+                }
 
                 Component chapterTitleComponent = null;
                 if(object.has("title"))
@@ -84,13 +116,13 @@ public class EnergizedPowerBookReloadListener extends SimpleJsonResourceReloadLi
                     blockResourceLocation = ResourceLocation.tryParse(imageElement.getAsJsonPrimitive().getAsString());
                 }
 
-                pages.add(new EnergizedPowerBookScreen.PageContent(chapterTitleComponent, contentComponent, imageResourceLocation, blockResourceLocation));
+                pages.add(Pair.of(resourceLocation, new EnergizedPowerBookScreen.PageContent(chapterTitleComponent, contentComponent, imageResourceLocation, blockResourceLocation)));
             }catch(Exception e) {
                 LOGGER.error(String.format("Failed to load energized power book page '%s' from data pack '%s'",
                         resourceLocation.getPath(), resourceLocation.getNamespace()), e);
             }
         }
 
-        EnergizedPowerBookScreen.pages = pages;
+        EnergizedPowerBookScreen.pages = new ArrayList<>(pages.stream().map(Pair::getSecond).collect(Collectors.toList()));
     }
 }
