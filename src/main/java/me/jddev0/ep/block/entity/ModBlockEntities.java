d990da86b51c4e3d60b42f2c3bc26ec6f9e8b379
JDDev0 <jddev0@dorneger.at>
2023-02-18 00:40:06 +0100

    Add Transformer (3 to 3)

--- src/main/java/me/jddev0/ep/block/entity/ModBlockEntities.java
+++ src/main/java/me/jddev0/ep/block/entity/ModBlockEntities.java
@@ -73,6 +73,10 @@ public final class ModBlockEntities {
             BLOCK_ENTITIES.register("transformer_1_to_n", () -> BlockEntityType.Builder.of((blockPos, state) ->
                             new TransformerBlockEntity(blockPos, state, TransformerBlock.Type.TYPE_1_TO_N),
                     ModBlocks.TRANSFORMER_1_TO_N.get()).build(null));
+    public static final RegistryObject<BlockEntityType<TransformerBlockEntity>> TRANSFORMER_3_TO_3_ENTITY =
+            BLOCK_ENTITIES.register("transformer_3_to_3", () -> BlockEntityType.Builder.of((blockPos, state) ->
+                            new TransformerBlockEntity(blockPos, state, TransformerBlock.Type.TYPE_3_TO_3),
+                    ModBlocks.TRANSFORMER_3_TO_3.get()).build(null));
     public static final RegistryObject<BlockEntityType<TransformerBlockEntity>> TRANSFORMER_N_TO_1_ENTITY =
             BLOCK_ENTITIES.register("transformer_n_to_1", () -> BlockEntityType.Builder.of((blockPos, state) ->
                             new TransformerBlockEntity(blockPos, state, TransformerBlock.Type.TYPE_N_TO_1),
