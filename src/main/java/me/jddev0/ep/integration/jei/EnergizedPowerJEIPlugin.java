60a9df9a2f936dd43c6ce4e8ce208831a6148704
JDDev0 <jddev0@dorneger.at>
2023-02-20 18:37:46 +0100

    Fix missing Override annotaion in the "EnergizedPowerJEIPlugin" class
    (cherry picked from commit 687090560d90a59ce9e14b26e97d5b02ea355cc7)

--- src/main/java/me/jddev0/ep/integration/jei/EnergizedPowerJEIPlugin.java
+++ src/main/java/me/jddev0/ep/integration/jei/EnergizedPowerJEIPlugin.java
@@ -44,6 +44,7 @@ public class EnergizedPowerJEIPlugin implements IModPlugin {
         );
     }
 
+    @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
         registration.addRecipeTransferHandler(new AutoCrafterTransferHandler(registration.getTransferHelper()),
                 VanillaRecipeCategoryUid.CRAFTING);
