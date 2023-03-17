8ce91c161bb27ddc6a167f5ed96b35349e1112d1
JDDev0 <jddev0@dorneger.at>
2023-03-17 19:43:48 +0100

    Change lightning generator: Add pushing of energy into energy consumers

--- src/main/java/me/jddev0/ep/block/entity/LightningGeneratorBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/LightningGeneratorBlockEntity.java
@@ -10,11 +10,18 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import team.reborn.energy.api.EnergyStorage;
 import team.reborn.energy.api.base.LimitingEnergyStorage;
 import team.reborn.energy.api.base.SimpleEnergyStorage;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LightningGeneratorBlockEntity extends BlockEntity implements EnergyStoragePacketUpdate {
+    public static final long MAX_EXTRACT = 65536;
 
     final LimitingEnergyStorage energyStorage;
     private final SimpleEnergyStorage internalEnergyStorage;
@@ -38,7 +45,7 @@ public class LightningGeneratorBlockEntity extends BlockEntity implements Energy
                 }
             }
         };
-        energyStorage = new LimitingEnergyStorage(internalEnergyStorage, 0, 65536);
+        energyStorage = new LimitingEnergyStorage(internalEnergyStorage, 0, MAX_EXTRACT);
     }
 
     public void onLightningStrike() {
@@ -60,6 +67,88 @@ public class LightningGeneratorBlockEntity extends BlockEntity implements Energy
         super.readNbt(nbt);
 
         internalEnergyStorage.amount = nbt.getLong("energy");
+    }
+
+    public static void tick(World level, BlockPos blockPos, BlockState state, LightningGeneratorBlockEntity blockEntity) {
+        if(level.isClient())
+            return;
+
+        transferEnergy(level, blockPos, state, blockEntity);
+    }
+
+    private static void transferEnergy(World level, BlockPos blockPos, BlockState state, LightningGeneratorBlockEntity blockEntity) {
+        if(level.isClient())
+            return;
+
+        List<EnergyStorage> consumerItems = new LinkedList<>();
+        List<Long> consumerEnergyValues = new LinkedList<>();
+        int consumptionSum = 0;
+        for(Direction direction:Direction.values()) {
+            BlockPos testPos = blockPos.offset(direction);
+
+            BlockEntity testBlockEntity = level.getBlockEntity(testPos);
+            if(testBlockEntity == null)
+                continue;
+
+            EnergyStorage energyStorage = EnergyStorage.SIDED.find(level, testPos, direction.getOpposite());
+            if(energyStorage == null)
+                continue;
+
+            if(!energyStorage.supportsInsertion())
+                continue;
+
+            try(Transaction transaction = Transaction.openOuter()) {
+                long received = energyStorage.insert(Math.min(MAX_EXTRACT, blockEntity.internalEnergyStorage.amount), transaction);
+
+                if(received <= 0)
+                    continue;
+
+                consumptionSum += received;
+                consumerItems.add(energyStorage);
+                consumerEnergyValues.add(received);
+            }
+        }
+
+        List<Long> consumerEnergyDistributed = new LinkedList<>();
+        for(int i = 0;i < consumerItems.size();i++)
+            consumerEnergyDistributed.add(0L);
+
+        long consumptionLeft = Math.min(MAX_EXTRACT, Math.min(blockEntity.internalEnergyStorage.amount, consumptionSum));
+        try(Transaction transaction = Transaction.openOuter()) {
+            blockEntity.internalEnergyStorage.extract(consumptionLeft, transaction);
+            transaction.commit();
+        }
+
+        int divisor = consumerItems.size();
+        outer:
+        while(consumptionLeft > 0) {
+            long consumptionPerConsumer = consumptionLeft / divisor;
+            if(consumptionPerConsumer == 0) {
+                divisor = Math.max(1, divisor - 1);
+                consumptionPerConsumer = consumptionLeft / divisor;
+            }
+
+            for(int i = 0;i < consumerEnergyValues.size();i++) {
+                long consumptionDistributed = consumerEnergyDistributed.get(i);
+                long consumptionOfConsumerLeft = consumerEnergyValues.get(i) - consumptionDistributed;
+
+                long consumptionDistributedNew = Math.min(consumptionOfConsumerLeft, Math.min(consumptionPerConsumer, consumptionLeft));
+                consumerEnergyDistributed.set(i, consumptionDistributed + consumptionDistributedNew);
+                consumptionLeft -= consumptionDistributedNew;
+                if(consumptionLeft == 0)
+                    break outer;
+            }
+        }
+
+        for(int i = 0;i < consumerItems.size();i++) {
+            long energy = consumerEnergyDistributed.get(i);
+            if(energy > 0) {
+                try(Transaction transaction = Transaction.openOuter()) {
+                    consumerItems.get(i).insert(energy, transaction);
+                    transaction.commit();
+                }
+            }
+        }
     }
 
     @Override
