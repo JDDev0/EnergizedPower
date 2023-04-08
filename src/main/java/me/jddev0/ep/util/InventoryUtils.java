f5ed29e903225a823b81ed9f7a3d13454ae14a41
JDDev0 <jddev0@dorneger.at>
2023-04-08 22:28:55 +0200

    Add Redstone Comparator output to the Auto Crafter block
    (cherry picked from commit f2e9a281ecc095e690b361c7d669d814bb216538)

---
+++ src/main/java/me/jddev0/ep/util/InventoryUtils.java
@@ -0,0 +1,25 @@
+package me.jddev0.ep.util;
+
+import net.minecraft.util.Mth;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.ItemStackHandler;
+
+public final class InventoryUtils {
+    private InventoryUtils() {}
+
+    public static int getRedstoneSignalFromItemStackHandler(ItemStackHandler itemHandler) {
+        float fullnessPercentSum = 0;
+        boolean isEmptyFlag = true;
+
+        int size = itemHandler.getSlots();
+        for(int i = 0;i < size;i++) {
+            ItemStack item = itemHandler.getStackInSlot(i);
+            if(!item.isEmpty()) {
+                fullnessPercentSum += (float)item.getCount() / Math.min(item.getMaxStackSize(), itemHandler.getSlotLimit(0));
+                isEmptyFlag = false;
+            }
+        }
+
+        return Mth.floor(fullnessPercentSum / size * 14.f) + (isEmptyFlag?0:1);
+    }
+}
