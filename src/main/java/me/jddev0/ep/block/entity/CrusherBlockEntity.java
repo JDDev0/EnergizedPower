b08bd4838efcf1878b3fdcba7343c81c9537080e
JDDev0 <jddev0@dorneger.at>
2023-04-08 23:44:25 +0200

    Add Redstone Comparator output to the Crusher block

--- src/main/java/me/jddev0/ep/block/entity/CrusherBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/CrusherBlockEntity.java
@@ -167,6 +167,10 @@ public class CrusherBlockEntity extends BlockEntity implements ExtendedScreenHan
         return new TranslatableText("container.energizedpower.crusher");
     }
 
+    public int getRedstoneOutput() {
+        return ScreenHandler.calculateComparatorOutput(internalInventory);
+    }
+
     @Nullable
     @Override
     public ScreenHandler createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
