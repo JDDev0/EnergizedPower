42144fb29abc99c7b7f73dd783fb5dcd6ae80ff9
JDDev0 <jddev0@dorneger.at>
2023-04-11 18:17:18 +0200

    Add the Netherite Hammer item
    (cherry picked from commit 66758aa9873d44648a4397ec21dbb427647c7a69)

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -79,6 +79,8 @@ public final class ModItems {
             () -> new HammerItem(Tiers.IRON, new Item.Properties()));
     public static final RegistryObject<Item> DIAMOND_HAMMER = ITEMS.register("diamond_hammer",
             () -> new HammerItem(Tiers.DIAMOND, new Item.Properties()));
+    public static final RegistryObject<Item> NETHERITE_HAMMER = ITEMS.register("netherite_hammer",
+            () -> new HammerItem(Tiers.NETHERITE, new Item.Properties().fireResistant()));
 
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
