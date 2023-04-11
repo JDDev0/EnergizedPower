29a53dab6abff697701187b1ff2616552beb21d3
JDDev0 <jddev0@dorneger.at>
2023-04-11 17:29:07 +0200

    Add the Diamond Hammer item

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -77,6 +77,8 @@ public final class ModItems {
             () -> new HammerItem(Tiers.STONE, new Item.Properties()));
     public static final RegistryObject<Item> IRON_HAMMER = ITEMS.register("iron_hammer",
             () -> new HammerItem(Tiers.IRON, new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_HAMMER = ITEMS.register("diamond_hammer",
+            () -> new HammerItem(Tiers.DIAMOND, new Item.Properties()));
 
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
