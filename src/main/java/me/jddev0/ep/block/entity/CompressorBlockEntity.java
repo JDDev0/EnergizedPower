18f864851127cf1fa9aa72895c0b3ab9b57cf3c6
JDDev0 <jddev0@dorneger.at>
2023-04-13 06:38:19 +0200

    Add Compressor

---
+++ src/main/java/me/jddev0/ep/block/entity/CompressorBlockEntity.java
@@ -0,0 +1,302 @@
+package me.jddev0.ep.block.entity;
+
+import me.jddev0.ep.block.entity.handler.InputOutputItemHandler;
+import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
+import me.jddev0.ep.energy.ReceiveOnlyEnergyStorage;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.EnergySyncS2CPacket;
+import me.jddev0.ep.recipe.CompressorRecipe;
+import me.jddev0.ep.screen.CompressorMenu;
+import me.jddev0.ep.util.ByteUtils;
+import me.jddev0.ep.util.InventoryUtils;
+import me.jddev0.ep.util.RecipeUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.IntTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+public class CompressorBlockEntity extends BlockEntity implements MenuProvider, EnergyStoragePacketUpdate {
+    private static final int ENERGY_USAGE_PER_TICK = 16;
+
+    private final ItemStackHandler itemHandler = new ItemStackHandler(2) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+        }
+
+        @Override
+        public boolean isItemValid(int slot, @NotNull ItemStack stack) {
+            return switch(slot) {
+                case 0 -> level == null || RecipeUtils.isIngredientOfAny(level, CompressorRecipe.Type.INSTANCE, stack);
+                case 1 -> false;
+                default -> super.isItemValid(slot, stack);
+            };
+        }
+
+        @Override
+        public void setStackInSlot(int slot, @NotNull ItemStack stack) {
+            if(slot == 0) {
+                ItemStack itemStack = getStackInSlot(slot);
+                if(level != null && !stack.isEmpty() && !itemStack.isEmpty() && !ItemStack.isSameItemSameTags(stack, itemStack))
+                    resetProgress(worldPosition, level.getBlockState(worldPosition));
+            }
+
+            super.setStackInSlot(slot, stack);
+        }
+    };
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+    private final LazyOptional<IItemHandler> lazyItemHandlerSided = LazyOptional.of(
+            () -> new InputOutputItemHandler(itemHandler, (i, stack) -> i == 0, i -> i == 1));
+
+    private final ReceiveOnlyEnergyStorage energyStorage;
+
+    private LazyOptional<IEnergyStorage> lazyEnergyStorage = LazyOptional.empty();
+
+    protected  final ContainerData data;
+    private int progress;
+    private int maxProgress = 100;
+    private int energyConsumptionLeft = -1;
+    private boolean hasEnoughEnergy;
+
+    public CompressorBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.COMPRESSOR_ENTITY.get(), blockPos, blockState);
+
+        energyStorage = new ReceiveOnlyEnergyStorage(0, 2048, 256) {
+            @Override
+            protected void onChange() {
+                setChanged();
+
+                if(level != null && !level.isClientSide())
+                    ModMessages.sendToAllPlayers(new EnergySyncS2CPacket(energy, capacity, getBlockPos()));
+            }
+        };
+        data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch(index) {
+                    case 0 -> CompressorBlockEntity.this.progress;
+                    case 1 -> CompressorBlockEntity.this.maxProgress;
+                    case 2, 3 -> ByteUtils.get2Bytes(CompressorBlockEntity.this.energyStorage.getEnergy(), index - 2);
+                    case 4, 5 -> ByteUtils.get2Bytes(CompressorBlockEntity.this.energyStorage.getCapacity(), index - 4);
+                    case 6, 7 -> ByteUtils.get2Bytes(CompressorBlockEntity.this.energyConsumptionLeft, index - 6);
+                    case 8 -> hasEnoughEnergy?1:0;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch(index) {
+                    case 0 -> CompressorBlockEntity.this.progress = value;
+                    case 1 -> CompressorBlockEntity.this.maxProgress = value;
+                    case 2, 3 -> CompressorBlockEntity.this.energyStorage.setEnergyWithoutUpdate(ByteUtils.with2Bytes(
+                            CompressorBlockEntity.this.energyStorage.getEnergy(), (short)value, index - 2
+                    ));
+                    case 4, 5 -> CompressorBlockEntity.this.energyStorage.setCapacityWithoutUpdate(ByteUtils.with2Bytes(
+                            CompressorBlockEntity.this.energyStorage.getCapacity(), (short)value, index - 4
+                    ));
+                    case 6, 7, 8 -> {}
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 9;
+            }
+        };
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("container.energizedpower.compressor");
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+        return new CompressorMenu(id, inventory, this, this.data);
+    }
+
+    public int getRedstoneOutput() {
+        return InventoryUtils.getRedstoneSignalFromItemStackHandler(itemHandler);
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER) {
+            if(side == null)
+                return lazyItemHandler.cast();
+
+            return lazyItemHandlerSided.cast();
+        }else if(cap == ForgeCapabilities.ENERGY) {
+            return lazyEnergyStorage.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+        lazyEnergyStorage = LazyOptional.of(() -> energyStorage);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+
+        lazyItemHandler.invalidate();
+        lazyEnergyStorage.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        nbt.put("inventory", itemHandler.serializeNBT());
+        nbt.put("energy", energyStorage.saveNBT());
+
+        nbt.put("recipe.progress", IntTag.valueOf(progress));
+        nbt.put("recipe.energy_consumption_left", IntTag.valueOf(energyConsumptionLeft));
+
+        super.saveAdditional(nbt);
+    }
+
+    @Override
+    public void load(@NotNull CompoundTag nbt) {
+        super.load(nbt);
+
+        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
+        energyStorage.loadNBT(nbt.get("energy"));
+
+        progress = nbt.getInt("recipe.progress");
+        energyConsumptionLeft = nbt.getInt("recipe.energy_consumption_left");
+    }
+
+    public void drops(Level level, BlockPos worldPosition) {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for(int i = 0;i < itemHandler.getSlots();i++)
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+
+        Containers.dropContents(level, worldPosition, inventory);
+    }
+
+    public static void tick(Level level, BlockPos blockPos, BlockState state, CompressorBlockEntity blockEntity) {
+        if(level.isClientSide)
+            return;
+
+        if(hasRecipe(blockEntity)) {
+            SimpleContainer inventory = new SimpleContainer(blockEntity.itemHandler.getSlots());
+            for(int i = 0;i < blockEntity.itemHandler.getSlots();i++)
+                inventory.setItem(i, blockEntity.itemHandler.getStackInSlot(i));
+
+            Optional<CompressorRecipe> recipe = level.getRecipeManager().getRecipeFor(CompressorRecipe.Type.INSTANCE, inventory, level);
+            if(recipe.isEmpty())
+                return;
+
+            if(blockEntity.energyConsumptionLeft < 0)
+                blockEntity.energyConsumptionLeft = ENERGY_USAGE_PER_TICK * blockEntity.maxProgress;
+
+            if(ENERGY_USAGE_PER_TICK <= blockEntity.energyStorage.getEnergy()) {
+                blockEntity.hasEnoughEnergy = true;
+
+                blockEntity.energyStorage.setEnergy(blockEntity.energyStorage.getEnergy() - ENERGY_USAGE_PER_TICK);
+                blockEntity.energyConsumptionLeft -= ENERGY_USAGE_PER_TICK;
+
+                blockEntity.progress++;
+                setChanged(level, blockPos, state);
+
+                if(blockEntity.progress >= blockEntity.maxProgress) {
+                    craftItem(blockPos, state, blockEntity);
+                }
+            }else {
+                blockEntity.hasEnoughEnergy = false;
+            }
+        }else {
+            blockEntity.resetProgress(blockPos, state);
+            setChanged(level, blockPos, state);
+        }
+    }
+
+    private void resetProgress(BlockPos blockPos, BlockState state) {
+        progress = 0;
+        energyConsumptionLeft = -1;
+        hasEnoughEnergy = true;
+    }
+
+    private static void craftItem(BlockPos blockPos, BlockState state, CompressorBlockEntity blockEntity) {
+        Level level = blockEntity.level;
+
+        SimpleContainer inventory = new SimpleContainer(blockEntity.itemHandler.getSlots());
+        for(int i = 0;i < blockEntity.itemHandler.getSlots();i++)
+            inventory.setItem(i, blockEntity.itemHandler.getStackInSlot(i));
+
+        Optional<CompressorRecipe> recipe = level.getRecipeManager().getRecipeFor(CompressorRecipe.Type.INSTANCE, inventory, level);
+
+        if(!hasRecipe(blockEntity) || recipe.isEmpty())
+            return;
+
+        blockEntity.itemHandler.extractItem(0, 1, false);
+        blockEntity.itemHandler.setStackInSlot(1, new ItemStack(recipe.get().getResultItem(level.registryAccess()).getItem(),
+                blockEntity.itemHandler.getStackInSlot(1).getCount() + 1));
+
+        blockEntity.resetProgress(blockPos, state);
+    }
+
+    private static boolean hasRecipe(CompressorBlockEntity blockEntity) {
+        Level level = blockEntity.level;
+
+        SimpleContainer inventory = new SimpleContainer(blockEntity.itemHandler.getSlots());
+        for(int i = 0;i < blockEntity.itemHandler.getSlots();i++)
+            inventory.setItem(i, blockEntity.itemHandler.getStackInSlot(i));
+
+        Optional<CompressorRecipe> recipe = level.getRecipeManager().getRecipeFor(CompressorRecipe.Type.INSTANCE, inventory, level);
+
+        return recipe.isPresent() && canInsertAmountIntoOutputSlot(inventory) && canInsertItemIntoOutputSlot(inventory,
+                recipe.get().getResultItem(level.registryAccess()));
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack itemStack) {
+        return inventory.getItem(1).isEmpty() || inventory.getItem(1).getItem() == itemStack.getItem();
+    }
+
+    private static boolean canInsertAmountIntoOutputSlot(SimpleContainer inventory) {
+        return inventory.getItem(1).getMaxStackSize() > inventory.getItem(1).getCount();
+    }
+
+    @Override
+    public void setEnergy(int energy) {
+        energyStorage.setEnergyWithoutUpdate(energy);
+    }
+
+    @Override
+    public void setCapacity(int capacity) {
+        energyStorage.setCapacityWithoutUpdate(capacity);
+    }
+
+    public int getCapacity() {
+        return energyStorage.getCapacity();
+    }
+}
