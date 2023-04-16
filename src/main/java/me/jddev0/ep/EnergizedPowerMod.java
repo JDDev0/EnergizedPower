c041bfb61146adde51a34fd08c795c2cd3ff7a54
JDDev0 <jddev0@dorneger.at>
2023-04-16 19:39:08 +0200

    Add the Iron Plate item

--- src/main/java/me/jddev0/ep/EnergizedPowerMod.java
+++ src/main/java/me/jddev0/ep/EnergizedPowerMod.java
@@ -115,6 +115,7 @@ public class EnergizedPowerMod implements ModInitializer {
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.SAWDUST);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.SILICON);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.COPPER_PLATE);
+		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.IRON_PLATE);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.ENERGIZED_COPPER_INGOT);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.ENERGIZED_GOLD_INGOT);
 	}
