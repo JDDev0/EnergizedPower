fd8f18d82495c448bfe6043ba375e409cf95534d
JDDev0 <jddev0@dorneger.at>
2023-04-17 19:39:27 +0200

    Add the Golden Hammer item

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -77,6 +77,8 @@ public final class ModItems {
             new HammerItem(ToolMaterials.STONE, new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
     public static final Item IRON_HAMMER = registerItem("iron_hammer",
             new HammerItem(ToolMaterials.IRON, new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
+    public static final Item GOLDEN_HAMMER = registerItem("golden_hammer",
+            new HammerItem(ToolMaterials.GOLD, new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
     public static final Item DIAMOND_HAMMER = registerItem("diamond_hammer",
             new HammerItem(ToolMaterials.DIAMOND, new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
     public static final Item NETHERITE_HAMMER = registerItem("netherite_hammer",
