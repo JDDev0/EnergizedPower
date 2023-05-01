88788ff8e89ac9b7d33c2d2b55fd49165af7e0d4
JDDev0 <jddev0@dorneger.at>
2023-05-01 13:52:00 +0200

    Fix the "craftItem()" method in the "AutoCrafterBlockEntity" and the "PlantGrowthChamberBlockEntity" class
    (cherry picked from commit a9be1dcb74742d647977ea2268bdd463b1dbee8a)

--- src/main/java/me/jddev0/ep/block/entity/AutoCrafterBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/AutoCrafterBlockEntity.java
@@ -414,6 +414,7 @@ public class AutoCrafterBlockEntity extends BlockEntity implements MenuProvider,
         List<ItemStack> itemStacksInsert = ItemStackUtils.combineItemStacks(outputItemStacks);
 
         List<Integer> emptyIndices = new ArrayList<>(18);
+        outer:
         for(ItemStack itemStack:itemStacksInsert) {
             for(int i = 0;i < itemHandler.getSlots();i++) {
                 ItemStack testItemStack = itemHandler.getStackInSlot(i);
@@ -434,6 +435,9 @@ public class AutoCrafterBlockEntity extends BlockEntity implements MenuProvider,
                         itemHandler.setStackInSlot(i, itemStackCopy);
 
                         itemStack.setCount(itemStack.getCount() - amount);
+
+                        if(itemStack.isEmpty())
+                            continue outer;
                     }
                 }
             }
