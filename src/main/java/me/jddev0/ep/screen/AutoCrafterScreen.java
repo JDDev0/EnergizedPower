43119d71cd0ecff369416fecaca2cef5addcc0e9
JDDev0 <jddev0@dorneger.at>
2023-05-03 18:22:36 +0200

    Improve Auto Crafter: Add ignore NBT checkbox

--- src/main/java/me/jddev0/ep/screen/AutoCrafterScreen.java
+++ src/main/java/me/jddev0/ep/screen/AutoCrafterScreen.java
@@ -2,13 +2,20 @@ package me.jddev0.ep.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.SetAutoCrafterCheckboxC2SPacket;
 import me.jddev0.ep.util.EnergyUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -34,6 +41,28 @@ public class AutoCrafterScreen extends HandledScreen<AutoCrafterMenu> {
     }
 
     @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isPointWithinBounds(158, 16, 11, 11, mouseX, mouseY)) {
+                //Ignore NBT checkbox
+
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeBlockPos(handler.getBlockEntity().getPos());
+                buf.writeInt(0);
+                buf.writeBoolean(!handler.isIgnoreNBT());
+                ClientPlayNetworking.send(ModMessages.SET_AUTO_CRAFTER_CHECKBOX_ID, buf);
+                clicked = true;
+            }
+
+            if(clicked)
+                client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
     protected void drawBackground(MatrixStack poseStack, float partialTick, int mouseX, int mouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexProgram);
         RenderSystem.setShaderColor(1.f, 1.f, 1.f, 1.f);
@@ -45,6 +74,7 @@ public class AutoCrafterScreen extends HandledScreen<AutoCrafterMenu> {
         renderProgressArrow(poseStack, x, y);
         renderEnergyMeter(poseStack, x, y);
         renderEnergyRequirementBar(poseStack, x, y);
+        renderCheckboxes(poseStack, x, y, mouseX, mouseY);
     }
 
     private void renderProgressArrow(MatrixStack poseStack, int x, int y) {
@@ -61,6 +91,14 @@ public class AutoCrafterScreen extends HandledScreen<AutoCrafterMenu> {
         int pos = handler.getEnergyRequirementBarPos();
         if(pos > 0)
             drawTexture(poseStack, x + 8, y + 17 + 52 - pos, 176, 69, 16, 1);
+    }
+
+    private void renderCheckboxes(MatrixStack poseStack, int x, int y, int mouseX, int mouseY) {
+        if(handler.isIgnoreNBT()) {
+            //Ignore NBT checkbox
+
+            drawTexture(poseStack, x + 158, y + 16, 176, 70, 11, 11);
+        }
     }
 
     @Override
@@ -84,6 +122,13 @@ public class AutoCrafterScreen extends HandledScreen<AutoCrafterMenu> {
                 components.add(Text.translatable("tooltip.energizedpower.recipe.energy_required_to_finish.txt",
                         EnergyUtils.getEnergyWithPrefix(handler.getEnergyRequirement())).formatted(Formatting.YELLOW));
             }
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }else if(isPointWithinBounds(158, 16, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Text> components = new ArrayList<>(2);
+            components.add(Text.translatable("tooltip.energizedpower.auto_crafter.cbx.ignore_nbt"));
 
             renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
         }
