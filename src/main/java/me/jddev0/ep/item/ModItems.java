45dc0c245403f052f08d8f7d5b8ce2a40099ea59
JDDev0 <jddev0@dorneger.at>
2023-05-03 18:48:32 +0200

    Add Good Fertilizer

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -39,6 +39,9 @@ public final class ModItems {
     public static final RegistryObject<Item> BASIC_FERTILIZER = ITEMS.register("basic_fertilizer",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
 
+    public static final RegistryObject<Item> GOOD_FERTILIZER = ITEMS.register("good_fertilizer",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
+
     public static final RegistryObject<Item> BASIC_SOLAR_CELL = ITEMS.register("basic_solar_cell",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
     public static final RegistryObject<Item> ADVANCED_SOLAR_CELL = ITEMS.register("advanced_solar_cell",
