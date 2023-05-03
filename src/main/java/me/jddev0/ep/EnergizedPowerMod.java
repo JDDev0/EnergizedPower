410971621e2d490a390358d59f98c1e10e3f2134
JDDev0 <jddev0@dorneger.at>
2023-05-03 18:17:32 +0200

    Add Weather Controller

--- src/main/java/me/jddev0/ep/EnergizedPowerMod.java
+++ src/main/java/me/jddev0/ep/EnergizedPowerMod.java
@@ -98,6 +98,7 @@ public class EnergizedPowerMod implements ModInitializer {
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModBlocks.UNCHARGER_ITEM);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModBlocks.ENERGIZER_ITEM);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModBlocks.CHARGING_STATION_ITEM);
+		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModBlocks.WEATHER_CONTROLLER_ITEM);
 
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.BASIC_SOLAR_CELL);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB, ModItems.ADVANCED_SOLAR_CELL);
---
+++ src/main/java/me/jddev0/ep/block/WeatherControllerBlock.java
@@ -0,0 +1,48 @@
+package me.jddev0.ep.block;
+
+import me.jddev0.ep.block.entity.WeatherControllerBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class WeatherControllerBlock extends BlockWithEntity {
+    public static int WEATHER_CHANGED_TICKS = 20 * 60; //1 minute
+
+    public WeatherControllerBlock(FabricBlockSettings props) {
+        super(props);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos blockPos, BlockState state) {
+        return new WeatherControllerBlockEntity(blockPos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World level, BlockPos blockPos, PlayerEntity player, Hand handItem, BlockHitResult hit) {
+        if(level.isClient())
+            return ActionResult.SUCCESS;
+
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if(!(blockEntity instanceof WeatherControllerBlockEntity))
+            throw new IllegalStateException("Container is invalid");
+
+        player.openHandledScreen((WeatherControllerBlockEntity)blockEntity);
+
+        return ActionResult.SUCCESS;
+    }
+}
