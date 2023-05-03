9c0a88554349b831475c91e868f8a58044c14065
JDDev0 <jddev0@dorneger.at>
2023-05-03 15:59:10 +0200

    Add Advanced Fertilizer

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -44,6 +44,9 @@ public final class ModItems {
     public static final Item GOOD_FERTILIZER = registerItem("good_fertilizer",
             new Item(new FabricItemSettings()));
 
+    public static final Item ADVANCED_FERTILIZER = registerItem("advanced_fertilizer",
+            new Item(new FabricItemSettings()));
+
     public static final Item BASIC_SOLAR_CELL = registerItem("basic_solar_cell",
             new Item(new FabricItemSettings()));
     public static final Item ADVANCED_SOLAR_CELL = registerItem("advanced_solar_cell",
