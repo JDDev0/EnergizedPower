c86a3dda3b20255ff1a9d49dddd1df3d19fea69b
JDDev0 <jddev0@dorneger.at>
2023-05-11 14:21:36 +0200

    Fix JEI recipe GUI for Dispenser Crafting and In World Crafting

--- src/main/java/me/jddev0/ep/integration/jei/DispenserCategory.java
+++ src/main/java/me/jddev0/ep/integration/jei/DispenserCategory.java
@@ -24,7 +24,7 @@ public class DispenserCategory implements IRecipeCategory<DispenserCategory.Disp
 
     public DispenserCategory(IGuiHelper helper) {
         ResourceLocation texture = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/recipe/misc_gui.png");
-        background = helper.createDrawable(texture, 1, 1, 105, 25);
+        background = helper.createDrawable(texture, 1, 1, 105, 26);
 
         icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Items.DISPENSER));
     }
