fb08d87dfb487113144530792a2f3a3864e8d7c8
JDDev0 <jddev0@dorneger.at>
2023-05-15 17:04:45 +0200

    Add Heat Generator

---
+++ src/main/java/me/jddev0/ep/block/entity/HeatGeneratorBlockEntity.java
@@ -0,0 +1,182 @@
+package me.jddev0.ep.block.entity;
+
+import me.jddev0.ep.block.LightningGeneratorBlock;
+import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
+import me.jddev0.ep.energy.ExtractOnlyEnergyStorage;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.EnergySyncS2CPacket;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HeatGeneratorBlockEntity extends BlockEntity implements EnergyStoragePacketUpdate {
+    public static final int ENERGY_PER_FACE_TOUCHING_LAVA = 25;
+
+    public static final int MAX_EXTRACT = 6 * ENERGY_PER_FACE_TOUCHING_LAVA;
+
+    private final ExtractOnlyEnergyStorage energyStorage;
+    private LazyOptional<IEnergyStorage> lazyEnergyStorage = LazyOptional.empty();
+
+    public HeatGeneratorBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.HEAT_GENERATOR_ENTITY.get(), blockPos, blockState);
+
+        int maxExtract = ENERGY_PER_FACE_TOUCHING_LAVA * 6; //6 times energy production per face
+        int capacity = maxExtract * 20 * 2; //2 seconds of max extract
+
+        energyStorage = new ExtractOnlyEnergyStorage(0, capacity, maxExtract) {
+            @Override
+            protected void onChange() {
+                setChanged();
+
+                if(level != null && !level.isClientSide())
+                    ModMessages.sendToAllPlayers(new EnergySyncS2CPacket(energy, capacity, getBlockPos()));
+            }
+        };
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityEnergy.ENERGY) {
+            return lazyEnergyStorage.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        lazyEnergyStorage = LazyOptional.of(() -> energyStorage);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+
+        lazyEnergyStorage.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        nbt.put("energy", energyStorage.saveNBT());
+
+        super.saveAdditional(nbt);
+    }
+
+    @Override
+    public void load(@NotNull CompoundTag nbt) {
+        super.load(nbt);
+
+        energyStorage.loadNBT(nbt.get("energy"));
+    }
+
+    public static void tick(Level level, BlockPos blockPos, BlockState state, HeatGeneratorBlockEntity blockEntity) {
+        if(level.isClientSide)
+            return;
+
+        int productionSum = 0;
+        for(Direction direction:Direction.values()) {
+            BlockPos checkPos = blockPos.relative(direction);
+            FluidState fluidState = level.getFluidState(checkPos);
+            if(fluidState.is(Fluids.LAVA) || fluidState.is(Fluids.FLOWING_LAVA)) {
+                productionSum += ENERGY_PER_FACE_TOUCHING_LAVA;
+            }
+        }
+
+        blockEntity.energyStorage.setEnergy(Math.min(blockEntity.energyStorage.getCapacity(),
+                blockEntity.energyStorage.getEnergy() + productionSum));
+
+        transferEnergy(level, blockPos, state, blockEntity);
+    }
+
+    private static void transferEnergy(Level level, BlockPos blockPos, BlockState state, HeatGeneratorBlockEntity blockEntity) {
+        if(level.isClientSide)
+            return;
+
+        List<IEnergyStorage> consumerItems = new LinkedList<>();
+        List<Integer> consumerEnergyValues = new LinkedList<>();
+        int consumptionSum = 0;
+        for(Direction direction:Direction.values()) {
+            BlockPos testPos = blockPos.relative(direction);
+
+            BlockEntity testBlockEntity = level.getBlockEntity(testPos);
+            if(testBlockEntity == null)
+                continue;
+
+            LazyOptional<IEnergyStorage> energyStorageLazyOptional = testBlockEntity.getCapability(CapabilityEnergy.ENERGY, direction.getOpposite());
+            if(!energyStorageLazyOptional.isPresent())
+                continue;
+
+            IEnergyStorage energyStorage = energyStorageLazyOptional.orElse(null);
+            if(!energyStorage.canReceive())
+                continue;
+
+            int received = energyStorage.receiveEnergy(Math.min(MAX_EXTRACT, blockEntity.energyStorage.getEnergy()), true);
+            if(received <= 0)
+                continue;
+
+            consumptionSum += received;
+            consumerItems.add(energyStorage);
+            consumerEnergyValues.add(received);
+        }
+
+        List<Integer> consumerEnergyDistributed = new LinkedList<>();
+        for(int i = 0;i < consumerItems.size();i++)
+            consumerEnergyDistributed.add(0);
+
+        int consumptionLeft = Math.min(MAX_EXTRACT, Math.min(blockEntity.energyStorage.getEnergy(), consumptionSum));
+        blockEntity.energyStorage.extractEnergy(consumptionLeft, false);
+
+        int divisor = consumerItems.size();
+        outer:
+        while(consumptionLeft > 0) {
+            int consumptionPerConsumer = consumptionLeft / divisor;
+            if(consumptionPerConsumer == 0) {
+                divisor = Math.max(1, divisor - 1);
+                consumptionPerConsumer = consumptionLeft / divisor;
+            }
+
+            for(int i = 0;i < consumerEnergyValues.size();i++) {
+                int consumptionDistributed = consumerEnergyDistributed.get(i);
+                int consumptionOfConsumerLeft = consumerEnergyValues.get(i) - consumptionDistributed;
+
+                int consumptionDistributedNew = Math.min(consumptionOfConsumerLeft, Math.min(consumptionPerConsumer, consumptionLeft));
+                consumerEnergyDistributed.set(i, consumptionDistributed + consumptionDistributedNew);
+                consumptionLeft -= consumptionDistributedNew;
+                if(consumptionLeft == 0)
+                    break outer;
+            }
+        }
+
+        for(int i = 0;i < consumerItems.size();i++) {
+            int energy = consumerEnergyDistributed.get(i);
+            if(energy > 0)
+                consumerItems.get(i).receiveEnergy(energy, false);
+        }
+    }
+
+    @Override
+    public void setEnergy(int energy) {
+        energyStorage.setEnergyWithoutUpdate(energy);
+    }
+
+    @Override
+    public void setCapacity(int capacity) {
+        energyStorage.setCapacityWithoutUpdate(capacity);
+    }
+}
