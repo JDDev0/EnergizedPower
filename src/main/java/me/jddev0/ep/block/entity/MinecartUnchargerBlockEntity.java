6a8bbc5db7ec6ed02a9556ecac705ac6addd050b
JDDev0 <jddev0@dorneger.at>
2023-05-15 17:28:09 +0200

    Add Minecart Uncharger

---
+++ src/main/java/me/jddev0/ep/block/entity/MinecartUnchargerBlockEntity.java
@@ -0,0 +1,184 @@
+package me.jddev0.ep.block.entity;
+
+import me.jddev0.ep.block.MinecartUnchargerBlock;
+import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
+import me.jddev0.ep.energy.ExtractOnlyEnergyStorage;
+import me.jddev0.ep.entity.MinecartBatteryBox;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.EnergySyncS2CPacket;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntitySelector;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.entity.EntityTypeTest;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MinecartUnchargerBlockEntity extends BlockEntity implements EnergyStoragePacketUpdate {
+    public static final int CAPACITY = 16384;
+    public static final int MAX_TRANSFER = 512;
+
+    private final ExtractOnlyEnergyStorage energyStorage;
+    private LazyOptional<IEnergyStorage> lazyEnergyStorage = LazyOptional.empty();
+
+    public MinecartUnchargerBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.MINECART_UNCHARGER_ENTITY.get(), blockPos, blockState);
+
+        energyStorage = new ExtractOnlyEnergyStorage(0, CAPACITY, MAX_TRANSFER) {
+            @Override
+            protected void onChange() {
+                setChanged();
+
+                if(level != null && !level.isClientSide())
+                    ModMessages.sendToAllPlayers(new EnergySyncS2CPacket(energy, capacity, getBlockPos()));
+            }
+        };
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityEnergy.ENERGY) {
+            return lazyEnergyStorage.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        lazyEnergyStorage = LazyOptional.of(() -> energyStorage);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+
+        lazyEnergyStorage.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        nbt.put("energy", energyStorage.saveNBT());
+
+        super.saveAdditional(nbt);
+    }
+
+    @Override
+    public void load(@NotNull CompoundTag nbt) {
+        super.load(nbt);
+
+        energyStorage.loadNBT(nbt.get("energy"));
+    }
+
+    public static void tick(Level level, BlockPos blockPos, BlockState state, MinecartUnchargerBlockEntity blockEntity) {
+        if(level.isClientSide)
+            return;
+
+        BlockPos blockPosFacing = blockEntity.getBlockPos().relative(blockEntity.getBlockState().getValue(MinecartUnchargerBlock.FACING));
+        List<MinecartBatteryBox> minecarts = level.getEntities(EntityTypeTest.forClass(MinecartBatteryBox.class),
+                new AABB(blockPosFacing.getX(), blockPosFacing.getY(),
+                        blockPosFacing.getZ(), blockPosFacing.getX() + 1,
+                        blockPosFacing.getY() + 1, blockPosFacing.getZ() + 1),
+                EntitySelector.ENTITY_STILL_ALIVE);
+        if(minecarts.isEmpty())
+            return;
+
+        MinecartBatteryBox minecart = minecarts.get(0);
+        int transferred = Math.min(Math.min(CAPACITY - blockEntity.energyStorage.getEnergy(), MAX_TRANSFER),
+                Math.min(MinecartBatteryBox.MAX_TRANSFER, minecart.getEnergy()));
+        minecart.setEnergy(minecart.getEnergy() - transferred);
+
+        blockEntity.energyStorage.setEnergy(blockEntity.energyStorage.getEnergy() + transferred);
+
+        transferEnergy(level, blockPos, state, blockEntity);
+    }
+
+    private static void transferEnergy(Level level, BlockPos blockPos, BlockState state, MinecartUnchargerBlockEntity blockEntity) {
+        if(level.isClientSide)
+            return;
+
+        List<IEnergyStorage> consumerItems = new LinkedList<>();
+        List<Integer> consumerEnergyValues = new LinkedList<>();
+        int consumptionSum = 0;
+        for(Direction direction:Direction.values()) {
+            BlockPos testPos = blockPos.relative(direction);
+
+            BlockEntity testBlockEntity = level.getBlockEntity(testPos);
+            if(testBlockEntity == null)
+                continue;
+
+            LazyOptional<IEnergyStorage> energyStorageLazyOptional = testBlockEntity.getCapability(CapabilityEnergy.ENERGY, direction.getOpposite());
+            if(!energyStorageLazyOptional.isPresent())
+                continue;
+
+            IEnergyStorage energyStorage = energyStorageLazyOptional.orElse(null);
+            if(!energyStorage.canReceive())
+                continue;
+
+            int received = energyStorage.receiveEnergy(Math.min(MAX_TRANSFER, blockEntity.energyStorage.getEnergy()), true);
+            if(received <= 0)
+                continue;
+
+            consumptionSum += received;
+            consumerItems.add(energyStorage);
+            consumerEnergyValues.add(received);
+        }
+
+        List<Integer> consumerEnergyDistributed = new LinkedList<>();
+        for(int i = 0;i < consumerItems.size();i++)
+            consumerEnergyDistributed.add(0);
+
+        int consumptionLeft = Math.min(MAX_TRANSFER, Math.min(blockEntity.energyStorage.getEnergy(), consumptionSum));
+        blockEntity.energyStorage.extractEnergy(consumptionLeft, false);
+
+        int divisor = consumerItems.size();
+        outer:
+        while(consumptionLeft > 0) {
+            int consumptionPerConsumer = consumptionLeft / divisor;
+            if(consumptionPerConsumer == 0) {
+                divisor = Math.max(1, divisor - 1);
+                consumptionPerConsumer = consumptionLeft / divisor;
+            }
+
+            for(int i = 0;i < consumerEnergyValues.size();i++) {
+                int consumptionDistributed = consumerEnergyDistributed.get(i);
+                int consumptionOfConsumerLeft = consumerEnergyValues.get(i) - consumptionDistributed;
+
+                int consumptionDistributedNew = Math.min(consumptionOfConsumerLeft, Math.min(consumptionPerConsumer, consumptionLeft));
+                consumerEnergyDistributed.set(i, consumptionDistributed + consumptionDistributedNew);
+                consumptionLeft -= consumptionDistributedNew;
+                if(consumptionLeft == 0)
+                    break outer;
+            }
+        }
+
+        for(int i = 0;i < consumerItems.size();i++) {
+            int energy = consumerEnergyDistributed.get(i);
+            if(energy > 0)
+                consumerItems.get(i).receiveEnergy(energy, false);
+        }
+    }
+
+    @Override
+    public void setEnergy(int energy) {
+        energyStorage.setEnergyWithoutUpdate(energy);
+    }
+
+    @Override
+    public void setCapacity(int capacity) {
+        energyStorage.setCapacityWithoutUpdate(capacity);
+    }
+}
