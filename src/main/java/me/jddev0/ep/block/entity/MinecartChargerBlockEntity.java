6d1fc3e579dc3da47dbd9d0f64393a09485ae8e1
JDDev0 <jddev0@dorneger.at>
2023-05-24 11:36:23 +0200

    Fix comparator output of Minecart Charger and Minecart Uncharger was not updated when minecart left
    (cherry picked from commit 06d54cec5fd1526c602d5bc5742dc609ec51f9cd)

--- src/main/java/me/jddev0/ep/block/entity/MinecartChargerBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/MinecartChargerBlockEntity.java
@@ -42,6 +42,9 @@ public class MinecartChargerBlockEntity extends BlockEntity implements MenuProvi
 
     protected final ContainerData data;
 
+    private boolean hasMinecartOld = true; //Default true (Force first update)
+    private boolean hasMinecart = false; //Default false (Force first update)
+
     public MinecartChargerBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModBlockEntities.MINECART_CHARGER_ENTITY.get(), blockPos, blockState);
 
@@ -153,13 +156,19 @@ public class MinecartChargerBlockEntity extends BlockEntity implements MenuProvi
         if(level.isClientSide)
             return;
 
+        if(blockEntity.hasMinecartOld != blockEntity.hasMinecart)
+            setChanged(level, blockPos, state);
+
+        blockEntity.hasMinecartOld = blockEntity.hasMinecart;
+
         BlockPos blockPosFacing = blockEntity.getBlockPos().relative(blockEntity.getBlockState().getValue(MinecartChargerBlock.FACING));
         List<MinecartBatteryBox> minecarts = level.getEntities(EntityTypeTest.forClass(MinecartBatteryBox.class),
                 new AABB(blockPosFacing.getX(), blockPosFacing.getY(),
                         blockPosFacing.getZ(), blockPosFacing.getX() + 1,
                         blockPosFacing.getY() + 1, blockPosFacing.getZ() + 1),
                 EntitySelector.ENTITY_STILL_ALIVE);
-        if(minecarts.isEmpty())
+        blockEntity.hasMinecart = !minecarts.isEmpty();
+        if(!blockEntity.hasMinecart)
             return;
 
         MinecartBatteryBox minecart = minecarts.get(0);
