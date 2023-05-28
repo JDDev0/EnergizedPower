801ba5958d7ca7b97fb0a6daa172aea6f02c1385
JDDev0 <jddev0@dorneger.at>
2023-05-28 12:27:09 +0200

    Make AbstractGenericEnergyStorageContainerScreen class more general

--- src/main/java/me/jddev0/ep/screen/AbstractGenericEnergyStorageHandledScreen.java
+++ src/main/java/me/jddev0/ep/screen/AbstractGenericEnergyStorageHandledScreen.java
@@ -11,6 +11,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
@@ -19,10 +20,51 @@ import java.util.Optional;
 
 @Environment(EnvType.CLIENT)
 public abstract class AbstractGenericEnergyStorageHandledScreen<T extends ScreenHandler & EnergyStorageMenu> extends HandledScreen<T> {
-    protected static final Identifier TEXTURE = new Identifier(EnergizedPowerMod.MODID, "textures/gui/container/generic_energy.png");
+    protected final Identifier TEXTURE;
+    protected final int energyMeterX;
+    protected final int energyMeterY;
+    protected final int energyMeterWidth;
+    protected final int energyMeterHeight;
+
+    protected final String energyIndicatorBarTooltipComponentID;
 
     public AbstractGenericEnergyStorageHandledScreen(T menu, PlayerInventory inventory, Text component) {
-        super(menu, inventory, component);
+        this(menu, inventory, component, null);
+    }
+
+    public AbstractGenericEnergyStorageHandledScreen(T menu, PlayerInventory inventory, Text titleComponent,
+                                                     String energyIndicatorBarTooltipComponentID) {
+        this(menu, inventory, titleComponent, energyIndicatorBarTooltipComponentID,
+                new Identifier(EnergizedPowerMod.MODID, "textures/gui/container/generic_energy.png"),
+                80, 17);
+    }
+
+    public AbstractGenericEnergyStorageHandledScreen(T menu, PlayerInventory inventory, Text titleComponent,
+                                                     Identifier texture, int energyMeterX, int energyMeterY) {
+        this(menu, inventory, titleComponent, null, texture, energyMeterX, energyMeterY);
+    }
+
+    public AbstractGenericEnergyStorageHandledScreen(T menu, PlayerInventory inventory, Text titleComponent,
+                                                     String energyIndicatorBarTooltipComponentID,
+                                                     Identifier texture, int energyMeterX, int energyMeterY) {
+        this(menu, inventory, titleComponent, energyIndicatorBarTooltipComponentID, texture,
+                energyMeterX, energyMeterY, 16, 52);
+    }
+
+    public AbstractGenericEnergyStorageHandledScreen(T menu, PlayerInventory inventory, Text titleComponent,
+                                                     String energyIndicatorBarTooltipComponentID,
+                                                     Identifier texture, int energyMeterX, int energyMeterY,
+                                                     int energyMeterWidth, int energyMeterHeight) {
+        super(menu, inventory, titleComponent);
+
+        this.TEXTURE = texture;
+
+        this.energyMeterX = energyMeterX;
+        this.energyMeterY = energyMeterY;
+        this.energyMeterWidth = energyMeterWidth;
+        this.energyMeterHeight = energyMeterHeight;
+
+        this.energyIndicatorBarTooltipComponentID = energyIndicatorBarTooltipComponentID;
     }
 
     @Override
@@ -35,11 +77,20 @@ public abstract class AbstractGenericEnergyStorageHandledScreen<T extends Screen
 
         drawTexture(poseStack, x, y, 0, 0, backgroundWidth, backgroundHeight);
         renderEnergyMeter(poseStack, x, y);
+        renderEnergyIndicatorBar(poseStack, x, y);
     }
 
-    private void renderEnergyMeter(MatrixStack poseStack, int x, int y) {
-        int pos = handler.getScaledEnergyMeterPos(52);
-        drawTexture(poseStack, x + 80, y + 17 + 52 - pos, 176, 52 - pos, 16, pos);
+    protected void renderEnergyMeter(MatrixStack poseStack, int x, int y) {
+        int pos = handler.getScaledEnergyMeterPos(energyMeterHeight);
+        drawTexture(poseStack, x + energyMeterX, y + energyMeterY + energyMeterHeight - pos, 176,
+                energyMeterHeight - pos, energyMeterWidth, pos);
+    }
+
+    protected void renderEnergyIndicatorBar(MatrixStack poseStack, int x, int y) {
+        int pos = handler.getScaledEnergyIndicatorBarPos(energyMeterHeight);
+        if(pos > 0)
+            drawTexture(poseStack, x + energyMeterX, y + energyMeterY + energyMeterHeight - pos, 176,
+                    energyMeterHeight, energyMeterWidth, 1);
     }
 
     @Override
@@ -55,10 +106,14 @@ public abstract class AbstractGenericEnergyStorageHandledScreen<T extends Screen
     protected void drawMouseoverTooltip(MatrixStack poseStack, int mouseX, int mouseY) {
         super.drawMouseoverTooltip(poseStack, mouseX, mouseY);
 
-        if(isPointWithinBounds(80, 17, 16, 52, mouseX, mouseY)) {
+        if(isPointWithinBounds(energyMeterX, energyMeterY, energyMeterWidth, energyMeterHeight, mouseX, mouseY)) {
             List<Text> components = new ArrayList<>(2);
             components.add(Text.translatable("tooltip.energizedpower.energy_meter.content.txt",
                     EnergyUtils.getEnergyWithPrefix(handler.getEnergy()), EnergyUtils.getEnergyWithPrefix(handler.getCapacity())));
+            if(handler.getEnergyIndicatorBarValue() > 0 && energyIndicatorBarTooltipComponentID != null) {
+                components.add(Text.translatable(energyIndicatorBarTooltipComponentID,
+                        EnergyUtils.getEnergyWithPrefix(handler.getEnergyIndicatorBarValue())).formatted(Formatting.YELLOW));
+            }
 
             renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
         }
--- src/main/java/me/jddev0/ep/screen/BatteryBoxMenu.java
+++ src/main/java/me/jddev0/ep/screen/BatteryBoxMenu.java
@@ -58,11 +58,11 @@ public class BatteryBoxMenu extends ScreenHandler implements EnergyStorageMenu, 
     }
 
     @Override
-    public int getScaledEnergyMeterPos(int energyBarHeight) {
+    public int getScaledEnergyMeterPos(int energyMeterHeight) {
         long energy = getEnergy();
         long capacity = getCapacity();
 
-        return (int)((energy == 0 || capacity == 0)?0:Math.max(1, energy * energyBarHeight / capacity));
+        return (int)((energy == 0 || capacity == 0)?0:Math.max(1, energy * energyMeterHeight / capacity));
     }
 
     @Override
