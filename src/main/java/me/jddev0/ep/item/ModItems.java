7274c7e280c24048d5b1ae3052a4b2ba903a0d28
JDDev0 <jddev0@dorneger.at>
2023-06-01 15:30:29 +0200

    Add the Crystal Matrix item

--- src/main/java/me/jddev0/ep/item/ModItems.java
+++ src/main/java/me/jddev0/ep/item/ModItems.java
@@ -59,6 +59,9 @@ public final class ModItems {
     public static final Item SAW_BLADE = registerItem("saw_blade",
             new Item(new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
 
+    public static final Item CRYSTAL_MATRIX = registerItem("crystal_matrix",
+            new Item(new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB)));
+
     public static final Item INVENTORY_COAL_ENGINE = registerItem("inventory_coal_engine",
             new InventoryCoalEngine(new FabricItemSettings().group(ModCreativeModeTab.ENERGIZED_POWER_TAB).maxCount(1)));
 
