578ead2c1305704f417ea45edd416681eebf410f
JDDev0 <jddev0@dorneger.at>
2023-06-01 15:46:06 +0200

    Add inverse rotation checkbox to the Block Placer

--- src/main/java/me/jddev0/ep/screen/BlockPlacerScreen.java
+++ src/main/java/me/jddev0/ep/screen/BlockPlacerScreen.java
@@ -1,11 +1,23 @@
 package me.jddev0.ep.screen;
 
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.networking.ModMessages;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Environment(EnvType.CLIENT)
 public class BlockPlacerScreen extends AbstractGenericEnergyStorageHandledScreen<BlockPlacerMenu> {
@@ -14,5 +26,59 @@ public class BlockPlacerScreen extends AbstractGenericEnergyStorageHandledScreen
                 "tooltip.energizedpower.block_placer.block_energy_left.txt",
                 new Identifier(EnergizedPowerMod.MODID, "textures/gui/container/block_placer.png"),
                 8, 17);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isPointWithinBounds(158, 16, 11, 11, mouseX, mouseY)) {
+                //Inverse rotation checkbox
+
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeBlockPos(handler.getBlockEntity().getPos());
+                buf.writeInt(0);
+                buf.writeBoolean(!handler.isInverseRotation());
+                ClientPlayNetworking.send(ModMessages.SET_BLOCK_PLACER_CHECKBOX_ID, buf);
+                clicked = true;
+            }
+
+            if(clicked)
+                client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    protected void drawBackground(MatrixStack poseStack, float partialTick, int mouseX, int mouseY) {
+        super.drawBackground(poseStack, partialTick, mouseX, mouseY);
+
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+
+        renderCheckboxes(poseStack, x, y, mouseX, mouseY);
+    }
+
+    private void renderCheckboxes(MatrixStack poseStack, int x, int y, int mouseX, int mouseY) {
+        if(handler.isInverseRotation()) {
+            //Inverse rotation checkbox
+
+            drawTexture(poseStack, x + 158, y + 16, 176, 53, 11, 11);
+        }
+    }
+
+    @Override
+    protected void drawMouseoverTooltip(MatrixStack poseStack, int mouseX, int mouseY) {
+        super.drawMouseoverTooltip(poseStack, mouseX, mouseY);
+
+        if(isPointWithinBounds(158, 16, 11, 11, mouseX, mouseY)) {
+            //Inverse rotation checkbox
+
+            List<Text> components = new ArrayList<>(2);
+            components.add(new TranslatableText("tooltip.energizedpower.block_placer.cbx.inverse_rotation"));
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }
     }
 }
