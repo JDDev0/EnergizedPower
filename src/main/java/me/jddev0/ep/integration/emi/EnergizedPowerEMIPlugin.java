69dd4c1fd9ff38357297b51f3a6cbdd30d413821
JDDev0 <jddev0@dorneger.at>
2023-06-17 09:49:22 +0200

    Add EMI integration

---
+++ src/main/java/me/jddev0/ep/integration/emi/EnergizedPowerEMIPlugin.java
@@ -0,0 +1,102 @@
+package me.jddev0.ep.integration.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
+import dev.emi.emi.api.stack.EmiStack;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.item.ModItems;
+import me.jddev0.ep.recipe.*;
+import me.jddev0.ep.screen.ModMenuTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraftforge.common.Tags;
+
+@EmiEntrypoint
+public class EnergizedPowerEMIPlugin implements EmiPlugin {
+
+    @Override
+    public void register(EmiRegistry registry) {
+        registerCategories(registry);
+
+        registerRecipes(registry);
+
+        registerRecipeHandlers(registry);
+    }
+
+    private void registerCategories(EmiRegistry registry) {
+        registry.addWorkstation(VanillaEmiRecipeCategories.CRAFTING, EmiStack.of(ModBlocks.AUTO_CRAFTER_ITEM.get()));
+        registry.addWorkstation(VanillaEmiRecipeCategories.SMELTING, EmiStack.of(ModBlocks.POWERED_FURNACE_ITEM.get()));
+
+        registry.addCategory(ChargerEMIRecipe.CATEGORY);
+        registry.addWorkstation(ChargerEMIRecipe.CATEGORY, ChargerEMIRecipe.ITEM);
+
+        registry.addCategory(CrusherEMIRecipe.CATEGORY);
+        registry.addWorkstation(CrusherEMIRecipe.CATEGORY, CrusherEMIRecipe.ITEM);
+
+        registry.addCategory(SawmillEMIRecipe.CATEGORY);
+        registry.addWorkstation(SawmillEMIRecipe.CATEGORY, SawmillEMIRecipe.ITEM);
+
+        registry.addCategory(CompressorEMIRecipe.CATEGORY);
+        registry.addWorkstation(CompressorEMIRecipe.CATEGORY, CompressorEMIRecipe.ITEM);
+
+        registry.addCategory(PlantGrowthChamberEMIRecipe.CATEGORY);
+        registry.addWorkstation(PlantGrowthChamberEMIRecipe.CATEGORY, PlantGrowthChamberEMIRecipe.ITEM);
+
+        registry.addCategory(PlantGrowthChamberFertilizerEMIRecipe.CATEGORY);
+        registry.addWorkstation(PlantGrowthChamberFertilizerEMIRecipe.CATEGORY, PlantGrowthChamberFertilizerEMIRecipe.ITEM);
+
+        registry.addCategory(EnergizerEMIRecipe.CATEGORY);
+        registry.addWorkstation(EnergizerEMIRecipe.CATEGORY, EnergizerEMIRecipe.ITEM);
+
+        registry.addCategory(DispenserEMIRecipe.CATEGORY);
+        registry.addWorkstation(DispenserEMIRecipe.CATEGORY, DispenserEMIRecipe.ITEM);
+
+        registry.addCategory(InWorldEMIRecipe.CATEGORY);
+        registry.addWorkstation(InWorldEMIRecipe.CATEGORY, InWorldEMIRecipe.ITEM);
+    }
+
+    private void registerRecipes(EmiRegistry registry) {
+        RecipeManager recipeManager = registry.getRecipeManager();
+
+        for(ChargerRecipe recipe:recipeManager.getAllRecipesFor(ChargerRecipe.Type.INSTANCE))
+            registry.addRecipe(new ChargerEMIRecipe(recipe));
+
+        for(CrusherRecipe recipe:recipeManager.getAllRecipesFor(CrusherRecipe.Type.INSTANCE))
+            registry.addRecipe(new CrusherEMIRecipe(recipe));
+
+        for(SawmillRecipe recipe:recipeManager.getAllRecipesFor(SawmillRecipe.Type.INSTANCE))
+            registry.addRecipe(new SawmillEMIRecipe(recipe));
+
+        for(CompressorRecipe recipe:recipeManager.getAllRecipesFor(CompressorRecipe.Type.INSTANCE))
+            registry.addRecipe(new CompressorEMIRecipe(recipe));
+
+        for(PlantGrowthChamberRecipe recipe:recipeManager.getAllRecipesFor(PlantGrowthChamberRecipe.Type.INSTANCE))
+            registry.addRecipe(new PlantGrowthChamberEMIRecipe(recipe));
+
+        for(PlantGrowthChamberFertilizerRecipe recipe:recipeManager.getAllRecipesFor(PlantGrowthChamberFertilizerRecipe.Type.INSTANCE))
+            registry.addRecipe(new PlantGrowthChamberFertilizerEMIRecipe(recipe));
+
+        for(EnergizerRecipe recipe:recipeManager.getAllRecipesFor(EnergizerRecipe.Type.INSTANCE))
+            registry.addRecipe(new EnergizerEMIRecipe(recipe));
+
+        registry.addRecipe(new DispenserEMIRecipe(new DispenserEMIRecipe.DispenserRecipe(
+                new ResourceLocation(EnergizedPowerMod.MODID, "dispenser/energizedpower/cable_insulator"),
+                Ingredient.of(Tags.Items.SHEARS), Ingredient.of(ItemTags.WOOL),
+                new ItemStack(ModItems.CABLE_INSULATOR.get(), 18))));
+
+        registry.addRecipe(new InWorldEMIRecipe(new InWorldEMIRecipe.InWorldRecipe(
+                new ResourceLocation(EnergizedPowerMod.MODID, "in_world_crafting/energizedpower/cable_insulator"),
+                Ingredient.of(Tags.Items.SHEARS), Ingredient.of(ItemTags.WOOL),
+                new ItemStack(ModItems.CABLE_INSULATOR.get(), 18))));
+    }
+
+    private void registerRecipeHandlers(EmiRegistry registry) {
+        registry.addRecipeHandler(ModMenuTypes.AUTO_CRAFTER_MENU.get(), new AutoCrafterRecipeHandler());
+    }
+}
---
+++ src/main/java/me/jddev0/ep/integration/emi/PlantGrowthChamberEMIRecipe.java
@@ -0,0 +1,94 @@
+package me.jddev0.ep.integration.emi;
+
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.WidgetHolder;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.recipe.PlantGrowthChamberRecipe;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlantGrowthChamberEMIRecipe implements EmiRecipe {
+    public static final ResourceLocation SIMPLIFIED_TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/block/plant_growth_chamber_front.png");
+    public static final EmiStack ITEM = EmiStack.of(ModBlocks.PLANT_GROWTH_CHAMBER_ITEM.get());
+    public static final EmiRecipeCategory CATEGORY = new EmiRecipeCategory(new ResourceLocation(EnergizedPowerMod.MODID, "plant_growth_chamber"),
+            ITEM, new EmiTexture(SIMPLIFIED_TEXTURE, 0, 0, 16, 16, 16, 16, 16, 16));
+
+    private final ResourceLocation id;
+    private final List<EmiIngredient> input;
+    private final List<EmiStack> output;
+    private final int ticks;
+
+    public PlantGrowthChamberEMIRecipe(PlantGrowthChamberRecipe recipe) {
+        this.id = recipe.getId();
+        this.input = List.of(EmiIngredient.of(recipe.getInput()));
+        this.output = Arrays.stream(recipe.getMaxOutputCounts()).map(EmiStack::of).toList();
+        this.ticks = recipe.getTicks();
+    }
+
+    @Override
+    public EmiRecipeCategory getCategory() {
+        return CATEGORY;
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public List<EmiIngredient> getInputs() {
+        return input;
+    }
+
+    @Override
+    public List<EmiStack> getOutputs() {
+        return output;
+    }
+
+    @Override
+    public int getDisplayWidth() {
+        return 108;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 48;
+    }
+
+    @Override
+    public void addWidgets(WidgetHolder widgets) {
+        ResourceLocation texture = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/plant_growth_chamber.png");
+        widgets.addTexture(texture, 0, 0, 108, 48, 61, 25);
+
+        widgets.addSlot(input.get(0), 0, 9).drawBack(false);
+
+        List<List<EmiStack>> outputSlotEntries = new ArrayList<>(4);
+        for(int i = 0;i < 4;i++)
+            outputSlotEntries.add(new LinkedList<>());
+
+        for(int i = 0;i < output.size();i++)
+            outputSlotEntries.get(i % 4).add(output.get(i));
+
+        widgets.addSlot(EmiIngredient.of(outputSlotEntries.get(0)), 72, 0).drawBack(false).recipeContext(this);
+        widgets.addSlot(EmiIngredient.of(outputSlotEntries.get(1)), 90, 0).drawBack(false).recipeContext(this);
+        widgets.addSlot(EmiIngredient.of(outputSlotEntries.get(2)), 72, 18).drawBack(false).recipeContext(this);
+        widgets.addSlot(EmiIngredient.of(outputSlotEntries.get(3)), 90, 18).drawBack(false).recipeContext(this);
+
+        Component ticksText = Component.translatable("recipes.energizedpower.plant_growth_chamber.ticks", ticks);
+        widgets.addText(ticksText.getVisualOrderText(),
+                widgets.getWidth() - Minecraft.getInstance().font.width(ticksText),
+                widgets.getHeight() - Minecraft.getInstance().font.lineHeight, ChatFormatting.WHITE.getColor(), false);
+    }
+}
