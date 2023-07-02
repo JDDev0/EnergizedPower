e01f07c2cb29e34c531d4133945bbdc7609cbef2
JDDev0 <jddev0@dorneger.at>
2023-07-02 13:20:19 +0200

    Improve Auto Crafter: Allow selection of recipes with same input items when a recipe viewer mod is used

--- src/main/java/me/jddev0/ep/integration/rei/AutoCrafterTransferHandler.java
+++ src/main/java/me/jddev0/ep/integration/rei/AutoCrafterTransferHandler.java
@@ -56,7 +56,7 @@ public class AutoCrafterTransferHandler implements TransferHandler {
             }
         }
 
-        ModMessages.sendToServer(new SetAutoCrafterPatternInputSlotsC2SPacket(container.getBlockEntity().getBlockPos(), itemStacks));
+        ModMessages.sendToServer(new SetAutoCrafterPatternInputSlotsC2SPacket(container.getBlockEntity().getBlockPos(), itemStacks, recipe.getId()));
 
         return Result.createSuccessful().blocksFurtherHandling();
     }
