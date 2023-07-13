e5405da3239b703df3a7e448de1bdd88d1a5ecd7
JDDev0 <jddev0@dorneger.at>
2023-07-13 17:43:39 +0200

    Add Item Tooltip text to the Battery Box Minecart and the Advanced Battery Box Minecart
    (cherry picked from commit 8200d12551b9d19b3f0a5d8307bc98d8517a0faf)

--- src/main/java/me/jddev0/ep/item/BatteryBoxMinecartItem.java
+++ src/main/java/me/jddev0/ep/item/BatteryBoxMinecartItem.java
@@ -1,14 +1,19 @@
 package me.jddev0.ep.item;
 
 import me.jddev0.ep.entity.MinecartBatteryBox;
+import me.jddev0.ep.util.EnergyUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.network.chat.Component;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseRailBlock;
@@ -16,6 +21,9 @@ import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.RailShape;
 import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BatteryBoxMinecartItem extends Item {
     private static final DefaultDispenseItemBehavior DISPENSE_ITEM_BEHAVIOR = new DefaultDispenseItemBehavior() {
@@ -64,6 +72,20 @@ public class BatteryBoxMinecartItem extends Item {
         super(props);
 
         DispenserBlock.registerBehavior(this, DISPENSE_ITEM_BEHAVIOR);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.translatable("tooltip.energizedpower.capacity.txt",
+                            EnergyUtils.getEnergyWithPrefix(MinecartBatteryBox.CAPACITY)).
+                    withStyle(ChatFormatting.GRAY));
+            components.add(Component.translatable("tooltip.energizedpower.transfer_rate.txt",
+                            EnergyUtils.getEnergyWithPrefix(MinecartBatteryBox.MAX_TRANSFER)).
+                    withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+        }else {
+            components.add(Component.translatable("tooltip.energizedpower.shift_details.txt").withStyle(ChatFormatting.YELLOW));
+        }
     }
 
     @Override
