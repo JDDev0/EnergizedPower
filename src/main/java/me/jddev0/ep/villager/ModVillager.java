c94c0ebef69da80b42272cccad7d43e5ab2bc2ef
JDDev0 <jddev0@dorneger.at>
2023-07-23 14:54:35 +0200

    Move Energized Power Book trade to the Electrician villager

--- src/main/java/me/jddev0/ep/villager/ModVillager.java
+++ src/main/java/me/jddev0/ep/villager/ModVillager.java
@@ -46,15 +46,13 @@ public final class ModVillager {
     }
 
     private static void registerTrades() {
-        TradeOfferHelper.registerVillagerOffers(VillagerProfession.LIBRARIAN, 1, factories -> {
+        TradeOfferHelper.registerVillagerOffers(ELECTRICIAN_PROFESSION, 1, factories -> {
             addOffer(factories,
                     new ItemStack(Items.EMERALD, 6),
                     new ItemStack(Items.BOOK),
                     new ItemStack(ModItems.ENERGIZED_POWER_BOOK),
                     3, 3, .02f);
-        });
 
-        TradeOfferHelper.registerVillagerOffers(ELECTRICIAN_PROFESSION, 1, factories -> {
             addOffer(factories,
                     new ItemStack(Items.COPPER_INGOT, 2),
                     new ItemStack(Items.EMERALD, 1),
