3ae5b54a424e17a62aad1de45a7d13e6846f2cf2
JDDev0 <jddev0@dorneger.at>
2023-07-23 22:11:44 +0200

    Add Advanced Battery Box Minecart

---
+++ src/main/java/me/jddev0/ep/screen/MinecartAdvancedBatteryBoxMenu.java
@@ -0,0 +1,73 @@
+package me.jddev0.ep.screen;
+
+import me.jddev0.ep.util.ByteUtils;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.inventory.SimpleContainerData;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class MinecartAdvancedBatteryBoxMenu extends AbstractContainerMenu implements EnergyStorageMenu {
+    private final Container container;
+    private final Level level;
+    private final ContainerData data;
+
+    public MinecartAdvancedBatteryBoxMenu(int id, Inventory inv, FriendlyByteBuf buffer) {
+        this(id, inv, new SimpleContainer(0), new SimpleContainerData(4));
+    }
+
+    public MinecartAdvancedBatteryBoxMenu(int id, Inventory inv, Container container, ContainerData data) {
+        super(ModMenuTypes.MINECART_ADVANCED_BATTERY_BOX_MENU.get(), id);
+
+        checkContainerSize(inv, 0);
+        checkContainerDataCount(data, 4);
+        this.container = container;
+        this.level = inv.player.level;
+        this.data = data;
+
+        addPlayerInventory(inv);
+        addPlayerHotbar(inv);
+
+        addDataSlots(this.data);
+    }
+
+    @Override
+    public int getEnergy() {
+        return ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1));
+    }
+
+    @Override
+    public int getCapacity() {
+        return ByteUtils.from2ByteChunks((short)data.get(2), (short)data.get(3));
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return container.stillValid(player);
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for(int i = 0;i < 3;i++) {
+            for(int j = 0;j < 9;j++) {
+                addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for(int i = 0;i < 9;i++) {
+            addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+    }
+}
