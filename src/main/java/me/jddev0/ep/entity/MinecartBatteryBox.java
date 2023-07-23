79d6c0d4bea5e45bf9face12dd2573a4459fca31
JDDev0 <jddev0@dorneger.at>
2023-07-23 22:09:02 +0200

    Add Abstract super class for MinecartBatteryBox

--- src/main/java/me/jddev0/ep/entity/MinecartBatteryBox.java
+++ src/main/java/me/jddev0/ep/entity/MinecartBatteryBox.java
@@ -4,18 +4,12 @@ import me.jddev0.ep.block.ModBlocks;
 import me.jddev0.ep.item.ModItems;
 import me.jddev0.ep.screen.MinecartBatteryBoxMenu;
 import me.jddev0.ep.util.ByteUtils;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.Container;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.Item;
@@ -24,7 +18,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class MinecartBatteryBox extends AbstractMinecart implements Container, MenuProvider {
+public class MinecartBatteryBox extends AbstractMinecartBatteryBox {
     public static final int CAPACITY = 65536;
     public static final int MAX_TRANSFER = 512;
 
@@ -36,7 +30,7 @@ public class MinecartBatteryBox extends AbstractMinecart implements Container, M
         public int get(int index) {
             return switch(index) {
                 case 0, 1 -> ByteUtils.get2Bytes(MinecartBatteryBox.this.getEnergy(), index);
-                case 2, 3 -> ByteUtils.get2Bytes(CAPACITY, index);
+                case 2, 3 -> ByteUtils.get2Bytes(MinecartBatteryBox.this.getCapacity(), index);
                 default -> 0;
             };
         }
@@ -70,11 +64,6 @@ public class MinecartBatteryBox extends AbstractMinecart implements Container, M
         return ModItems.BATTERY_BOX_MINECART.get();
     }
 
-    @Override
-    public Type getMinecartType() {
-        return Type.CHEST;
-    }
-
     public BlockState getDefaultDisplayBlockState() {
         return ModBlocks.BATTERY_BOX.get().defaultBlockState();
     }
@@ -86,17 +75,6 @@ public class MinecartBatteryBox extends AbstractMinecart implements Container, M
     }
 
     @Override
-    public InteractionResult interact(Player player, InteractionHand interactionHand) {
-        player.openMenu(this);
-        return InteractionResult.SUCCESS;
-    }
-
-    @Override
-    public boolean stillValid(Player player) {
-        return !isRemoved() && position().closerThan(player.position(), 8.);
-    }
-
-    @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
 
@@ -104,65 +82,23 @@ public class MinecartBatteryBox extends AbstractMinecart implements Container, M
     }
 
     @Override
-    public int getContainerSize() {
-        return 0;
+    public int getCapacity() {
+        return CAPACITY;
     }
 
     @Override
-    public boolean isEmpty() {
-        return true;
+    public int getTransferRate() {
+        return MAX_TRANSFER;
     }
 
     @Override
-    public ItemStack getItem(int slot) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public ItemStack removeItem(int slot, int amount) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public ItemStack removeItemNoUpdate(int slot) {
-        return ItemStack.EMPTY;
-    }
-
-    @Override
-    public void setItem(int slot, ItemStack stack) {
-
-    }
-
-    @Override
-    public void setChanged() {
-
-    }
-
-    @Override
-    public void clearContent() {
-
-    }
-
     public int getEnergy() {
         return entityData.get(DATA_ID_ENERGY);
     }
 
+    @Override
     public void setEnergy(int energy) {
         entityData.set(DATA_ID_ENERGY, energy);
-    }
-
-    @Override
-    protected void addAdditionalSaveData(CompoundTag nbt) {
-        nbt.putInt("energy", getEnergy());
-
-        super.addAdditionalSaveData(nbt);
-    }
-
-    @Override
-    protected void readAdditionalSaveData(CompoundTag nbt) {
-        super.readAdditionalSaveData(nbt);
-
-        setEnergy(nbt.getInt("energy"));
     }
 
     @Override
