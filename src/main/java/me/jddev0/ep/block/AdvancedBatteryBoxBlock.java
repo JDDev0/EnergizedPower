742a4ded1b579aa9fd70e28745c1cd6781e59183
JDDev0 <jddev0@dorneger.at>
2023-07-23 22:06:45 +0200

    Add Item Tooltip text to the Battery Box and the Advanced Battery Box

--- src/main/java/me/jddev0/ep/block/AdvancedBatteryBoxBlock.java
+++ src/main/java/me/jddev0/ep/block/AdvancedBatteryBoxBlock.java
@@ -2,13 +2,21 @@ package me.jddev0.ep.block;
 
 import me.jddev0.ep.block.entity.AdvancedBatteryBoxBlockEntity;
 import me.jddev0.ep.block.entity.ModBlockEntities;
+import me.jddev0.ep.util.EnergyUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -17,6 +25,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class AdvancedBatteryBoxBlock extends BaseEntityBlock {
     public AdvancedBatteryBoxBlock(Properties props) {
@@ -52,5 +62,25 @@ public class AdvancedBatteryBoxBlock extends BaseEntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
         return createTickerHelper(type, ModBlockEntities.ADVANCED_BATTERY_BOX_ENTITY.get(), AdvancedBatteryBoxBlockEntity::tick);
+    }
+
+    public static class Item extends BlockItem {
+        public Item(Block block, Properties props) {
+            super(block, props);
+        }
+
+        @Override
+        public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+            if(Screen.hasShiftDown()) {
+                components.add(Component.translatable("tooltip.energizedpower.capacity.txt",
+                                EnergyUtils.getEnergyWithPrefix(AdvancedBatteryBoxBlockEntity.CAPACITY)).
+                        withStyle(ChatFormatting.GRAY));
+                components.add(Component.translatable("tooltip.energizedpower.transfer_rate.txt",
+                                EnergyUtils.getEnergyWithPrefix(AdvancedBatteryBoxBlockEntity.MAX_TRANSFER)).
+                        withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+            }else {
+                components.add(Component.translatable("tooltip.energizedpower.shift_details.txt").withStyle(ChatFormatting.YELLOW));
+            }
+        }
     }
 }
