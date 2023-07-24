a05e50d9e57bc2250d96ce8cbb3d4135a54e67e1
JDDev0 <jddev0@dorneger.at>
2023-07-24 17:21:04 +0200

    Add Item Tooltip text to the Battery Box and the Advanced Battery Box

--- src/main/java/me/jddev0/ep/block/BatteryBoxBlock.java
+++ src/main/java/me/jddev0/ep/block/BatteryBoxBlock.java
@@ -2,20 +2,31 @@ package me.jddev0.ep.block;
 
 import me.jddev0.ep.block.entity.BatteryBoxBlockEntity;
 import me.jddev0.ep.block.entity.ModBlockEntities;
+import me.jddev0.ep.util.EnergyUtils;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BatteryBoxBlock extends BlockWithEntity {
     public BatteryBoxBlock(FabricBlockSettings props) {
@@ -51,5 +62,25 @@ public class BatteryBoxBlock extends BlockWithEntity {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World level, BlockState state, BlockEntityType<T> type) {
         return checkType(type, ModBlockEntities.BATTERY_BOX_ENTITY, BatteryBoxBlockEntity::tick);
+    }
+
+    public static class Item extends BlockItem {
+        public Item(Block block, FabricItemSettings props) {
+            super(block, props);
+        }
+
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            if(Screen.hasShiftDown()) {
+                tooltip.add(Text.translatable("tooltip.energizedpower.capacity.txt",
+                                EnergyUtils.getEnergyWithPrefix(BatteryBoxBlockEntity.CAPACITY)).
+                        formatted(Formatting.GRAY));
+                tooltip.add(Text.translatable("tooltip.energizedpower.transfer_rate.txt",
+                                EnergyUtils.getEnergyWithPrefix(BatteryBoxBlockEntity.MAX_TRANSFER)).
+                        formatted(Formatting.GRAY, Formatting.ITALIC));
+            }else {
+                tooltip.add(Text.translatable("tooltip.energizedpower.shift_details.txt").formatted(Formatting.YELLOW));
+            }
+        }
     }
 }
