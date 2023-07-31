ae8be7b663f189e4f11b96a1698e56c5484e8bfe
JDDev0 <jddev0@dorneger.at>
2023-07-31 17:05:47 +0200

    Add Advanced Minecart Charger

---
+++ src/main/java/me/jddev0/ep/block/AdvancedMinecartChargerBlock.java
@@ -0,0 +1,100 @@
+package me.jddev0.ep.block;
+
+import me.jddev0.ep.block.entity.AdvancedMinecartChargerBlockEntity;
+import me.jddev0.ep.block.entity.ModBlockEntities;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class AdvancedMinecartChargerBlock extends BlockWithEntity {
+    public static final DirectionProperty FACING = Properties.FACING;
+
+    protected AdvancedMinecartChargerBlock(FabricBlockSettings props) {
+        super(props);
+
+        this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos blockPos, BlockState state) {
+        return new AdvancedMinecartChargerBlockEntity(blockPos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World level, BlockPos blockPos) {
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if(!(blockEntity instanceof AdvancedMinecartChargerBlockEntity advancedMinecartChargerBlockEntity))
+            return super.getComparatorOutput(state, level, blockPos);
+
+        return advancedMinecartChargerBlockEntity.getRedstoneOutput();
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World level, BlockPos blockPos, PlayerEntity player, Hand handItem, BlockHitResult hit) {
+        if(level.isClient())
+            return ActionResult.SUCCESS;
+
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if(!(blockEntity instanceof AdvancedMinecartChargerBlockEntity))
+            throw new IllegalStateException("Container is invalid");
+
+        player.openHandledScreen((AdvancedMinecartChargerBlockEntity)blockEntity);
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState().with(FACING, context.getPlayerLookDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(FACING);
+    }
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World level, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, ModBlockEntities.ADVANCED_MINECART_CHARGER_ENTITY, AdvancedMinecartChargerBlockEntity::tick);
+    }
+}
---
+++ src/main/java/me/jddev0/ep/screen/AdvancedMinecartChargerMenu.java
@@ -0,0 +1,72 @@
+package me.jddev0.ep.screen;
+
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.block.entity.AdvancedMinecartChargerBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.world.World;
+
+public class AdvancedMinecartChargerMenu extends ScreenHandler implements EnergyStorageMenu {
+    private final AdvancedMinecartChargerBlockEntity blockEntity;
+    private final World level;
+
+    public AdvancedMinecartChargerMenu(int id, PlayerInventory inv, PacketByteBuf buf) {
+        this(id, inv.player.getWorld().getBlockEntity(buf.readBlockPos()), inv);
+    }
+
+    public AdvancedMinecartChargerMenu(int id, BlockEntity blockEntity, PlayerInventory playerInventory) {
+        super(ModMenuTypes.ADVANCED_MINECART_CHARGER_MENU, id);
+
+        this.blockEntity = (AdvancedMinecartChargerBlockEntity)blockEntity;
+
+        this.level = playerInventory.player.getWorld();
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+    }
+
+    @Override
+    public long getEnergy() {
+        return blockEntity.getEnergy();
+    }
+
+    @Override
+    public long getCapacity() {
+        return blockEntity.getCapacity();
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int index) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return canUse(ScreenHandlerContext.create(level, blockEntity.getPos()), player, ModBlocks.ADVANCED_MINECART_CHARGER);
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for(int i = 0;i < 3;i++) {
+            for(int j = 0;j < 9;j++) {
+                addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for(int i = 0;i < 9;i++) {
+            addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+    }
+
+    public BlockEntity getBlockEntity() {
+        return blockEntity;
+    }
+}
