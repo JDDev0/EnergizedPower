2aadccc0d597961ef313d1c6356bdf3505adbe1b
JDDev0 <jddev0@dorneger.at>
2023-08-06 10:10:10 +0200

    Add validation comments to config files
    (cherry picked from commit 4a53584bab204cde9ed0115e2ea721a9ec52b89b)

--- src/main/java/me/jddev0/ep/config/validation/ValueValidator.java
+++ src/main/java/me/jddev0/ep/config/validation/ValueValidator.java
@@ -3,19 +3,37 @@ package me.jddev0.ep.config.validation;
 import me.jddev0.ep.config.ConfigValidationException;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @FunctionalInterface
 public interface ValueValidator<T> {
     void validate(@NotNull T value) throws ConfigValidationException;
+
+    default @NotNull List<String> getValidationCommentLines() {
+        return new ArrayList<>();
+    }
 
     /**
      * @param validator The validator to be executed before this validator
      * @return The combined validator
      */
     default @NotNull ValueValidator<? super T> validateBefore(@NotNull ValueValidator<? super T> validator) {
-        return value -> {
-            validator.validate(value);
+        return new ValueValidator<>() {
+            @Override
+            public void validate(@NotNull T value) throws ConfigValidationException {
+                validator.validate(value);
 
-            validate(value);
+                ValueValidator.this.validate(value);
+            }
+
+            @Override
+            public @NotNull List<String> getValidationCommentLines() {
+                List<String> commentLines = new ArrayList<>(validator.getValidationCommentLines());
+                commentLines.addAll(ValueValidator.this.getValidationCommentLines());
+
+                return commentLines;
+            }
         };
     }
 
@@ -24,10 +42,21 @@ public interface ValueValidator<T> {
      * @return The combined validator
      */
     default @NotNull ValueValidator<? super T> validateAfter(@NotNull ValueValidator<? super T> validator) {
-        return value -> {
-            validate(value);
+        return new ValueValidator<>() {
+            @Override
+            public void validate(@NotNull T value) throws ConfigValidationException {
+                ValueValidator.this.validate(value);
 
-            validator.validate(value);
+                validator.validate(value);
+            }
+
+            @Override
+            public @NotNull List<String> getValidationCommentLines() {
+                List<String> commentLines = new ArrayList<>(ValueValidator.this.getValidationCommentLines());
+                commentLines.addAll(validator.getValidationCommentLines());
+
+                return commentLines;
+            }
         };
     }
 }
