4a53584bab204cde9ed0115e2ea721a9ec52b89b
JDDev0 <jddev0@dorneger.at>
2023-08-06 10:10:10 +0200

    Add validation comments to config files

--- src/main/java/me/jddev0/ep/config/validation/Validators.java
+++ src/main/java/me/jddev0/ep/config/validation/Validators.java
@@ -1,15 +1,34 @@
 package me.jddev0.ep.config.validation;
 
 import me.jddev0.ep.config.ConfigValidationException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public final class Validators {
-    public static final ValueValidator<String> STRING_NOT_EMPTY = value -> {
-        if(value.isEmpty())
-            throw new ConfigValidationException("Value must not be empty");
+    public static final ValueValidator<String> STRING_NOT_EMPTY = new ValueValidator<>() {
+        @Override
+        public void validate(@NotNull String value) throws ConfigValidationException {
+            if(value.isEmpty())
+                throw new ConfigValidationException("Value must not be empty");
+        }
+
+        @Override
+        public @NotNull List<String> getValidationCommentLines() {
+            return List.of("Value must not be empty");
+        }
     };
-    public static final ValueValidator<String> STRING_NOT_BLANK = value -> {
-        if(value.isBlank())
-            throw new ConfigValidationException("Value must not be blank");
+    public static final ValueValidator<String> STRING_NOT_BLANK = new ValueValidator<>() {
+        @Override
+        public void validate(@NotNull String value) throws ConfigValidationException {
+            if(value.isEmpty())
+                throw new ConfigValidationException("Value must not be blank");
+        }
+
+        @Override
+        public @NotNull List<String> getValidationCommentLines() {
+            return List.of("Value must not be blank");
+        }
     };
 
     private Validators() {}
