dc727f2b4bd64769a0d3ad503e9ee1a92bb43e95
JDDev0 <jddev0@dorneger.at>
2023-08-06 19:44:44 +0200

    Add configuration values for capacity, transfer rate, energy production multiplier of the coal engine
    (cherry picked from commit d95e0f4abf7126542150531d140410f006c802e8)

--- src/main/java/me/jddev0/ep/config/ModConfigs.java
+++ src/main/java/me/jddev0/ep/config/ModConfigs.java
@@ -190,6 +190,16 @@ public final class ModConfigs {
             "block.time_controller", "Time Controller", 32768
     );
 
+    public static final ConfigValue<Integer> COMMON_COAL_ENGINE_CAPACITY = registerEnergyCapacityConfigValue(
+            "block.coal_engine", "Coal Engine", 2048
+    );
+    public static final ConfigValue<Integer> COMMON_COAL_ENGINE_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "block.coal_engine", "Coal Engine", 256
+    );
+    public static final ConfigValue<Float> COMMON_COAL_ENGINE_ENERGY_PRODUCTION_MULTIPLIER = registerEnergyProductionMultiplierConfigValue(
+            "block.coal_engine", "Coal Engine"
+    );
+
     //Entities
     public static final ConfigValue<Integer> COMMON_BATTERY_BOX_MINECART_CAPACITY = registerEnergyCapacityConfigValue(
             "entity.battery_box_minecart", "Battery Box Minecart", 65536
