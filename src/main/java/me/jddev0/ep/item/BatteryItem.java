eaa447ab1f2552bd1f3dd7a5df4312bfdc58e5b1
JDDev0 <jddev0@dorneger.at>
2023-08-06 10:38:45 +0200

    Add configuration values for capacity and transfer rate of batteries
    (cherry picked from commit b5b04d02562540940314d08769fa07ca88c8199a)

--- src/main/java/me/jddev0/ep/item/BatteryItem.java
+++ src/main/java/me/jddev0/ep/item/BatteryItem.java
@@ -1,5 +1,6 @@
 package me.jddev0.ep.item;
 
+import me.jddev0.ep.config.ModConfigs;
 import me.jddev0.ep.energy.ReceiveAndExtractEnergyStorage;
 import me.jddev0.ep.item.energy.EnergizedPowerEnergyItem;
 import me.jddev0.ep.util.EnergyUtils;
@@ -37,14 +38,14 @@ public class BatteryItem extends EnergizedPowerEnergyItem {
     }
 
     public enum Tier {
-        BATTERY_1("battery_1", 256, 2),
-        BATTERY_2("battery_2", 1024, 8),
-        BATTERY_3("battery_3", 2048, 32),
-        BATTERY_4("battery_4", 8192, 128),
-        BATTERY_5("battery_5", 16384, 512),
-        BATTERY_6("battery_6", 65536, 2048),
-        BATTERY_7("battery_7", 262144, 8192),
-        BATTERY_8("battery_8", 1048576, 32768);
+        BATTERY_1("battery_1", ModConfigs.COMMON_BATTERY_1_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_1_TRANSFER_RATE.getValue()),
+        BATTERY_2("battery_2", ModConfigs.COMMON_BATTERY_2_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_2_TRANSFER_RATE.getValue()),
+        BATTERY_3("battery_3", ModConfigs.COMMON_BATTERY_3_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_3_TRANSFER_RATE.getValue()),
+        BATTERY_4("battery_4", ModConfigs.COMMON_BATTERY_4_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_4_TRANSFER_RATE.getValue()),
+        BATTERY_5("battery_5", ModConfigs.COMMON_BATTERY_5_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_5_TRANSFER_RATE.getValue()),
+        BATTERY_6("battery_6", ModConfigs.COMMON_BATTERY_6_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_6_TRANSFER_RATE.getValue()),
+        BATTERY_7("battery_7", ModConfigs.COMMON_BATTERY_7_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_7_TRANSFER_RATE.getValue()),
+        BATTERY_8("battery_8", ModConfigs.COMMON_BATTERY_8_CAPACITY.getValue(), ModConfigs.COMMON_BATTERY_8_TRANSFER_RATE.getValue());
 
         private final String resourceId;
         private final int capacity;
