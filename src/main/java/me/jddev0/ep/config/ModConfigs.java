fd427be8053bdd72e58898211a8d2d5a85cf4c73
JDDev0 <jddev0@dorneger.at>
2023-08-07 20:09:41 +0200

    Fix Heat Generator default values for capacity and transfer rate
    (cherry picked from commit 8ee0e3b8eefea0771aa693f3c4505dba327d353f)

--- src/main/java/me/jddev0/ep/config/ModConfigs.java
+++ src/main/java/me/jddev0/ep/config/ModConfigs.java
@@ -347,10 +347,10 @@ public final class ModConfigs {
     );
 
     public static final ConfigValue<Integer> COMMON_HEAT_GENERATOR_CAPACITY = registerEnergyCapacityConfigValue(
-            "block.heat_generator", "Heat Generator", 2048
+            "block.heat_generator", "Heat Generator", 10000
     );
     public static final ConfigValue<Integer> COMMON_HEAT_GENERATOR_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
-            "block.heat_generator", "Heat Generator", 256
+            "block.heat_generator", "Heat Generator", 500
     );
     public static final ConfigValue<Float> COMMON_HEAT_GENERATOR_ENERGY_PRODUCTION_MULTIPLIER = registerEnergyProductionMultiplierConfigValue(
             "block.heat_generator", "Heat Generator"
