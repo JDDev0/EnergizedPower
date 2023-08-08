9377995285b7d25c976b05511b5c5bc720088575
JDDev0 <jddev0@dorneger.at>
2023-08-08 19:26:00 +0200

    Add configuration values for capacity, transfer rate, recipe energy consumption multiplier [Used for charger recipes, not for charging items] of the charger and the advanced charger
    (cherry picked from commit 517dcf80748ec189842a1d108dd21c63d5851338)

--- src/main/java/me/jddev0/ep/integration/jei/ChargerCategory.java
+++ src/main/java/me/jddev0/ep/integration/jei/ChargerCategory.java
@@ -3,6 +3,7 @@ package me.jddev0.ep.integration.jei;
 import com.mojang.blaze3d.vertex.PoseStack;
 import me.jddev0.ep.EnergizedPowerMod;
 import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.block.entity.ChargerBlockEntity;
 import me.jddev0.ep.recipe.ChargerRecipe;
 import me.jddev0.ep.util.EnergyUtils;
 import mezz.jei.api.constants.VanillaTypes;
@@ -65,7 +66,8 @@ public class ChargerCategory implements IRecipeCategory<ChargerRecipe> {
     @Override
     public void draw(ChargerRecipe recipe, IRecipeSlotsView iRecipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
         Font font = Minecraft.getInstance().font;
-        Component component = Component.literal(EnergyUtils.getEnergyWithPrefix(recipe.getEnergyConsumption())).withStyle(ChatFormatting.YELLOW);
+        int energyConsumption = (int)(recipe.getEnergyConsumption() *ChargerBlockEntity.CHARGER_RECIPE_ENERGY_CONSUMPTION_MULTIPLIER);
+        Component component = Component.literal(EnergyUtils.getEnergyWithPrefix(energyConsumption)).withStyle(ChatFormatting.YELLOW);
         int textWidth = font.width(component);
 
         Minecraft.getInstance().font.draw(matrixStack, component, 113 - textWidth, 38, 0xFFFFFFFF);
