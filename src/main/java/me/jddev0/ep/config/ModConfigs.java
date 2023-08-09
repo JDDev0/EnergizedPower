301d47b41b2d066439f68cc63cdeb3ccd4b171a1
JDDev0 <jddev0@dorneger.at>
2023-08-09 20:37:34 +0200

    Add configuration values for capacity and transfer rate of batteries

--- src/main/java/me/jddev0/ep/config/ModConfigs.java
+++ src/main/java/me/jddev0/ep/config/ModConfigs.java
@@ -1,6 +1,7 @@
 package me.jddev0.ep.config;
 
 import com.mojang.logging.LogUtils;
+import me.jddev0.ep.config.value.LongConfigValue;
 import net.fabricmc.loader.api.FabricLoader;
 import me.jddev0.ep.config.value.IntegerConfigValue;
 import org.slf4j.Logger;
@@ -14,12 +15,70 @@ public final class ModConfigs {
     private ModConfigs() {}
 
     public static final Config COMMON_CONFIG = new Config(getRelativeConfigFile("common.conf"), "Energized Power Common Config");
-    //TODO common config values
+
+    public static final ConfigValue<Long> COMMON_BATTERY_1_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_1", "Battery (Tier I)", 256
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_1_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_1", "Battery (Tier I)", 2
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_2_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_2", "Battery (Tier II)", 1024
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_2_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_2", "Battery (Tier II)", 8
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_3_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_3", "Battery (Tier III)", 2048
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_3_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_3", "Battery (Tier III)", 32
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_4_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_4", "Battery (Tier IV)", 8192
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_4_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_4", "Battery (Tier IV)", 128
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_5_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_5", "Battery (Tier V)", 16384
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_5_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_5", "Battery (Tier V)", 512
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_6_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_6", "Battery (Tier VI)", 65536
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_6_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_6", "Battery (Tier VI)", 2048
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_7_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_7", "Battery (Tier VII)", 262144
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_7_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_7", "Battery (Tier VII)", 8192
+    );
+
+    public static final ConfigValue<Long> COMMON_BATTERY_8_CAPACITY = registerEnergyCapacityConfigValue(
+            "item.battery_8", "Battery (Tier VIII)", 1048576
+    );
+    public static final ConfigValue<Long> COMMON_BATTERY_8_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
+            "item.battery_8", "Battery (Tier VIII)", 32768
+    );
+
 
     public static final Config SERVER_CONFIG = new Config(getRelativeConfigFile("server.conf"), "Energized Power Server Config");
     //TODO server config values
 
+
     public static final Config CLIENT_CONFIG = new Config(getRelativeConfigFile("client.conf"), "Energized Power Client Config");
+
     public static final ConfigValue<Integer> CLIENT_ENERGIZED_POWER_BOOK_IMAGE_CYCLE_DELAY = CLIENT_CONFIG.register(
             new IntegerConfigValue(
                     "energized_power_book.image_cycle_delay",
@@ -32,6 +91,23 @@ public final class ModConfigs {
 
     private static File getRelativeConfigFile(String fileName) {
         return FabricLoader.getInstance().getConfigDir().resolve("energizedpower/" + fileName).toFile();
+    }
+
+    private static ConfigValue<Long> registerEnergyCapacityConfigValue(String baseConfigKey, String itemName, long defaultValue) {
+        return COMMON_CONFIG.register(new LongConfigValue(
+                baseConfigKey + ".capacity",
+                "The energy capacity of " + itemName + " in F",
+                defaultValue,
+                1L, null
+        ));
+    }
+    private static ConfigValue<Long> registerEnergyTransferRateConfigValue(String baseConfigKey, String itemName, long defaultValue) {
+        return COMMON_CONFIG.register(new LongConfigValue(
+                baseConfigKey + ".transfer_rate",
+                "The energy transfer rate of " + itemName + " in F per tick",
+                defaultValue,
+                1L, null
+        ));
     }
 
     public static void registerConfigs(boolean isServer) {
