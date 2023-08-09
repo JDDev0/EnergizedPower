cfead7e6ad578e2036e3f9cb98b1b0eb0fe49ceb
JDDev0 <jddev0@dorneger.at>
2023-08-09 21:29:30 +0200

    Add configuration value for placement frequency of electrician village house

--- src/main/java/me/jddev0/ep/event/ServerStartingHandler.java
+++ src/main/java/me/jddev0/ep/event/ServerStartingHandler.java
@@ -3,6 +3,7 @@ package me.jddev0.ep.event;
 import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.config.ModConfigs;
 import me.jddev0.ep.mixin.world.village.StructurePoolElementGetterSetter;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
@@ -21,11 +22,12 @@ public class ServerStartingHandler implements ServerLifecycleEvents.ServerStarti
         Registry<StructurePool> templatePoolRegistry = server.getRegistryManager().get(Registry.STRUCTURE_POOL_KEY);
 
         //Electrician 1
-        addVillageHouse(templatePoolRegistry, "desert", "electrician_1", 5);
-        addVillageHouse(templatePoolRegistry, "plains", "electrician_1", 5);
-        addVillageHouse(templatePoolRegistry, "savanna", "electrician_1", 5);
-        addVillageHouse(templatePoolRegistry, "snowy", "electrician_1", 5);
-        addVillageHouse(templatePoolRegistry, "taiga", "electrician_1", 5);
+        int weight = ModConfigs.COMMON_ELECTRICIAN_BUILDING_PLACEMENT_WEIGHT.getValue();
+        addVillageHouse(templatePoolRegistry, "desert", "electrician_1", weight);
+        addVillageHouse(templatePoolRegistry, "plains", "electrician_1", weight);
+        addVillageHouse(templatePoolRegistry, "savanna", "electrician_1", weight);
+        addVillageHouse(templatePoolRegistry, "snowy", "electrician_1", weight);
+        addVillageHouse(templatePoolRegistry, "taiga", "electrician_1", weight);
     }
 
     private static void addVillageHouse(Registry<StructurePool> templatePoolRegistry, String villageType, String buildingName, int weight) {
