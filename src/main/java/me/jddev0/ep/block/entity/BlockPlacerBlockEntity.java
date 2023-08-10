fdaf5a0717dd5a983f332085b1561e8d54f6069d
JDDev0 <jddev0@dorneger.at>
2023-08-10 19:08:32 +0200

    Add configuration values for capacity, transfer rate, energy consumption per tick, placement duration of the block placer

--- src/main/java/me/jddev0/ep/block/entity/BlockPlacerBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/BlockPlacerBlockEntity.java
@@ -4,6 +4,7 @@ import me.jddev0.ep.block.BlockPlacerBlock;
 import me.jddev0.ep.block.entity.handler.CachedSidedInventoryStorage;
 import me.jddev0.ep.block.entity.handler.InputOutputItemHandler;
 import me.jddev0.ep.block.entity.handler.SidedInventoryWrapper;
+import me.jddev0.ep.config.ModConfigs;
 import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
 import me.jddev0.ep.networking.ModMessages;
 import me.jddev0.ep.screen.BlockPlacerMenu;
@@ -41,9 +42,9 @@ import team.reborn.energy.api.base.SimpleEnergyStorage;
 import java.util.stream.IntStream;
 
 public class BlockPlacerBlockEntity extends BlockEntity implements ExtendedScreenHandlerFactory, EnergyStoragePacketUpdate {
-    public static final long CAPACITY = 2048;
-    public static final long MAX_RECEIVE = 128;
-    private static final long ENERGY_USAGE_PER_TICK = 32;
+    public static final long CAPACITY = ModConfigs.COMMON_BLOCK_PLACER_CAPACITY.getValue();
+    public static final long MAX_RECEIVE = ModConfigs.COMMON_BLOCK_PLACER_TRANSFER_RATE.getValue();
+    private static final long ENERGY_USAGE_PER_TICK = ModConfigs.COMMON_BLOCK_PLACER_ENERGY_CONSUMPTION_PER_TICK.getValue();
 
     final CachedSidedInventoryStorage<BlockPlacerBlockEntity> cachedSidedInventoryStorage;
     final InputOutputItemHandler inventory;
@@ -54,7 +55,7 @@ public class BlockPlacerBlockEntity extends BlockEntity implements ExtendedScree
 
     protected final PropertyDelegate data;
     private int progress;
-    private int maxProgress = 20;
+    private int maxProgress = ModConfigs.COMMON_BLOCK_PLACER_PLACEMENT_DURATION.getValue();
     private long energyConsumptionLeft = -1;
     private boolean hasEnoughEnergy;
     private boolean inverseRotation;
@@ -138,11 +139,11 @@ public class BlockPlacerBlockEntity extends BlockEntity implements ExtendedScree
             @Override
             public int get(int index) {
                 return switch(index) {
-                    case 0 -> BlockPlacerBlockEntity.this.progress;
-                    case 1 -> BlockPlacerBlockEntity.this.maxProgress;
-                    case 2, 3, 4, 5 -> ByteUtils.get2Bytes(BlockPlacerBlockEntity.this.energyConsumptionLeft, index - 2);
-                    case 6 -> hasEnoughEnergy?1:0;
-                    case 7 -> inverseRotation?1:0;
+                    case 0, 1 -> ByteUtils.get2Bytes(BlockPlacerBlockEntity.this.progress, index);
+                    case 2, 3 -> ByteUtils.get2Bytes(BlockPlacerBlockEntity.this.maxProgress, index - 2);
+                    case 4, 5, 6, 7 -> ByteUtils.get2Bytes(BlockPlacerBlockEntity.this.energyConsumptionLeft, index - 4);
+                    case 8 -> hasEnoughEnergy?1:0;
+                    case 9 -> inverseRotation?1:0;
                     default -> 0;
                 };
             }
@@ -150,16 +151,20 @@ public class BlockPlacerBlockEntity extends BlockEntity implements ExtendedScree
             @Override
             public void set(int index, int value) {
                 switch(index) {
-                    case 0 -> BlockPlacerBlockEntity.this.progress = value;
-                    case 1 -> BlockPlacerBlockEntity.this.maxProgress = value;
-                    case 2, 3, 4, 5, 6 -> {}
-                    case 7 -> BlockPlacerBlockEntity.this.inverseRotation = value != 0;
+                    case 0, 1 -> BlockPlacerBlockEntity.this.progress = ByteUtils.with2Bytes(
+                            BlockPlacerBlockEntity.this.progress, (short)value, index
+                    );
+                    case 2, 3 -> BlockPlacerBlockEntity.this.maxProgress = ByteUtils.with2Bytes(
+                            BlockPlacerBlockEntity.this.maxProgress, (short)value, index - 2
+                    );
+                    case 4, 5, 6, 7, 8 -> {}
+                    case 9 -> BlockPlacerBlockEntity.this.inverseRotation = value != 0;
                 }
             }
 
             @Override
             public int size() {
-                return 8;
+                return 10;
             }
         };
     }
