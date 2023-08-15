6b16a893cc2a8bdce000d075fb46852120ef14fa
JDDev0 <jddev0@dorneger.at>
2023-08-15 13:50:19 +0200

    Add Pulverizer

---
+++ src/main/java/me/jddev0/ep/integration/emi/PulverizerEMIRecipe.java
@@ -0,0 +1,74 @@
+package me.jddev0.ep.integration.emi;
+
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.WidgetHolder;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.recipe.PulverizerRecipe;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PulverizerEMIRecipe implements EmiRecipe {
+    public static final ResourceLocation SIMPLIFIED_TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/block/pulverizer_side.png");
+    public static final EmiStack ITEM = EmiStack.of(ModBlocks.PULVERIZER_ITEM.get());
+    public static final EmiRecipeCategory CATEGORY = new EmiRecipeCategory(new ResourceLocation(EnergizedPowerMod.MODID, "pulverizer"),
+            ITEM, new EmiTexture(SIMPLIFIED_TEXTURE, 0, 0, 16, 16, 16, 16, 16, 16));
+
+    private final ResourceLocation id;
+    private final List<EmiIngredient> input;
+    private final List<EmiStack> output;
+
+    public PulverizerEMIRecipe(PulverizerRecipe recipe) {
+        this.id = recipe.getId();
+        this.input = List.of(EmiIngredient.of(recipe.getInput()));
+
+        this.output = Arrays.stream(recipe.getMaxOutputCounts()).filter(itemStack -> !itemStack.isEmpty()).map(EmiStack::of).toList();
+    }
+
+    @Override
+    public EmiRecipeCategory getCategory() {
+        return CATEGORY;
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public List<EmiIngredient> getInputs() {
+        return input;
+    }
+
+    @Override
+    public List<EmiStack> getOutputs() {
+        return output;
+    }
+
+    @Override
+    public int getDisplayWidth() {
+        return 109;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 26;
+    }
+
+    @Override
+    public void addWidgets(WidgetHolder widgets) {
+        ResourceLocation texture = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/pulverizer.png");
+        widgets.addTexture(texture, 0, 0, 109, 26, 42, 30);
+
+        widgets.addSlot(input.get(0), 0, 4).drawBack(false);
+        widgets.addSlot(output.get(0), 64, 4).drawBack(false).recipeContext(this);
+        if(output.size() == 2)
+            widgets.addSlot(output.get(1), 91, 4).drawBack(false).recipeContext(this);
+    }
+}
---
+++ src/main/java/me/jddev0/ep/recipe/PulverizerRecipe.java
@@ -0,0 +1,207 @@
+package me.jddev0.ep.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+
+public class PulverizerRecipe implements Recipe<SimpleContainer> {
+    private final ResourceLocation id;
+    private final OutputItemStackWithPercentages output;
+    private final OutputItemStackWithPercentages secondaryOutput;
+    private final Ingredient input;
+
+    public PulverizerRecipe(ResourceLocation id, OutputItemStackWithPercentages output, OutputItemStackWithPercentages secondaryOutput, Ingredient input) {
+        this.id = id;
+        this.output = output;
+        this.secondaryOutput = secondaryOutput;
+        this.input = input;
+    }
+
+    public OutputItemStackWithPercentages getOutput() {
+        return output;
+    }
+
+    public OutputItemStackWithPercentages getSecondaryOutput() {
+        return secondaryOutput;
+    }
+
+    public Ingredient getInput() {
+        return input;
+    }
+
+    public ItemStack[] getMaxOutputCounts() {
+        ItemStack[] generatedOutputs = new ItemStack[2];
+
+        generatedOutputs[0] = output.output.copyWithCount(output.percentages.length);
+        generatedOutputs[1] = secondaryOutput.output.copyWithCount(secondaryOutput.percentages.length);
+
+        return generatedOutputs;
+    }
+
+    public ItemStack[] generateOutputs(RandomSource randomSource) {
+        ItemStack[] generatedOutputs = new ItemStack[2];
+        for(int i = 0;i < 2;i++) {
+            int count = 0;
+            OutputItemStackWithPercentages output = i == 0?this.output:this.secondaryOutput;
+
+            for(double percentage:output.percentages)
+                if(randomSource.nextDouble() <= percentage)
+                    count++;
+
+            generatedOutputs[i] = output.output.copyWithCount(count);
+        }
+
+        return generatedOutputs;
+    }
+
+    @Override
+    public boolean matches(SimpleContainer container, Level level) {
+        if(level.isClientSide)
+            return false;
+
+        return input.test(container.getItem(0));
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer container) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int width, int height) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        NonNullList<Ingredient> ingredients = NonNullList.createWithCapacity(1);
+        ingredients.add(0, input);
+        return ingredients;
+    }
+
+    @Override
+    public ItemStack getToastSymbol() {
+        return new ItemStack(ModBlocks.PULVERIZER_ITEM.get());
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static final class Type implements RecipeType<PulverizerRecipe> {
+        private Type() {}
+
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "pulverizer";
+    }
+
+    public static final class Serializer implements RecipeSerializer<PulverizerRecipe> {
+        private Serializer() {}
+
+        public static final Serializer INSTANCE = new Serializer();
+        public static final ResourceLocation ID = new ResourceLocation(EnergizedPowerMod.MODID, "pulverizer");
+
+        @Override
+        public PulverizerRecipe fromJson(ResourceLocation recipeID, JsonObject json) {
+            Ingredient input = Ingredient.fromJson(json.get("ingredient"));
+
+            OutputItemStackWithPercentages[] outputs = new OutputItemStackWithPercentages[2];
+            for(int i = 0;i < 2;i++) {
+                JsonObject outputJson = json.getAsJsonObject(i == 0?"output":"secondaryOutput");
+
+                ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(outputJson, "output"));
+
+                JsonArray percentagesJson = GsonHelper.getAsJsonArray(outputJson, "percentages");
+                double[] percentages = new double[percentagesJson.size()];
+                boolean minimumAtLeastOneFlag = false;
+                for(int j = 0;j < percentagesJson.size();j++) {
+                    double value = percentagesJson.get(j).getAsDouble();
+
+                    minimumAtLeastOneFlag |= (int)value >= 1;
+                    percentages[j] = value;
+                }
+
+                if(i == 0 && !minimumAtLeastOneFlag)
+                    throw new JsonSyntaxException("The primary output must have a minimum count of at least 1 (At least one percentage value must be >= 1.0)");
+
+                outputs[i] = new OutputItemStackWithPercentages(output, percentages);
+
+                if(!json.has("secondaryOutput")) {
+                    outputs[1] = new OutputItemStackWithPercentages(ItemStack.EMPTY, new double[0]);
+
+                    break;
+                }
+            }
+
+            return new PulverizerRecipe(recipeID, outputs[0], outputs[1], input);
+        }
+
+        @Override
+        public PulverizerRecipe fromNetwork(ResourceLocation recipeID, FriendlyByteBuf buffer) {
+            Ingredient input = Ingredient.fromNetwork(buffer);
+
+            OutputItemStackWithPercentages[] outputs = new OutputItemStackWithPercentages[2];
+            for(int i = 0;i < 2;i++) {
+                ItemStack output = buffer.readItem();
+
+                int percentageCount = buffer.readInt();
+                double[] percentages = new double[percentageCount];
+                for(int j = 0;j < percentageCount;j++)
+                    percentages[j] = buffer.readDouble();
+
+                outputs[i] = new OutputItemStackWithPercentages(output, percentages);
+            }
+
+            return new PulverizerRecipe(recipeID, outputs[0], outputs[1], input);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buffer, PulverizerRecipe recipe) {
+            recipe.input.toNetwork(buffer);
+
+            for(int i = 0;i < 2;i++) {
+                OutputItemStackWithPercentages output = i == 0?recipe.output:recipe.secondaryOutput;
+                buffer.writeItemStack(output.output, false);
+
+                buffer.writeInt(output.percentages.length);
+                for(double percentage:output.percentages)
+                    buffer.writeDouble(percentage);
+            }
+        }
+    }
+
+    public record OutputItemStackWithPercentages(ItemStack output, double[] percentages) {}
+}
