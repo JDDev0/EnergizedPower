b1203e4ba50a58a3b5f09bdfc37a583de5321ae0
JDDev0 <jddev0@dorneger.at>
2023-08-15 09:12:31 +0200

    Fix icon items in EMI, REI, and JEI integrations
    (cherry picked from commit ab5b9c56645f5ca423bf97e91a28aca60ad58019)

--- src/main/java/me/jddev0/ep/integration/rei/PlantGrowthChamberFertilizerCategory.java
+++ src/main/java/me/jddev0/ep/integration/rei/PlantGrowthChamberFertilizerCategory.java
@@ -33,7 +33,7 @@ public class PlantGrowthChamberFertilizerCategory implements DisplayCategory<Pla
 
     @Override
     public Renderer getIcon() {
-        return EntryStacks.of(ModBlocks.PLANT_GROWTH_CHAMBER.get());
+        return EntryStacks.of(ModBlocks.PLANT_GROWTH_CHAMBER_ITEM.get());
     }
 
     @Override
