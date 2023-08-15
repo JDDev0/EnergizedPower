5b0dfe6a81f53a048d96b5441ee0759a867bac3c
JDDev0 <jddev0@dorneger.at>
2023-08-15 13:58:20 +0200

    Add Pulverizer

---
+++ src/main/java/me/jddev0/ep/recipe/PulverizerRecipe.java
@@ -0,0 +1,207 @@
+package me.jddev0.ep.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.recipe.*;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+
+public class PulverizerRecipe implements Recipe<SimpleInventory> {
+    private final Identifier id;
+    private final OutputItemStackWithPercentages output;
+    private final OutputItemStackWithPercentages secondaryOutput;
+    private final Ingredient input;
+
+    public PulverizerRecipe(Identifier id, OutputItemStackWithPercentages output, OutputItemStackWithPercentages secondaryOutput, Ingredient input) {
+        this.id = id;
+        this.output = output;
+        this.secondaryOutput = secondaryOutput;
+        this.input = input;
+    }
+
+    public OutputItemStackWithPercentages getOutputItem() {
+        return output;
+    }
+
+    public OutputItemStackWithPercentages getSecondaryOutput() {
+        return secondaryOutput;
+    }
+
+    public Ingredient getInput() {
+        return input;
+    }
+
+    public ItemStack[] getMaxOutputCounts() {
+        ItemStack[] generatedOutputs = new ItemStack[2];
+
+        generatedOutputs[0] = output.output.copyWithCount(output.percentages.length);
+        generatedOutputs[1] = secondaryOutput.output.copyWithCount(secondaryOutput.percentages.length);
+
+        return generatedOutputs;
+    }
+
+    public ItemStack[] generateOutputs(Random randomSource) {
+        ItemStack[] generatedOutputs = new ItemStack[2];
+        for(int i = 0;i < 2;i++) {
+            int count = 0;
+            OutputItemStackWithPercentages output = i == 0?this.output:this.secondaryOutput;
+
+            for(double percentage:output.percentages)
+                if(randomSource.nextDouble() <= percentage)
+                    count++;
+
+            generatedOutputs[i] = output.output.copyWithCount(count);
+        }
+
+        return generatedOutputs;
+    }
+
+    @Override
+    public boolean matches(SimpleInventory container, World level) {
+        if(level.isClient())
+            return false;
+
+        return input.test(container.getStack(0));
+    }
+
+    @Override
+    public ItemStack craft(SimpleInventory container) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean fits(int width, int height) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getOutput() {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public DefaultedList<Ingredient> getIngredients() {
+        DefaultedList<Ingredient> ingredients = DefaultedList.ofSize(1);
+        ingredients.add(0, input);
+        return ingredients;
+    }
+
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(ModBlocks.PULVERIZER_ITEM);
+    }
+
+    @Override
+    public Identifier getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isIgnoredInRecipeBook() {
+        return true;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static final class Type implements RecipeType<PulverizerRecipe> {
+        private Type() {}
+
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "pulverizer";
+    }
+
+    public static final class Serializer implements RecipeSerializer<PulverizerRecipe> {
+        private Serializer() {}
+
+        public static final Serializer INSTANCE = new Serializer();
+        public static final Identifier ID = new Identifier(EnergizedPowerMod.MODID, "pulverizer");
+
+        @Override
+        public PulverizerRecipe read(Identifier recipeID, JsonObject json) {
+            Ingredient input = Ingredient.fromJson(json.get("ingredient"));
+
+            OutputItemStackWithPercentages[] outputs = new OutputItemStackWithPercentages[2];
+            for(int i = 0;i < 2;i++) {
+                JsonObject outputJson = json.getAsJsonObject(i == 0?"output":"secondaryOutput");
+
+                ItemStack output = ShapedRecipe.outputFromJson(JsonHelper.getObject(outputJson, "output"));
+
+                JsonArray percentagesJson = JsonHelper.getArray(outputJson, "percentages");
+                double[] percentages = new double[percentagesJson.size()];
+                boolean minimumAtLeastOneFlag = false;
+                for(int j = 0;j < percentagesJson.size();j++) {
+                    double value = percentagesJson.get(j).getAsDouble();
+
+                    minimumAtLeastOneFlag |= (int)value >= 1;
+                    percentages[j] = value;
+                }
+
+                if(i == 0 && !minimumAtLeastOneFlag)
+                    throw new JsonSyntaxException("The primary output must have a minimum count of at least 1 (At least one percentage value must be >= 1.0)");
+
+                outputs[i] = new OutputItemStackWithPercentages(output, percentages);
+
+                if(!json.has("secondaryOutput")) {
+                    outputs[1] = new OutputItemStackWithPercentages(ItemStack.EMPTY, new double[0]);
+
+                    break;
+                }
+            }
+
+            return new PulverizerRecipe(recipeID, outputs[0], outputs[1], input);
+        }
+
+        @Override
+        public PulverizerRecipe read(Identifier recipeID, PacketByteBuf buffer) {
+            Ingredient input = Ingredient.fromPacket(buffer);
+
+            OutputItemStackWithPercentages[] outputs = new OutputItemStackWithPercentages[2];
+            for(int i = 0;i < 2;i++) {
+                ItemStack output = buffer.readItemStack();
+
+                int percentageCount = buffer.readInt();
+                double[] percentages = new double[percentageCount];
+                for(int j = 0;j < percentageCount;j++)
+                    percentages[j] = buffer.readDouble();
+
+                outputs[i] = new OutputItemStackWithPercentages(output, percentages);
+            }
+
+            return new PulverizerRecipe(recipeID, outputs[0], outputs[1], input);
+        }
+
+        @Override
+        public void write(PacketByteBuf buffer, PulverizerRecipe recipe) {
+            recipe.input.write(buffer);
+
+            for(int i = 0;i < 2;i++) {
+                OutputItemStackWithPercentages output = i == 0?recipe.output:recipe.secondaryOutput;
+                buffer.writeItemStack(output.output);
+
+                buffer.writeInt(output.percentages.length);
+                for(double percentage:output.percentages)
+                    buffer.writeDouble(percentage);
+            }
+        }
+    }
+
+    public record OutputItemStackWithPercentages(ItemStack output, double[] percentages) {}
+}
---
+++ src/main/java/me/jddev0/ep/screen/PulverizerMenu.java
@@ -0,0 +1,154 @@
+package me.jddev0.ep.screen;
+
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.block.entity.PulverizerBlockEntity;
+import me.jddev0.ep.inventory.ConstraintInsertSlot;
+import me.jddev0.ep.recipe.PulverizerRecipe;
+import me.jddev0.ep.util.ByteUtils;
+import me.jddev0.ep.util.RecipeUtils;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.world.World;
+
+public class PulverizerMenu extends ScreenHandler implements EnergyStorageConsumerIndicatorBarMenu {
+    private final PulverizerBlockEntity blockEntity;
+    private final Inventory inv;
+    private final World level;
+    private final PropertyDelegate data;
+
+    public PulverizerMenu(int id, PlayerInventory inv, PacketByteBuf buf) {
+        this(id, inv.player.getWorld().getBlockEntity(buf.readBlockPos()), inv, new SimpleInventory(3) {
+            @Override
+            public boolean isValid(int slot, ItemStack stack) {
+                return switch(slot) {
+                    case 0 -> RecipeUtils.isIngredientOfAny(inv.player.getWorld(), PulverizerRecipe.Type.INSTANCE, stack);
+                    case 1, 2 -> false;
+                    default -> super.isValid(slot, stack);
+                };
+            }
+        }, new ArrayPropertyDelegate(9));
+    }
+
+    public PulverizerMenu(int id, BlockEntity blockEntity, PlayerInventory playerInventory, Inventory inv, PropertyDelegate data) {
+        super(ModMenuTypes.PULVERIZER_MENU, id);
+
+        this.blockEntity = (PulverizerBlockEntity)blockEntity;
+
+        this.inv = inv;
+        checkSize(this.inv, 3);
+        checkDataCount(data, 9);
+        this.level = playerInventory.player.getWorld();
+        this.inv.onOpen(playerInventory.player);
+        this.data = data;
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+
+        addSlot(new ConstraintInsertSlot(this.inv, 0, 43, 35));
+        addSlot(new ConstraintInsertSlot(this.inv, 1, 107, 35));
+        addSlot(new ConstraintInsertSlot(this.inv, 2, 134, 35));
+
+        addProperties(this.data);
+    }
+
+    @Override
+    public long getEnergy() {
+        return blockEntity.getEnergy();
+    }
+
+    @Override
+    public long getCapacity() {
+        return blockEntity.getCapacity();
+    }
+
+    @Override
+    public long getEnergyIndicatorBarValue() {
+        return ByteUtils.from2ByteChunks((short)data.get(4), (short)data.get(5), (short)data.get(6), (short)data.get(7));
+    }
+
+    /**
+     * @return Same as isCrafting but energy requirements are ignored
+     */
+    public boolean isCraftingActive() {
+        return ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1)) > 0;
+    }
+
+    public boolean isCrafting() {
+        return ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1)) > 0 && data.get(8) == 1;
+    }
+
+    public int getScaledProgressArrowSize() {
+        int progress = ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1));
+        int maxProgress = ByteUtils.from2ByteChunks((short)data.get(2), (short)data.get(3));
+        int progressArrowSize = 24;
+
+        return (maxProgress == 0 || progress == 0)?0:progress * progressArrowSize / maxProgress;
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int index) {
+        Slot sourceSlot = slots.get(index);
+        if(sourceSlot == null || !sourceSlot.hasStack())
+            return ItemStack.EMPTY;
+
+        ItemStack sourceItem = sourceSlot.getStack();
+        ItemStack sourceItemCopy = sourceItem.copy();
+
+        if(index < 4 * 9) {
+            //Player inventory slot -> Merge into tile inventory
+            if(!insertItem(sourceItem, 4 * 9, 4 * 9 + 1, false)) {
+                //"+1" instead of "+3": Do not allow adding to output slots
+                return ItemStack.EMPTY;
+            }
+        }else if(index < 4 * 9 + 3) {
+            //Tile inventory slot -> Merge into player inventory
+            if(!insertItem(sourceItem, 0, 4 * 9, false)) {
+                return ItemStack.EMPTY;
+            }
+        }else {
+            throw new IllegalArgumentException("Invalid slot index");
+        }
+
+        if(sourceItem.getCount() == 0)
+            sourceSlot.setStack(ItemStack.EMPTY);
+        else
+            sourceSlot.markDirty();
+
+        sourceSlot.onTakeItem(player, sourceItem);
+
+        return sourceItemCopy;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return canUse(ScreenHandlerContext.create(level, blockEntity.getPos()), player, ModBlocks.PULVERIZER);
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for(int i = 0;i < 3;i++) {
+            for(int j = 0;j < 9;j++) {
+                addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for(int i = 0;i < 9;i++) {
+            addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+    }
+
+    public BlockEntity getBlockEntity() {
+        return blockEntity;
+    }
+}
