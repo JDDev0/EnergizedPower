cd02d6ee84014e9c30a9c67e54c62bc58b6753b5
JDDev0 <jddev0@dorneger.at>
2023-08-19 14:06:38 +0200

    Add check if chunk is present in all packets which are sent from a client

--- src/main/java/me/jddev0/ep/networking/packet/SetTimeFromTimeControllerC2SPacket.java
+++ src/main/java/me/jddev0/ep/networking/packet/SetTimeFromTimeControllerC2SPacket.java
@@ -8,6 +8,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.World;
 import team.reborn.energy.api.EnergyStorage;
 
 public final class SetTimeFromTimeControllerC2SPacket {
@@ -19,7 +21,11 @@ public final class SetTimeFromTimeControllerC2SPacket {
         int time = buf.readInt();
 
         server.execute(() -> {
-            BlockEntity blockEntity = player.getWorld().getBlockEntity(pos);
+            World level = player.getWorld();
+            if(!level.isChunkLoaded(ChunkSectionPos.getSectionCoord(pos.getX()), ChunkSectionPos.getSectionCoord(pos.getZ())))
+                return;
+
+            BlockEntity blockEntity = level.getBlockEntity(pos);
             if(!(blockEntity instanceof TimeControllerBlockEntity timeControllerBlockEntity))
                 return;
 
