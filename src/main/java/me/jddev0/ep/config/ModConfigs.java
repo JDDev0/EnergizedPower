7085949565c73dc4f9cffe79649c11f3d4dba092
JDDev0 <jddev0@dorneger.at>
2023-08-22 22:04:55 +0200

    Change default energy value of the Powered Lamp
    (cherry picked from commit ade4bbe3dc9a53a6eca0edc47ef685ba10c74405)

--- src/main/java/me/jddev0/ep/config/ModConfigs.java
+++ src/main/java/me/jddev0/ep/config/ModConfigs.java
@@ -420,7 +420,7 @@ public final class ModConfigs {
     ));
 
     public static final ConfigValue<Integer> COMMON_POWERED_LAMP_TRANSFER_RATE = registerEnergyTransferRateConfigValue(
-            "block.powered_lamp", "Powered Lamp", 64
+            "block.powered_lamp", "Powered Lamp", 16
     );
 
     public static final ConfigValue<Integer> COMMON_POWERED_FURNACE_CAPACITY = registerEnergyCapacityConfigValue(
