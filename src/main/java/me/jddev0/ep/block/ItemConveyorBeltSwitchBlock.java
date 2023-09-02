9ecd42544dc8d01273296e9ccb5a8682c33c39da
JDDev0 <jddev0@dorneger.at>
2023-09-02 19:05:37 +0200

    Add Item Conveyor Belt Switch

---
+++ src/main/java/me/jddev0/ep/block/ItemConveyorBeltSwitchBlock.java
@@ -0,0 +1,93 @@
+package me.jddev0.ep.block;
+
+import me.jddev0.ep.block.entity.ItemConveyorBeltSwitchBlockEntity;
+import me.jddev0.ep.block.entity.ModBlockEntities;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemConveyorBeltSwitchBlock extends BlockWithEntity {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+
+    protected ItemConveyorBeltSwitchBlock(FabricBlockSettings props) {
+        super(props);
+
+        this.setDefaultState(this.getStateManager().getDefaultState().with(POWERED, false).with(FACING, Direction.NORTH));
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos blockPos, BlockState state) {
+        return new ItemConveyorBeltSwitchBlockEntity(blockPos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public void neighborUpdate(BlockState selfState, World level, BlockPos selfPos, Block fromBlock, BlockPos fromPos, boolean isMoving) {
+        super.neighborUpdate(selfState, level, selfPos, fromBlock, fromPos, isMoving);
+
+        if(level.isClient())
+            return;
+
+        boolean isPowered = level.isReceivingRedstonePower(selfPos);
+        if(isPowered != selfState.get(POWERED))
+            level.setBlockState(selfPos, selfState.with(POWERED, isPowered), 3);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState selfState, World level, BlockPos selfPos, BlockState oldState, boolean isMoving) {
+        if(level.isClient())
+            return;
+
+        boolean isPowered = level.isReceivingRedstonePower(selfPos);
+        if(isPowered != selfState.get(POWERED))
+            level.setBlockState(selfPos, selfState.with(POWERED, isPowered), 2);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(POWERED, FACING);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World level, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, ModBlockEntities.ITEM_CONVEYOR_BELT_SWITCH_ENTITY, ItemConveyorBeltSwitchBlockEntity::tick);
+    }
+}
---
+++ src/main/java/me/jddev0/ep/block/entity/ItemConveyorBeltSwitchBlockEntity.java
@@ -0,0 +1,94 @@
+package me.jddev0.ep.block.entity;
+
+import me.jddev0.ep.block.ItemConveyorBeltBlock;
+import me.jddev0.ep.block.ItemConveyorBeltSwitchBlock;
+import me.jddev0.ep.block.ModBlockStateProperties;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.config.ModConfigs;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+public class ItemConveyorBeltSwitchBlockEntity extends BlockEntity {
+    private static final int TICKS_PER_ITEM = ModConfigs.COMMON_ITEM_CONVEYOR_BELT_SWITCH_TICKS_PER_ITEM.getValue();
+
+    public ItemConveyorBeltSwitchBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(ModBlockEntities.ITEM_CONVEYOR_BELT_SWITCH_ENTITY, blockPos, blockState);
+    }
+
+    public static void tick(World level, BlockPos blockPos, BlockState state, ItemConveyorBeltSwitchBlockEntity blockEntity) {
+        if(level.isClient())
+            return;
+
+        if(level.getTime() % TICKS_PER_ITEM == 0) {
+            Direction facing = state.get(ItemConveyorBeltSwitchBlock.FACING);
+
+            BlockPos inputPos = blockPos.offset(facing);
+            BlockState inputBlockState = level.getBlockState(inputPos);
+            if(!inputBlockState.isOf(ModBlocks.ITEM_CONVEYOR_BELT))
+                return;
+
+            //Conveyor belt must face towards Switch and must not be ascending
+            ModBlockStateProperties.ConveyorBeltDirection inputBeltFacing = inputBlockState.get(ItemConveyorBeltBlock.FACING);
+            if(inputBeltFacing.isAscending() || inputBeltFacing.getDirection().getOpposite() != facing)
+                return;
+
+            BlockEntity inputBlockEntity = level.getBlockEntity(inputPos);
+            if(!(inputBlockEntity instanceof ItemConveyorBeltBlockEntity))
+                return;
+            Storage<ItemVariant> inputBeltItemStackStorage = ItemStorage.SIDED.find(level, inputPos, facing.getOpposite());
+            if(inputBeltItemStackStorage == null || !inputBeltItemStackStorage.supportsExtraction())
+                return;
+
+            ItemStack itemStackToSwitch = ItemStack.EMPTY;
+            try(Transaction transaction = Transaction.openOuter()) {
+                for(StorageView<ItemVariant> itemView:inputBeltItemStackStorage) {
+                    if(itemView.isResourceBlank())
+                        continue;
+
+                    ItemVariant itemVariant = itemView.getResource();
+                    long amount = inputBeltItemStackStorage.extract(itemVariant, 1, transaction);
+                    if(amount > 0) {
+                        itemStackToSwitch = itemVariant.toStack(1);
+
+                        break;
+                    }
+                }
+            }
+            if(itemStackToSwitch.isEmpty())
+                return;
+
+            boolean isPowered = state.get(ItemConveyorBeltSwitchBlock.POWERED);
+            Direction outputDirection = isPowered?facing.rotateYCounterclockwise():facing.rotateYClockwise();
+
+            BlockPos outputPos = blockPos.offset(outputDirection);
+            BlockState outputBlockState = level.getBlockState(outputPos);
+            if(!outputBlockState.isOf(ModBlocks.ITEM_CONVEYOR_BELT))
+                return;
+
+            BlockEntity outputBlockEntity = level.getBlockEntity(outputPos);
+            if(!(outputBlockEntity instanceof ItemConveyorBeltBlockEntity))
+                return;
+
+            Storage<ItemVariant> outputBeltItemStackStorage = ItemStorage.SIDED.find(level, outputPos, outputDirection.getOpposite());
+            if(outputBeltItemStackStorage == null || !outputBeltItemStackStorage.supportsInsertion())
+                return;
+
+            try(Transaction transaction = Transaction.openOuter()) {
+                long amount = outputBeltItemStackStorage.insert(ItemVariant.of(itemStackToSwitch), 1, transaction);
+                if(amount > 0)
+                    inputBeltItemStackStorage.extract(ItemVariant.of(itemStackToSwitch),1, transaction);
+
+                transaction.commit();
+            }
+        }
+    }
+}
