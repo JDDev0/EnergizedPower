2ce0f9a435ec746ff88405b214a467199486799a
JDDev0 <jddev0@dorneger.at>
2023-09-02 14:15:35 +0200

    Improve code for Wrench configuration

--- src/main/java/me/jddev0/ep/item/WrenchItem.java
+++ src/main/java/me/jddev0/ep/item/WrenchItem.java
@@ -1,16 +1,8 @@
 package me.jddev0.ep.item;
 
-import me.jddev0.ep.block.ItemConveyorBeltBlock;
-import me.jddev0.ep.block.FluidPipeBlock;
-import me.jddev0.ep.block.ModBlockStateProperties;
-import me.jddev0.ep.block.ModBlocks;
-import me.jddev0.ep.block.entity.FluidPipeBlockEntity;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
@@ -21,7 +13,6 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.s2c.play.OverlayMessageS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.state.property.EnumProperty;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
@@ -30,7 +21,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import me.jddev0.ep.block.entity.ItemConveyorBeltBlockEntity;
+import me.jddev0.ep.block.*;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -65,144 +56,31 @@ public class WrenchItem extends Item {
         ));
     }
 
-    public ActionResult useOnFluidPipe(ItemUsageContext useOnContext) {
-        World level = useOnContext.getWorld();
-        BlockPos blockPos = useOnContext.getBlockPos();
-        BlockState state = level.getBlockState(blockPos);
-
-        ItemStack itemStack = useOnContext.getStack();
-        Direction currentFace = getCurrentFace(itemStack);
-
-        BlockPos testPos = blockPos.offset(currentFace);
-
-        PlayerEntity player = useOnContext.getPlayer();
-
-        BlockEntity testBlockEntity = level.getBlockEntity(testPos);
-        if(testBlockEntity == null || testBlockEntity instanceof FluidPipeBlockEntity) {
-            //Connections to non-fluid blocks nor connections to another pipe can not be modified
-
-            if(player instanceof ServerPlayerEntity serverPlayer) {
-                serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
-                        Text.translatable("tooltip.energizedpower.wrench.face_change_not_possible",
-                                Text.translatable("tooltip.energizedpower.direction." + currentFace.asString()).
-                                        formatted(Formatting.WHITE)
-                        ).formatted(Formatting.RED)
-                ));
-            }
-
-            return ActionResult.SUCCESS;
-        }
-
-        Storage<FluidVariant> fluidStorage = FluidStorage.SIDED.find(level, testPos, currentFace.getOpposite());
-        if(fluidStorage == null) {
-            if(player instanceof ServerPlayerEntity serverPlayer) {
-                serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
-                        Text.translatable("tooltip.energizedpower.wrench.face_change_not_possible",
-                                Text.translatable("tooltip.energizedpower.direction." + currentFace.asString()).
-                                        formatted(Formatting.WHITE)
-                        ).formatted(Formatting.RED)
-                ));
-            }
-
-            return ActionResult.SUCCESS;
-        }
-
-
-        //If first has no next, no tanks are present
-        if(!fluidStorage.iterator().hasNext()) {
-            if(player instanceof ServerPlayerEntity serverPlayer) {
-                serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
-                        Text.translatable("tooltip.energizedpower.wrench.face_change_not_possible",
-                                Text.translatable("tooltip.energizedpower.direction." + currentFace.asString()).
-                                        formatted(Formatting.WHITE)
-                        ).formatted(Formatting.RED)
-                ));
-            }
-
-            return ActionResult.SUCCESS;
-        }
-
-        EnumProperty<ModBlockStateProperties.PipeConnection> pipeConnectionProperty =
-                FluidPipeBlock.getPipeConnectionPropertyFromDirection(currentFace);
-
-        int diff = player != null && player.isSneaking()?-1:1;
-
-        ModBlockStateProperties.PipeConnection pipeConnection = state.get(pipeConnectionProperty);
-        pipeConnection = ModBlockStateProperties.PipeConnection.values()[(pipeConnection.ordinal() + diff +
-                ModBlockStateProperties.PipeConnection.values().length) %
-                ModBlockStateProperties.PipeConnection.values().length];
-
-        level.setBlockState(blockPos, state.with(pipeConnectionProperty, pipeConnection), 3);
-
-        if(player instanceof ServerPlayerEntity serverPlayer) {
-            serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
-                    Text.translatable("tooltip.energizedpower.wrench.face_change",
-                            Text.translatable("tooltip.energizedpower.direction." + currentFace.asString()).
-                                    formatted(Formatting.WHITE),
-                            Text.translatable(pipeConnection.getTranslationKey()).
-                                    formatted(Formatting.WHITE, Formatting.BOLD)
-                    ).formatted(Formatting.GREEN)
-            ));
-        }
-
-        return ActionResult.SUCCESS;
-    }
-
-    public ActionResult useOnConveyorBelt(ItemUsageContext useOnContext) {
-        World level = useOnContext.getWorld();
-        BlockPos blockPos = useOnContext.getBlockPos();
-        BlockState state = level.getBlockState(blockPos);
-
-        PlayerEntity player = useOnContext.getPlayer();
-
-        ModBlockStateProperties.ConveyorBeltDirection facing = state.get(ItemConveyorBeltBlock.FACING);
-        Boolean shape;
-
-        if(player != null && player.isSneaking()) {
-            if(facing.isAscending())
-                shape = null;
-            else if(facing.isDescending())
-                shape = true;
-            else
-                shape = false;
-        }else {
-            if(facing.isAscending())
-                shape = false;
-            else if(facing.isDescending())
-                shape = null;
-            else
-                shape = true;
-        }
-
-        level.setBlockState(blockPos, state.with(ItemConveyorBeltBlock.FACING, ModBlockStateProperties.ConveyorBeltDirection.of(facing.getDirection(), shape)), 3);
-
-        if(player instanceof ServerPlayerEntity serverPlayer) {
-            serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
-                    Text.translatable("tooltip.energizedpower.wrench.change",
-                            Text.translatable("tooltip.energizedpower.conveyor_belt_direction.slope." + (shape == null?"flat":(shape?"ascending":"descending"))).
-                                    formatted(Formatting.WHITE, Formatting.BOLD)
-                    ).formatted(Formatting.GREEN)
-            ));
-        }
-
-        return ActionResult.SUCCESS;
-    }
-
     @Override
     public ActionResult useOnBlock(ItemUsageContext useOnContext) {
         World level = useOnContext.getWorld();
         if(level.isClient())
             return ActionResult.SUCCESS;
 
+        PlayerEntity player = useOnContext.getPlayer();
+
         BlockPos blockPos = useOnContext.getBlockPos();
         BlockState state = level.getBlockState(blockPos);
-        if(state.isOf(ModBlocks.FLUID_PIPE) && (level.getBlockEntity(blockPos) instanceof FluidPipeBlockEntity))
-            return useOnFluidPipe(useOnContext);
+        Block block = state.getBlock();
+        if(!(block instanceof WrenchConfigurable wrenchConfigurableBlock)) {
+            if(player instanceof ServerPlayerEntity serverPlayer) {
+                serverPlayer.networkHandler.sendPacket(new OverlayMessageS2CPacket(
+                        Text.translatable("tooltip.energizedpower.wrench.not_configurable").formatted(Formatting.RED)
+                ));
+            }
 
-        if(state.isOf(ModBlocks.ITEM_CONVEYOR_BELT) && (level.getBlockEntity(blockPos) instanceof ItemConveyorBeltBlockEntity))
-            return useOnConveyorBelt(useOnContext);
+            return ActionResult.SUCCESS;
+        }
 
-        return ActionResult.SUCCESS;
+        ItemStack itemStack = useOnContext.getStack();
+        Direction currentFace = getCurrentFace(itemStack);
+
+        return wrenchConfigurableBlock.onUseWrench(useOnContext, currentFace, player != null && player.isSneaking());
     }
 
     @Override
