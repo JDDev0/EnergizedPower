9db93d7bb012aa96aa50cce957c54b5782f0da5e
JDDev0 <jddev0@dorneger.at>
2023-09-07 18:51:43 +0200

    Add Gear painting
    (cherry picked from commit ff4e5feb24a72a8e9816eb1e7e0c821d7b6a17ab)

---
+++ src/main/java/me/jddev0/ep/painting/ModPaintings.java
@@ -0,0 +1,20 @@
+package me.jddev0.ep.painting;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import net.minecraft.world.entity.decoration.PaintingVariant;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class ModPaintings {
+    private ModPaintings() {}
+
+    public static final DeferredRegister<PaintingVariant> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, EnergizedPowerMod.MODID);
+
+    public static final RegistryObject<PaintingVariant> GEAR = PAINTINGS.register("gear", () -> new PaintingVariant(32, 32));
+
+    public static void register(IEventBus modEventBus) {
+        PAINTINGS.register(modEventBus);
+    }
+}
