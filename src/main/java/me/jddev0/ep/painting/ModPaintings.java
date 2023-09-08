02e07089e63af1a173c6216a8de0ac16efd42964
JDDev0 <jddev0@dorneger.at>
2023-09-08 18:34:15 +0200

    Add Factory painting
    (cherry picked from commit 440c8836c78d1c058197802d0e24c908b44fbe65)

--- src/main/java/me/jddev0/ep/painting/ModPaintings.java
+++ src/main/java/me/jddev0/ep/painting/ModPaintings.java
@@ -13,6 +13,7 @@ public final class ModPaintings {
     public static final DeferredRegister<PaintingVariant> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, EnergizedPowerMod.MODID);
 
     public static final RegistryObject<PaintingVariant> GEAR = PAINTINGS.register("gear", () -> new PaintingVariant(32, 32));
+    public static final RegistryObject<PaintingVariant> FACTORY = PAINTINGS.register("factory", () -> new PaintingVariant(32, 32));
 
     public static void register(IEventBus modEventBus) {
         PAINTINGS.register(modEventBus);
