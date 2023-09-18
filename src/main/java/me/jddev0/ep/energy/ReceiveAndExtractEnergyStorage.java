0f127aa6a81fd7f9218b2298a1a2d967ba80efa6
JDDev0 <jddev0@dorneger.at>
2023-09-18 17:24:07 +0200

    Fix crash with cables in structure generation
    (cherry picked from commit b050c8ff3b54c8fbfed71df2149cd63c2190730f)

--- src/main/java/me/jddev0/ep/energy/ReceiveAndExtractEnergyStorage.java
+++ src/main/java/me/jddev0/ep/energy/ReceiveAndExtractEnergyStorage.java
@@ -120,8 +120,11 @@ public class ReceiveAndExtractEnergyStorage implements IEnergizedPowerEnergyStor
 
     @Override
     public void loadNBT(Tag tag) {
-        if(!(tag instanceof IntTag))
-            throw new IllegalArgumentException("Tag must be of type IntTag!");
+        if(!(tag instanceof IntTag)) {
+            energy = 0;
+
+            return;
+        }
 
         energy = ((IntTag)tag).getAsInt();
     }
