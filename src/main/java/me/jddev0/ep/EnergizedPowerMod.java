faec55a7bf9735868df02f8cf681293096459e59
JDDev0 <jddev0@dorneger.at>
2023-10-06 14:47:19 +0200

    Add Reinforced Advanced Solar Cell
    (cherry picked from commit 5661ef5f11adac9730291dd16827c6ceb935a95f)

--- src/main/java/me/jddev0/ep/EnergizedPowerMod.java
+++ src/main/java/me/jddev0/ep/EnergizedPowerMod.java
@@ -165,6 +165,7 @@ public class EnergizedPowerMod {
 
             event.accept(ModItems.BASIC_SOLAR_CELL);
             event.accept(ModItems.ADVANCED_SOLAR_CELL);
+            event.accept(ModItems.REINFORCED_ADVANCED_SOLAR_CELL);
 
             addEmptyAndFullyChargedItem(event, ModItems.INVENTORY_COAL_ENGINE, InventoryCoalEngineItem.CAPACITY);
             event.accept(ModItems.INVENTORY_CHARGER);
