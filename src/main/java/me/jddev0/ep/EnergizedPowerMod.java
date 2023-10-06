c18fbf8f574d6b38ae1ba03407d3614ed89fbb88
JDDev0 <jddev0@dorneger.at>
2023-10-06 20:00:16 +0200

    Add Gear Press Mold

--- src/main/java/me/jddev0/ep/EnergizedPowerMod.java
+++ src/main/java/me/jddev0/ep/EnergizedPowerMod.java
@@ -198,6 +198,7 @@ public class EnergizedPowerMod {
             event.accept(ModItems.GOOD_FERTILIZER);
             event.accept(ModItems.ADVANCED_FERTILIZER);
             event.accept(ModItems.RAW_GEAR_PRESS_MOLD);
+            event.accept(ModItems.GEAR_PRESS_MOLD);
             event.accept(ModItems.SILICON);
             event.accept(ModItems.COPPER_DUST);
             event.accept(ModItems.IRON_DUST);
