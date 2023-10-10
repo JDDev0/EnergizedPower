d117a4717baa631a44cce4be20e936e60f14f0a7
JDDev0 <jddev0@dorneger.at>
2023-10-10 18:23:07 +0200

    Add Tooltip to the Metal Press

--- src/main/java/me/jddev0/ep/block/MetalPressBlock.java
+++ src/main/java/me/jddev0/ep/block/MetalPressBlock.java
@@ -2,13 +2,20 @@ package me.jddev0.ep.block;
 
 import me.jddev0.ep.block.entity.MetalPressBlockEntity;
 import me.jddev0.ep.block.entity.ModBlockEntities;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -16,6 +23,8 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalPressBlock extends BaseEntityBlock {
     public MetalPressBlock(Properties props) {
@@ -79,5 +88,21 @@ public class MetalPressBlock extends BaseEntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
         return createTickerHelper(type, ModBlockEntities.METAL_PRESS_ENTITY.get(), MetalPressBlockEntity::tick);
+    }
+
+    public static class Item extends BlockItem {
+        public Item(Block block, Properties props) {
+            super(block, props);
+        }
+
+        @Override
+        public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+            if(Screen.hasShiftDown()) {
+                components.add(Component.translatable("tooltip.energizedpower.metal_press.txt.shift.1").
+                        withStyle(ChatFormatting.GRAY));
+            }else {
+                components.add(Component.translatable("tooltip.energizedpower.shift_details.txt").withStyle(ChatFormatting.YELLOW));
+            }
+        }
     }
 }
