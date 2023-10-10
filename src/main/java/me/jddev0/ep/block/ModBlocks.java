81a0d64c453ae8ee5161f8e48b89c53d417b3987
JDDev0 <jddev0@dorneger.at>
2023-10-10 18:23:07 +0200

    Add Tooltip to the Metal Press
    (cherry picked from commit d117a4717baa631a44cce4be20e936e60f14f0a7)

--- src/main/java/me/jddev0/ep/block/ModBlocks.java
+++ src/main/java/me/jddev0/ep/block/ModBlocks.java
@@ -222,7 +222,8 @@ public final class ModBlocks {
     public static final RegistryObject<Block> METAL_PRESS = BLOCKS.register("metal_press",
             () -> new MetalPressBlock(BlockBehaviour.Properties.of().
                     requiresCorrectToolForDrops().strength(5.0f, 6.0f).sound(SoundType.METAL)));
-    public static final RegistryObject<Item> METAL_PRESS_ITEM = createBlockItem("metal_press", METAL_PRESS);
+    public static final RegistryObject<Item> METAL_PRESS_ITEM = ModItems.ITEMS.register("metal_press",
+            () -> new MetalPressBlock.Item(METAL_PRESS.get(), new Item.Properties()));
 
     public static final RegistryObject<Block> PLANT_GROWTH_CHAMBER = BLOCKS.register("plant_growth_chamber",
             () -> new PlantGrowthChamberBlock(BlockBehaviour.Properties.of().
