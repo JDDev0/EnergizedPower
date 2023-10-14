19ca86170c239a1b3ef4d5feb2d12442b34e196a
JDDev0 <jddev0@dorneger.at>
2023-10-14 14:14:50 +0200

    Add left-click on block feature to the Energized Power book

--- src/main/java/me/jddev0/ep/item/EnergizedPowerBookItem.java
+++ src/main/java/me/jddev0/ep/item/EnergizedPowerBookItem.java
@@ -4,15 +4,20 @@ import me.jddev0.ep.screen.EnergizedPowerBookScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.WrittenBookItem;
+import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -27,6 +32,13 @@ public class EnergizedPowerBookItem extends WrittenBookItem {
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         tooltip.add(Text.translatable("book.byAuthor", "JDDev0").formatted(Formatting.GRAY));
         tooltip.add(Text.translatable("book.generation.0").formatted(Formatting.GRAY));
+
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.energizedpower.energized_power_book.txt.shift.1").
+                    formatted(Formatting.GRAY));
+        }else {
+            tooltip.add(Text.translatable("tooltip.energizedpower.shift_details.txt").formatted(Formatting.YELLOW));
+        }
     }
 
     @Override
@@ -34,14 +46,29 @@ public class EnergizedPowerBookItem extends WrittenBookItem {
         ItemStack itemStack = player.getStackInHand(interactionHand);
 
         if(level.isClient())
-            showBookViewScreen();
+            showBookViewScreen(null);
 
         return TypedActionResult.success(itemStack, level.isClient());
     }
 
+    @Override
+    public float getMiningSpeedMultiplier(ItemStack itemStack, BlockState blockState) {
+        return Float.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public boolean canMine(BlockState state, World level, BlockPos blockPos, PlayerEntity player) {
+        if(!level.isClient())
+            return false;
+
+        showBookViewScreen(Registries.BLOCK.getId(state.getBlock()));
+
+        return false;
+    }
+
     @Environment(EnvType.CLIENT)
-    private void showBookViewScreen() {
-        MinecraftClient.getInstance().setScreen(new EnergizedPowerBookScreen());
+    private void showBookViewScreen(Identifier openOnPageForBlock) {
+        MinecraftClient.getInstance().setScreen(new EnergizedPowerBookScreen(openOnPageForBlock));
     }
 
     @Override
