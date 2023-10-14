641d1d47ae70c6c3119d6f957111b2bf5fe881a3
JDDev0 <jddev0@dorneger.at>
2023-10-14 11:55:44 +0200

    Add left-click on block feature to the Energized Power book
    (cherry picked from commit ebd94c5d373b89decb08025c0a79e70b0e3eafa2)

--- src/main/java/me/jddev0/ep/item/EnergizedPowerBookItem.java
+++ src/main/java/me/jddev0/ep/item/EnergizedPowerBookItem.java
@@ -3,14 +3,23 @@ package me.jddev0.ep.item;
 import me.jddev0.ep.screen.EnergizedPowerBookScreen;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -24,6 +33,13 @@ public class EnergizedPowerBookItem extends WrittenBookItem {
     public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
         components.add(Component.translatable("book.byAuthor", "JDDev0").withStyle(ChatFormatting.GRAY));
         components.add(Component.translatable("book.generation.0").withStyle(ChatFormatting.GRAY));
+
+        if(Screen.hasShiftDown()) {
+            components.add(Component.translatable("tooltip.energizedpower.energized_power_book.txt.shift.1").
+                    withStyle(ChatFormatting.GRAY));
+        }else {
+            components.add(Component.translatable("tooltip.energizedpower.shift_details.txt").withStyle(ChatFormatting.YELLOW));
+        }
     }
 
     @Override
@@ -31,14 +47,29 @@ public class EnergizedPowerBookItem extends WrittenBookItem {
         ItemStack itemStack = player.getItemInHand(interactionHand);
 
         if(level.isClientSide)
-            showBookViewScreen();
+            showBookViewScreen(null);
 
         return InteractionResultHolder.sidedSuccess(itemStack, level.isClientSide());
     }
 
+    @Override
+    public float getDestroySpeed(ItemStack itemStack, BlockState blockState) {
+        return Float.POSITIVE_INFINITY;
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState state, Level level, BlockPos blockPos, Player player) {
+        if(!level.isClientSide)
+            return false;
+
+        showBookViewScreen(ForgeRegistries.BLOCKS.getKey(state.getBlock()));
+
+        return false;
+    }
+
     @OnlyIn(Dist.CLIENT)
-    private void showBookViewScreen() {
-        Minecraft.getInstance().setScreen(new EnergizedPowerBookScreen());
+    private void showBookViewScreen(ResourceLocation openOnPageForBlock) {
+        Minecraft.getInstance().setScreen(new EnergizedPowerBookScreen(openOnPageForBlock));
     }
 
     @Override
