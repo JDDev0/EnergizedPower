477b62cb05ee908ff933410fefe152bcb6696836
JDDev0 <jddev0@dorneger.at>
2023-11-04 10:53:21 +0100

    Add Teleporter Processing Unit

--- src/main/java/me/jddev0/ep/EnergizedPowerMod.java
+++ src/main/java/me/jddev0/ep/EnergizedPowerMod.java
@@ -186,6 +186,7 @@ public class EnergizedPowerMod implements ModInitializer {
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB_REG_KEY, ModItems.ADVANCED_CIRCUIT);
 
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB_REG_KEY, ModItems.TELEPORTER_MATRIX);
+		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB_REG_KEY, ModItems.TELEPORTER_PROCESSING_UNIT);
 
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB_REG_KEY, ModBlocks.SILICON_BLOCK_ITEM);
 		addToCreativeTab(ModCreativeModeTab.ENERGIZED_POWER_TAB_REG_KEY, ModBlocks.SAWDUST_BLOCK_ITEM);
