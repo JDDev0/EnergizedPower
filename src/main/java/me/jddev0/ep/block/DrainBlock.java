2d3f76dad075ded4ef95f668ee449957628859e8
JDDev0 <jddev0@dorneger.at>
2023-11-12 11:39:17 +0100

    Add Drain

---
+++ src/main/java/me/jddev0/ep/block/DrainBlock.java
@@ -0,0 +1,95 @@
+package me.jddev0.ep.block;
+
+import me.jddev0.ep.block.entity.DrainBlockEntity;
+import me.jddev0.ep.block.entity.ModBlockEntities;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DrainBlock extends BlockWithEntity {
+    public DrainBlock(FabricBlockSettings props) {
+        super(props);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos blockPos, BlockState state) {
+        return new DrainBlockEntity(blockPos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World level, BlockPos blockPos) {
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if(!(blockEntity instanceof DrainBlockEntity drainBlockEntity))
+            return super.getComparatorOutput(state, level, blockPos);
+
+        return drainBlockEntity.getRedstoneOutput();
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World level, BlockPos blockPos, PlayerEntity player, Hand handItem, BlockHitResult hit) {
+        if(level.isClient())
+            return ActionResult.SUCCESS;
+
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if(!(blockEntity instanceof DrainBlockEntity))
+            throw new IllegalStateException("Container is invalid");
+
+        player.openHandledScreen((DrainBlockEntity)blockEntity);
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World level, BlockState state, BlockEntityType<T> type) {
+        return checkType(type, ModBlockEntities.DRAIN_ENTITY, DrainBlockEntity::tick);
+    }
+
+    public static class Item extends BlockItem {
+        public Item(Block block, FabricItemSettings props) {
+            super(block, props);
+        }
+
+        @Override
+        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+            if(Screen.hasShiftDown()) {
+                tooltip.add(Text.translatable("tooltip.energizedpower.drain.txt.shift").
+                        formatted(Formatting.GRAY));
+            }else {
+                tooltip.add(Text.translatable("tooltip.energizedpower.shift_details.txt").formatted(Formatting.YELLOW));
+            }
+        }
+    }
+}
