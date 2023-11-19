304a4d2f356f079963d2f519d92b43344dc354a7
JDDev0 <jddev0@dorneger.at>
2023-11-19 07:40:41 +0100

    Add Redstone Mode configuration to Crusher, Sawmill, and Compressor

--- src/main/java/me/jddev0/ep/screen/SawmillScreen.java
+++ src/main/java/me/jddev0/ep/screen/SawmillScreen.java
@@ -1,20 +1,49 @@
 package me.jddev0.ep.screen;
 
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.ChangeRedstoneModeC2SPacket;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Inventory;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @OnlyIn(Dist.CLIENT)
 public class SawmillScreen extends AbstractGenericEnergyStorageContainerScreen<SawmillMenu> {
+    private final ResourceLocation CONFIGURATION_ICONS_TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/machine_configuration/configuration_buttons.png");
+
     public SawmillScreen(SawmillMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component,
                 "tooltip.energizedpower.recipe.energy_required_to_finish.txt",
                 new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/sawmill.png"),
                 8, 17);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+                //Redstone Mode
+
+                ModMessages.sendToServer(new ChangeRedstoneModeC2SPacket(menu.getBlockEntity().getBlockPos()));
+                clicked = true;
+            }
+
+            if(clicked)
+                minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
@@ -25,10 +54,39 @@ public class SawmillScreen extends AbstractGenericEnergyStorageContainerScreen<S
         int y = (height - imageHeight) / 2;
 
         renderProgressArrow(guiGraphics, x, y);
+
+        renderConfiguration(guiGraphics, x, y, mouseX, mouseY);
     }
 
     private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
         if(menu.isCraftingActive())
             guiGraphics.blit(TEXTURE, x + 68, y + 34, 176, 53, menu.getScaledProgressArrowSize(), 17);
+    }
+
+    private void renderConfiguration(GuiGraphics guiGraphics, int x, int y, int mouseX, int mouseY) {
+        RedstoneMode redstoneMode = menu.getRedstoneMode();
+        int ordinal = redstoneMode.ordinal();
+
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            guiGraphics.blit(CONFIGURATION_ICONS_TEXTURE, x - 22, y + 2, 20 * ordinal, 20, 20, 20);
+        }else {
+            guiGraphics.blit(CONFIGURATION_ICONS_TEXTURE, x - 22, y + 2, 20 * ordinal, 0, 20, 20);
+        }
+    }
+
+    @Override
+    protected void renderTooltip(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        super.renderTooltip(guiGraphics, mouseX, mouseY);
+
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            //Redstone Mode
+
+            RedstoneMode redstoneMode = menu.getRedstoneMode();
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.name().toLowerCase()));
+
+            guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
+        }
     }
 }
