7be249896200023897882a958b33254d7ba06a3a
JDDev0 <jddev0@dorneger.at>
2023-11-19 07:40:41 +0100

    Add Redstone Mode configuration to Crusher, Sawmill, and Compressor
    (cherry picked from commit 304a4d2f356f079963d2f519d92b43344dc354a7)

--- src/main/java/me/jddev0/ep/block/entity/SawmillBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/SawmillBlockEntity.java
@@ -1,9 +1,12 @@
 package me.jddev0.ep.block.entity;
 
+import me.jddev0.ep.block.SawmillBlock;
 import me.jddev0.ep.block.entity.handler.InputOutputItemHandler;
 import me.jddev0.ep.config.ModConfigs;
 import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
 import me.jddev0.ep.energy.ReceiveOnlyEnergyStorage;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.machine.configuration.RedstoneModeUpdate;
 import me.jddev0.ep.networking.ModMessages;
 import me.jddev0.ep.networking.packet.EnergySyncS2CPacket;
 import me.jddev0.ep.recipe.SawmillRecipe;
@@ -40,7 +43,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
-public class SawmillBlockEntity extends BlockEntity implements MenuProvider, EnergyStoragePacketUpdate {
+public class SawmillBlockEntity extends BlockEntity implements MenuProvider, EnergyStoragePacketUpdate, RedstoneModeUpdate {
     private static final int ENERGY_USAGE_PER_TICK = ModConfigs.COMMON_SAWMILL_ENERGY_CONSUMPTION_PER_TICK.getValue();
 
     private final ItemStackHandler itemHandler = new ItemStackHandler(3) {
@@ -83,6 +86,8 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
     private int energyConsumptionLeft = -1;
     private boolean hasEnoughEnergy;
 
+    private @NotNull RedstoneMode redstoneMode = RedstoneMode.IGNORE;
+
     public SawmillBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModBlockEntities.SAWMILL_ENTITY.get(), blockPos, blockState);
 
@@ -107,6 +112,7 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
                     case 2, 3 -> ByteUtils.get2Bytes(SawmillBlockEntity.this.maxProgress, index - 2);
                     case 4, 5 -> ByteUtils.get2Bytes(SawmillBlockEntity.this.energyConsumptionLeft, index - 4);
                     case 6 -> hasEnoughEnergy?1:0;
+                    case 7 -> redstoneMode.ordinal();
                     default -> 0;
                 };
             }
@@ -121,12 +127,13 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
                             SawmillBlockEntity.this.maxProgress, (short)value, index - 2
                     );
                     case 4, 5, 6 -> {}
+                    case 7 -> SawmillBlockEntity.this.redstoneMode = RedstoneMode.fromIndex(value);
                 }
             }
 
             @Override
             public int getCount() {
-                return 7;
+                return 8;
             }
         };
     }
@@ -187,6 +194,8 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
         nbt.put("recipe.progress", IntTag.valueOf(progress));
         nbt.put("recipe.energy_consumption_left", IntTag.valueOf(energyConsumptionLeft));
 
+        nbt.putInt("configuration.redstone_mode", redstoneMode.ordinal());
+
         super.saveAdditional(nbt);
     }
 
@@ -199,6 +208,8 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
 
         progress = nbt.getInt("recipe.progress");
         energyConsumptionLeft = nbt.getInt("recipe.energy_consumption_left");
+
+        redstoneMode = RedstoneMode.fromIndex(nbt.getInt("configuration.redstone_mode"));
     }
 
     public void drops(Level level, BlockPos worldPosition) {
@@ -211,6 +222,9 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
 
     public static void tick(Level level, BlockPos blockPos, BlockState state, SawmillBlockEntity blockEntity) {
         if(level.isClientSide)
+            return;
+
+        if(!blockEntity.redstoneMode.isActive(state.getValue(SawmillBlock.POWERED)))
             return;
 
         if(hasRecipe(blockEntity)) {
@@ -326,5 +340,11 @@ public class SawmillBlockEntity extends BlockEntity implements MenuProvider, Ene
     @Override
     public void setCapacity(int capacity) {
         energyStorage.setCapacityWithoutUpdate(capacity);
+    }
+
+    @Override
+    public void setNextRedstoneMode() {
+        redstoneMode = RedstoneMode.fromIndex(redstoneMode.ordinal() + 1);
+        setChanged();
     }
 }
--- src/main/java/me/jddev0/ep/screen/CompressorScreen.java
+++ src/main/java/me/jddev0/ep/screen/CompressorScreen.java
@@ -1,22 +1,49 @@
 package me.jddev0.ep.screen;
 
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.ChangeRedstoneModeC2SPacket;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @OnlyIn(Dist.CLIENT)
 public class CompressorScreen extends AbstractGenericEnergyStorageContainerScreen<CompressorMenu> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/compressor.png");
+    private final ResourceLocation CONFIGURATION_ICONS_TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/machine_configuration/configuration_buttons.png");
 
     public CompressorScreen(CompressorMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component,
                 "tooltip.energizedpower.recipe.energy_required_to_finish.txt",
                 new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/compressor.png"),
                 8, 17);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+                //Redstone Mode
+
+                ModMessages.sendToServer(new ChangeRedstoneModeC2SPacket(menu.getBlockEntity().getBlockPos()));
+                clicked = true;
+            }
+
+            if(clicked)
+                minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
@@ -27,10 +54,39 @@ public class CompressorScreen extends AbstractGenericEnergyStorageContainerScree
         int y = (height - imageHeight) / 2;
 
         renderProgressArrow(guiGraphics, x, y);
+
+        renderConfiguration(guiGraphics, x, y, mouseX, mouseY);
     }
 
     private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
         if(menu.isCraftingActive())
             guiGraphics.blit(TEXTURE, x + 79, y + 30, 176, 53, menu.getScaledProgressArrowSize(), 25);
+    }
+
+    private void renderConfiguration(GuiGraphics guiGraphics, int x, int y, int mouseX, int mouseY) {
+        RedstoneMode redstoneMode = menu.getRedstoneMode();
+        int ordinal = redstoneMode.ordinal();
+
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            guiGraphics.blit(CONFIGURATION_ICONS_TEXTURE, x - 22, y + 2, 20 * ordinal, 20, 20, 20);
+        }else {
+            guiGraphics.blit(CONFIGURATION_ICONS_TEXTURE, x - 22, y + 2, 20 * ordinal, 0, 20, 20);
+        }
+    }
+
+    @Override
+    protected void renderTooltip(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        super.renderTooltip(guiGraphics, mouseX, mouseY);
+
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            //Redstone Mode
+
+            RedstoneMode redstoneMode = menu.getRedstoneMode();
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.name().toLowerCase()));
+
+            guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
+        }
     }
 }
