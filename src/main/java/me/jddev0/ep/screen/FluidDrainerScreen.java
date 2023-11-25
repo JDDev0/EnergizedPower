ef28dc23f78f9e3dfc69f30d072d9a3740436760
JDDev0 <jddev0@dorneger.at>
2023-11-25 15:02:26 +0100

    Add Redstone Mode configuration to Plant Growth Chamber, Fluid Drainer, and Fluid Filler

--- src/main/java/me/jddev0/ep/screen/FluidDrainerScreen.java
+++ src/main/java/me/jddev0/ep/screen/FluidDrainerScreen.java
@@ -6,17 +6,24 @@ import me.jddev0.ep.fluid.FluidStack;
 import me.jddev0.ep.util.FluidUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
+import net.minecraft.client.sound.PositionedSoundInstance;
 import net.minecraft.client.texture.MissingSprite;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.networking.ModMessages;
 import org.joml.Matrix4f;
 
 import java.util.ArrayList;
@@ -25,10 +32,32 @@ import java.util.Optional;
 
 @Environment(EnvType.CLIENT)
 public class FluidDrainerScreen extends AbstractGenericEnergyStorageHandledScreen<FluidDrainerMenu> {
+    private final Identifier CONFIGURATION_ICONS_TEXTURE = new Identifier(EnergizedPowerMod.MODID, "textures/gui/machine_configuration/configuration_buttons.png");
+
     public FluidDrainerScreen(FluidDrainerMenu menu, PlayerInventory inventory, Text component) {
         super(menu, inventory, component,
                 new Identifier(EnergizedPowerMod.MODID, "textures/gui/container/fluid_drainer.png"),
                 8, 17);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isPointWithinBounds(-22, 2, 20, 20, mouseX, mouseY)) {
+                //Redstone Mode
+
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeBlockPos(handler.getBlockEntity().getPos());
+                ClientPlayNetworking.send(ModMessages.CHANGE_REDSTONE_MODE_ID, buf);
+                clicked = true;
+            }
+
+            if(clicked)
+                client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
@@ -40,6 +69,8 @@ public class FluidDrainerScreen extends AbstractGenericEnergyStorageHandledScree
 
         renderFluidMeterContent(poseStack, x, y);
         renderFluidMeterOverlay(poseStack, x, y);
+
+        renderConfiguration(poseStack, x, y, mouseX, mouseY);
     }
 
     private void renderFluidMeterContent(MatrixStack poseStack, int x, int y) {
@@ -107,6 +138,18 @@ public class FluidDrainerScreen extends AbstractGenericEnergyStorageHandledScree
         drawTexture(poseStack, x + 152, y + 17, 176, 53, 16, 52);
     }
 
+    private void renderConfiguration(MatrixStack poseStack, int x, int y, int mouseX, int mouseY) {
+        RedstoneMode redstoneMode = handler.getRedstoneMode();
+        int ordinal = redstoneMode.ordinal();
+
+        RenderSystem.setShaderTexture(0, CONFIGURATION_ICONS_TEXTURE);
+        if(isPointWithinBounds(-22, 2, 20, 20, mouseX, mouseY)) {
+            drawTexture(poseStack, x - 22, y + 2, 20 * ordinal, 20, 20, 20);
+        }else {
+            drawTexture(poseStack, x - 22, y + 2, 20 * ordinal, 0, 20, 20);
+        }
+    }
+
     @Override
     protected void drawMouseoverTooltip(MatrixStack poseStack, int mouseX, int mouseY) {
         super.drawMouseoverTooltip(poseStack, mouseX, mouseY);
@@ -130,6 +173,15 @@ public class FluidDrainerScreen extends AbstractGenericEnergyStorageHandledScree
             }
 
             components.add(tooltipComponent);
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }else if(isPointWithinBounds(-22, 2, 20, 20, mouseX, mouseY)) {
+            //Redstone Mode
+
+            RedstoneMode redstoneMode = handler.getRedstoneMode();
+
+            List<Text> components = new ArrayList<>(2);
+            components.add(Text.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.name().toLowerCase()));
 
             renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
         }
