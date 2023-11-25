9ba5ea81314a24fbe9dc79c893518a503c7c217a
JDDev0 <jddev0@dorneger.at>
2023-11-25 13:59:25 +0100

    Add Redstone Mode configuration to Advanced Uncharger

--- src/main/java/me/jddev0/ep/screen/AdvancedUnchargerScreen.java
+++ src/main/java/me/jddev0/ep/screen/AdvancedUnchargerScreen.java
@@ -1,18 +1,87 @@
 package me.jddev0.ep.screen;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.ChangeRedstoneModeC2SPacket;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @OnlyIn(Dist.CLIENT)
 public class AdvancedUnchargerScreen extends AbstractGenericEnergyStorageContainerScreen<AdvancedUnchargerMenu> {
+    private final ResourceLocation CONFIGURATION_ICONS_TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/machine_configuration/configuration_buttons.png");
+
     public AdvancedUnchargerScreen(AdvancedUnchargerMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component,
                 "tooltip.energizedpower.advanced_uncharger.items_energy_left.txt",
                 new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/advanced_uncharger.png"),
                 8, 17);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+                //Redstone Mode
+
+                ModMessages.sendToServer(new ChangeRedstoneModeC2SPacket(menu.getBlockEntity().getBlockPos()));
+                clicked = true;
+            }
+
+            if(clicked)
+                minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
+        super.renderBg(poseStack, partialTick, mouseX, mouseY);
+
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        renderConfiguration(poseStack, x, y, mouseX, mouseY);
+    }
+
+    private void renderConfiguration(PoseStack poseStack, int x, int y, int mouseX, int mouseY) {
+        RedstoneMode redstoneMode = menu.getRedstoneMode();
+        int ordinal = redstoneMode.ordinal();
+
+        RenderSystem.setShaderTexture(0, CONFIGURATION_ICONS_TEXTURE);
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            blit(poseStack, x - 22, y + 2, 20 * ordinal, 20, 20, 20);
+        }else {
+            blit(poseStack, x - 22, y + 2, 20 * ordinal, 0, 20, 20);
+        }
+    }
+
+    @Override
+    protected void renderTooltip(PoseStack poseStack, int mouseX, int mouseY) {
+        super.renderTooltip(poseStack, mouseX, mouseY);
+
+        if(isHovering(-22, 2, 20, 20, mouseX, mouseY)) {
+            //Redstone Mode
+
+            RedstoneMode redstoneMode = menu.getRedstoneMode();
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.name().toLowerCase()));
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }
     }
 }
