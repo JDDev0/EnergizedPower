c0bcb042959340e8dfb9ea7cf1c63521c5709bf7
JDDev0 <jddev0@dorneger.at>
2023-11-26 09:13:44 +0100

    Fix machines placed next to an active redstone signal will not connect to cables and pipes

--- src/main/java/me/jddev0/ep/block/BlockPlacerBlock.java
+++ src/main/java/me/jddev0/ep/block/BlockPlacerBlock.java
@@ -99,18 +99,9 @@ public class BlockPlacerBlock extends BaseEntityBlock {
     }
 
     @Override
-    public void onPlace(BlockState selfState, Level level, BlockPos selfPos, BlockState oldState, boolean isMoving) {
-        if(level.isClientSide())
-            return;
-
-        boolean isPowered = level.hasNeighborSignal(selfPos);
-        if(isPowered != selfState.getValue(POWERED))
-            level.setBlock(selfPos, selfState.setValue(POWERED, isPowered), 2);
-    }
-
-    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite()).
+                setValue(POWERED, context.getLevel().hasNeighborSignal(context.getClickedPos()));
     }
 
     @Override
