a445c13603fc43a71522d013d6c65b984be50ee7
JDDev0 <jddev0@dorneger.at>
2023-12-01 20:42:49 +0100

    Fix toLowerCase causes crash in mod initalization for some system default locales
    (cherry picked from commit e58c13257cdb61eeb2851fb5c579bd481eb953a1)

--- src/main/java/me/jddev0/ep/screen/StoneSolidifierScreen.java
+++ src/main/java/me/jddev0/ep/screen/StoneSolidifierScreen.java
@@ -279,7 +279,7 @@ public class StoneSolidifierScreen extends AbstractGenericEnergyStorageContainer
             RedstoneMode redstoneMode = menu.getRedstoneMode();
 
             List<Component> components = new ArrayList<>(2);
-            components.add(Component.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.name().toLowerCase()));
+            components.add(Component.translatable("tooltip.energizedpower.machine_configuration.redstone_mode." + redstoneMode.getSerializedName()));
 
             guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
         }
