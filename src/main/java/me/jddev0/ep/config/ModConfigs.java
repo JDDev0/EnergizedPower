4c2d42642003341f55ee6b3a40d250478602ef59
JDDev0 <jddev0@dorneger.at>
2024-01-13 14:14:29 +0100

    Change the default tank capacity of the Stone Solidifer to 8000 mB
    (cherry picked from commit c682c16c2f5a0a55db62426fd08f6effb5df541c)

--- src/main/java/me/jddev0/ep/config/ModConfigs.java
+++ src/main/java/me/jddev0/ep/config/ModConfigs.java
@@ -664,7 +664,7 @@ public final class ModConfigs {
             "block.stone_solidifier", "Stone Solidifier", 128
     );
     public static final ConfigValue<Integer> COMMON_STONE_SOLIDIFIER_TANK_CAPACITY = registerFluidTankCapacityConfigValue(
-            "block.stone_solidifier", "Stone Solidifier", 4
+            "block.stone_solidifier", "Stone Solidifier", 8
     );
     public static final ConfigValue<Integer> COMMON_CSTONE_SOLIDIFIER_RECIPE_DURATION = registerRecipeDurationConfigValue(
             "block.stone_solidifier", "Stone Solidifier", 25
