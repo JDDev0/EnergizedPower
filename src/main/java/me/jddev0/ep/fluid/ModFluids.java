79b9ba5408974d2ab05a3061ef1f2d2e85b2c87d
JDDev0 <jddev0@dorneger.at>
2024-01-21 19:05:19 +0100

    Add Dirty Water fluid

---
+++ src/main/java/me/jddev0/ep/fluid/ModFluids.java
@@ -0,0 +1,43 @@
+package me.jddev0.ep.fluid;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public final class ModFluids {
+    private ModFluids() {}
+
+    public static final FlowableFluid DIRTY_WATER = registerFluid("dirty_water", new DirtyWaterFluid.Source());
+    public static final FlowableFluid FLOWING_DIRTY_WATER = registerFluid("flowing_dirty_water", new DirtyWaterFluid.Flowing());
+    public static final FluidBlock DIRTY_WATER_BLOCK = createBlock("dirty_water",
+            new DirtyWaterFluidBlock(DIRTY_WATER, FabricBlockSettings.copyOf(Blocks.WATER)));
+    public static final BucketItem DIRTY_WATER_BUCKET_ITEM = createItem("dirty_water_bucket",
+            new BucketItem(DIRTY_WATER, new FabricItemSettings().recipeRemainder(Items.BUCKET).maxCount(1)));
+
+    private static <T extends Fluid> T registerFluid(String name, T fluid) {
+        return Registry.register(Registries.FLUID, new Identifier(EnergizedPowerMod.MODID, name), fluid);
+    }
+
+    private static <T extends Block> T createBlock(String name, T block) {
+        return Registry.register(Registries.BLOCK, new Identifier(EnergizedPowerMod.MODID, name), block);
+    }
+
+    public static <T extends Item> T createItem(String name, T item) {
+        return Registry.register(Registries.ITEM, new Identifier(EnergizedPowerMod.MODID, name), item);
+    }
+
+    public static void register() {
+
+    }
+}
