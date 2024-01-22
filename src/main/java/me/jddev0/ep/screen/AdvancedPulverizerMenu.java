bd2124f573eb191cc53d80db17801a56bc9c7f58
JDDev0 <jddev0@dorneger.at>
2024-01-22 19:22:10 +0100

    Add Advanced Pulverizer

---
+++ src/main/java/me/jddev0/ep/screen/AdvancedPulverizerMenu.java
@@ -0,0 +1,168 @@
+package me.jddev0.ep.screen;
+
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.block.entity.AdvancedPulverizerBlockEntity;
+import me.jddev0.ep.fluid.FluidStack;
+import me.jddev0.ep.inventory.ConstraintInsertSlot;
+import me.jddev0.ep.machine.configuration.RedstoneMode;
+import me.jddev0.ep.recipe.PulverizerRecipe;
+import me.jddev0.ep.util.ByteUtils;
+import me.jddev0.ep.util.RecipeUtils;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.ArrayPropertyDelegate;
+import net.minecraft.screen.PropertyDelegate;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.world.World;
+
+public class AdvancedPulverizerMenu extends ScreenHandler implements EnergyStorageConsumerIndicatorBarMenu {
+    private final AdvancedPulverizerBlockEntity blockEntity;
+    private final Inventory inv;
+    private final World level;
+    private final PropertyDelegate data;
+
+    public AdvancedPulverizerMenu(int id, PlayerInventory inv, PacketByteBuf buf) {
+        this(id, inv.player.getWorld().getBlockEntity(buf.readBlockPos()), inv, new SimpleInventory(3) {
+            @Override
+            public boolean isValid(int slot, ItemStack stack) {
+                return switch(slot) {
+                    case 0 -> RecipeUtils.isIngredientOfAny(inv.player.getWorld(), PulverizerRecipe.Type.INSTANCE, stack);
+                    case 1, 2 -> false;
+                    default -> super.isValid(slot, stack);
+                };
+            }
+        }, new ArrayPropertyDelegate(10));
+    }
+
+    public AdvancedPulverizerMenu(int id, BlockEntity blockEntity, PlayerInventory playerInventory, Inventory inv, PropertyDelegate data) {
+        super(ModMenuTypes.ADVANCED_PULVERIZER_MENU, id);
+
+        this.blockEntity = (AdvancedPulverizerBlockEntity)blockEntity;
+
+        this.inv = inv;
+        checkSize(this.inv, 3);
+        checkDataCount(data, 10);
+        this.level = playerInventory.player.getWorld();
+        this.inv.onOpen(playerInventory.player);
+        this.data = data;
+
+        addPlayerInventory(playerInventory);
+        addPlayerHotbar(playerInventory);
+
+        addSlot(new ConstraintInsertSlot(this.inv, 0, 66, 35));
+        addSlot(new ConstraintInsertSlot(this.inv, 1, 126, 25));
+        addSlot(new ConstraintInsertSlot(this.inv, 2, 126, 49));
+
+        addProperties(this.data);
+    }
+
+    @Override
+    public long getEnergy() {
+        return blockEntity.getEnergy();
+    }
+
+    @Override
+    public long getCapacity() {
+        return blockEntity.getCapacity();
+    }
+
+    @Override
+    public long getEnergyIndicatorBarValue() {
+        return ByteUtils.from2ByteChunks((short)data.get(4), (short)data.get(5), (short)data.get(6), (short)data.get(7));
+    }
+
+    public FluidStack getFluid(int tank) {
+        return blockEntity.getFluid(tank);
+    }
+
+    public long getTankCapacity(int tank) {
+        return blockEntity.getTankCapacity(tank);
+    }
+
+    /**
+     * @return Same as isCrafting but energy requirements are ignored
+     */
+    public boolean isCraftingActive() {
+        return ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1)) > 0;
+    }
+
+    public boolean isCrafting() {
+        return ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1)) > 0 && data.get(8) == 1;
+    }
+
+    public int getScaledProgressArrowSize() {
+        int progress = ByteUtils.from2ByteChunks((short)data.get(0), (short)data.get(1));
+        int maxProgress = ByteUtils.from2ByteChunks((short)data.get(2), (short)data.get(3));
+        int progressArrowSize = 24;
+
+        return (maxProgress == 0 || progress == 0)?0:progress * progressArrowSize / maxProgress;
+    }
+
+    public RedstoneMode getRedstoneMode() {
+        return RedstoneMode.fromIndex(data.get(7));
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int index) {
+        Slot sourceSlot = slots.get(index);
+        if(sourceSlot == null || !sourceSlot.hasStack())
+            return ItemStack.EMPTY;
+
+        ItemStack sourceItem = sourceSlot.getStack();
+        ItemStack sourceItemCopy = sourceItem.copy();
+
+        if(index < 4 * 9) {
+            //Player inventory slot -> Merge into tile inventory
+            if(!insertItem(sourceItem, 4 * 9, 4 * 9 + 1, false)) {
+                //"+1" instead of "+3": Do not allow adding to output slots
+                return ItemStack.EMPTY;
+            }
+        }else if(index < 4 * 9 + 3) {
+            //Tile inventory slot -> Merge into player inventory
+            if(!insertItem(sourceItem, 0, 4 * 9, false)) {
+                return ItemStack.EMPTY;
+            }
+        }else {
+            throw new IllegalArgumentException("Invalid slot index");
+        }
+
+        if(sourceItem.getCount() == 0)
+            sourceSlot.setStack(ItemStack.EMPTY);
+        else
+            sourceSlot.markDirty();
+
+        sourceSlot.onTakeItem(player, sourceItem);
+
+        return sourceItemCopy;
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        return canUse(ScreenHandlerContext.create(level, blockEntity.getPos()), player, ModBlocks.ADVANCED_PULVERIZER);
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for(int i = 0;i < 3;i++) {
+            for(int j = 0;j < 9;j++) {
+                addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for(int i = 0;i < 9;i++) {
+            addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+    }
+
+    public BlockEntity getBlockEntity() {
+        return blockEntity;
+    }
+}
