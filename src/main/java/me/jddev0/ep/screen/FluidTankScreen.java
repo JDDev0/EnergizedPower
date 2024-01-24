2f83407f697f778b0288db10f87ce656f8e92e83
JDDev0 <jddev0@dorneger.at>
2024-01-24 17:04:24 +0100

    Add filter settings to Fluid Tanks

--- src/main/java/me/jddev0/ep/screen/FluidTankScreen.java
+++ src/main/java/me/jddev0/ep/screen/FluidTankScreen.java
@@ -5,20 +5,30 @@ import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Matrix4f;
 import me.jddev0.ep.EnergizedPowerMod;
 import me.jddev0.ep.block.FluidTankBlock;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.SetFluidTankCheckboxC2SPacket;
+import me.jddev0.ep.networking.packet.SetFluidTankFilterC2SPacket;
 import me.jddev0.ep.util.FluidUtils;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,11 +41,47 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
     public FluidTankScreen(FluidTankMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component);
 
-        TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/generic_fluid.png");
+        TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/fluid_tank.png");
     }
 
     public FluidTankBlock.Tier getTier() {
         return menu.getTier();
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isHovering(158, 16, 11, 11, mouseX, mouseY)) {
+                //Ignore NBT checkbox
+
+                ModMessages.sendToServer(new SetFluidTankCheckboxC2SPacket(menu.getBlockEntity().getBlockPos(), 0, !menu.isIgnoreNBT()));
+                clicked = true;
+            }
+
+            if(isHovering(151, 34, 18, 18, mouseX, mouseY)) {
+                //Fluid Filter
+
+                FluidStack fluidFilter = FluidStack.EMPTY;
+
+                ItemStack carriedItemStack = menu.getCarried();
+
+                LazyOptional<IFluidHandlerItem> fluidStorageLazyOptional = carriedItemStack.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
+                if(fluidStorageLazyOptional.isPresent()) {
+                    IFluidHandlerItem fluidStorage = fluidStorageLazyOptional.orElse(null);
+                    if(fluidStorage != null && fluidStorage.getTanks() > 0)
+                        fluidFilter = fluidStorage.getFluidInTank(0);
+                }
+
+                ModMessages.sendToServer(new SetFluidTankFilterC2SPacket(menu.getBlockEntity().getBlockPos(), fluidFilter));
+                clicked = true;
+            }
+
+            if(clicked)
+                minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
     }
 
     @Override
@@ -48,29 +94,36 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
 
         blit(poseStack, x, y, 0, 0, imageWidth, imageHeight);
 
-        renderFluidMeterContent(poseStack, x, y);
-        renderFluidMeterOverlay(poseStack, x, y);
+        for(int i = 0;i < 2;i++) {
+            renderFluidMeterContent(poseStack, x, y, i);
+            renderFluidMeterOverlay(poseStack, x, y, i);
+        }
+
+        renderCheckboxes(poseStack, x, y, mouseX, mouseY);
     }
 
-    private void renderFluidMeterContent(PoseStack poseStack, int x, int y) {
+    private void renderFluidMeterContent(PoseStack poseStack, int x, int y, int tank) {
         RenderSystem.enableBlend();
         poseStack.pushPose();
 
-        poseStack.translate(x + 80, y + 17, 0);
+        if(tank == 0)
+            poseStack.translate(x + 80, y + 17, 0);
+        else if(tank == 1)
+            poseStack.translate(x + 152, y + 19, 0);
 
-        renderFluidStack(poseStack);
+        renderFluidStack(poseStack, tank);
 
         poseStack.popPose();
         RenderSystem.setShaderColor(1.f, 1.f, 1.f, 1.f);
         RenderSystem.disableBlend();
     }
 
-    private void renderFluidStack(PoseStack poseStack) {
-        FluidStack fluidStack = menu.getFluid();
+    private void renderFluidStack(PoseStack poseStack, int tank) {
+        FluidStack fluidStack = menu.getFluid(tank);
         if(fluidStack.isEmpty())
             return;
 
-        int capacity = menu.getTankCapacity();
+        int capacity = menu.getTankCapacity(tank);
 
         Fluid fluid = fluidStack.getFluid();
         IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluid);
@@ -80,8 +133,12 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
 
         int fluidColorTint = fluidTypeExtensions.getTintColor(fluidStack);
 
-        int fluidMeterPos = 52 - ((fluidStack.getAmount() <= 0 || capacity == 0)?0:
-                (Math.min(fluidStack.getAmount(), capacity - 1) * 52 / capacity + 1));
+        int fluidMeterPos = switch(tank) {
+            case 0 -> 52 - ((fluidStack.getAmount() <= 0 || capacity == 0)?0:
+                    (Math.min(fluidStack.getAmount(), capacity - 1) * 52 / capacity + 1));
+            case 1 -> 16;
+            default -> 0;
+        };
 
         RenderSystem.setShaderTexture(0, InventoryMenu.BLOCK_ATLAS);
 
@@ -92,7 +149,7 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
 
         Matrix4f mat = poseStack.last().pose();
 
-        for(int yOffset = 52;yOffset > fluidMeterPos;yOffset -= 16) {
+        for(int yOffset = tank == 0?52:32;yOffset > fluidMeterPos;yOffset -= 16) {
             int height = Math.min(yOffset - fluidMeterPos, 16);
 
             float u0 = stillFluidSprite.getU0();
@@ -112,9 +169,20 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
         }
     }
 
-    private void renderFluidMeterOverlay(PoseStack poseStack, int x, int y) {
+    private void renderFluidMeterOverlay(PoseStack poseStack, int x, int y, int tank) {
         RenderSystem.setShaderTexture(0, TEXTURE);
-        blit(poseStack, x + 80, y + 17, 176, 0, 16, 52);
+        if(tank == 0)
+            blit(poseStack, x + 80, y + 17, 176, 0, 16, 52);
+        else if(tank == 1)
+            blit(poseStack, x + 152, y + 35, 176, 64, 16, 16);
+    }
+
+    private void renderCheckboxes(PoseStack poseStack, int x, int y, int mouseX, int mouseY) {
+        if(menu.isIgnoreNBT()) {
+            //Ignore NBT checkbox
+
+            blit(poseStack, x + 158, y + 16, 176, 53, 11, 11);
+        }
     }
 
     @Override
@@ -135,19 +203,51 @@ public class FluidTankScreen extends AbstractContainerScreen<FluidTankMenu> {
 
             List<Component> components = new ArrayList<>(2);
 
-            boolean fluidEmpty =  menu.getFluid().isEmpty();
+            boolean fluidEmpty =  menu.getFluid(0).isEmpty();
 
-            int fluidAmount = fluidEmpty?0:menu.getFluid().getAmount();
+            int fluidAmount = fluidEmpty?0:menu.getFluid(0).getAmount();
 
             Component tooltipComponent = Component.translatable("tooltip.energizedpower.fluid_meter.content_amount.txt",
-                    FluidUtils.getFluidAmountWithPrefix(fluidAmount), FluidUtils.getFluidAmountWithPrefix(menu.getTankCapacity()));
+                    FluidUtils.getFluidAmountWithPrefix(fluidAmount), FluidUtils.getFluidAmountWithPrefix(menu.getTankCapacity(0)));
 
             if(!fluidEmpty) {
-                tooltipComponent = Component.translatable(menu.getFluid().getTranslationKey()).append(" ").
+                tooltipComponent = Component.translatable(menu.getFluid(0).getTranslationKey()).append(" ").
                         append(tooltipComponent);
             }
 
             components.add(tooltipComponent);
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }
+
+        if(isHovering(158, 16, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.fluid_tanks.cbx.ignore_nbt"));
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }
+
+        if(isHovering(151, 34, 18, 18, mouseX, mouseY)) {
+            //Fluid Filter
+
+            List<Component> components = new ArrayList<>(2);
+
+            FluidStack fluidFilter = menu.getFluid(1);
+
+            if(fluidFilter.isEmpty())
+                components.add(Component.translatable("tooltip.energizedpower.fluid_tanks.fluid_filter.no_filter_set"));
+            else
+                components.add(Component.translatable("tooltip.energizedpower.fluid_tanks.fluid_filter.filter_set",
+                        Component.translatable(fluidFilter.getTranslationKey())));
+
+            components.add(Component.empty());
+
+            components.add(Component.translatable("tooltip.energizedpower.fluid_tanks.fluid_filter.txt.1").
+                    withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
+            components.add(Component.translatable("tooltip.energizedpower.fluid_tanks.fluid_filter.txt.2").
+                    withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC));
 
             renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
         }
