cfdbcc33b4eb6f0522cae56de7b63ea90f08ab04
JDDev0 <jddev0@dorneger.at>
2024-02-25 13:38:49 +0100

    Add Cutter feature: Cutters can break cables faster

--- src/main/java/me/jddev0/ep/item/CutterItem.java
+++ src/main/java/me/jddev0/ep/item/CutterItem.java
@@ -1,10 +1,12 @@
 package me.jddev0.ep.item;
 
+import me.jddev0.ep.block.CableBlock;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.Vanishable;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class CutterItem extends TieredItem implements Vanishable {
     private final RandomSource random = RandomSource.create();
@@ -25,5 +27,13 @@ public class CutterItem extends TieredItem implements Vanishable {
     @Override
     public boolean hasCraftingRemainingItem(ItemStack itemStack) {
         return true;
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack itemStack, BlockState blockState) {
+        if(blockState.getBlock() instanceof CableBlock)
+            return 15.f;
+
+        return super.getDestroySpeed(itemStack, blockState);
     }
 }
