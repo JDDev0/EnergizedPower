f3153bdbcbf2fbe8709479940b2b0d131b0ad7a9
JDDev0 <jddev0@dorneger.at>
2024-04-04 18:36:09 +0200

    Improve Creative Battery Box: Add Energy Production and Energy Consumption checkboxes

--- src/main/java/me/jddev0/ep/block/entity/CreativeBatteryBoxBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/CreativeBatteryBoxBlockEntity.java
@@ -2,9 +2,17 @@ package me.jddev0.ep.block.entity;
 
 import me.jddev0.ep.energy.EnergyStoragePacketUpdate;
 import me.jddev0.ep.energy.InfinityEnergyStorage;
+import me.jddev0.ep.machine.CheckboxUpdate;
+import me.jddev0.ep.screen.CreativeBatteryBoxMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,19 +23,75 @@ import net.minecraftforge.energy.IEnergyStorage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class CreativeBatteryBoxBlockEntity extends BlockEntity implements EnergyStoragePacketUpdate {
+public class CreativeBatteryBoxBlockEntity extends BlockEntity implements MenuProvider, EnergyStoragePacketUpdate,
+        CheckboxUpdate {
     private final InfinityEnergyStorage energyStorage;
     private LazyOptional<IEnergyStorage> lazyEnergyStorage = LazyOptional.empty();
+
+    protected final ContainerData data;
+
+    private boolean energyProduction = true;
+    private boolean energyConsumption;
 
     public CreativeBatteryBoxBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(ModBlockEntities.CREATIVE_BATTERY_BOX_ENTITY.get(), blockPos, blockState);
 
         energyStorage = new InfinityEnergyStorage() {
             @Override
+            public int extractEnergy(int maxExtract, boolean simulate) {
+                if(energyProduction)
+                    return super.extractEnergy(maxExtract, simulate);
+
+                return 0;
+            }
+
+            @Override
+            public int receiveEnergy(int maxReceive, boolean simulate) {
+                if(energyConsumption)
+                    return super.receiveEnergy(maxReceive, simulate);
+
+                return 0;
+            }
+
+            @Override
             protected void onChange() {
                 setChanged();
             }
         };
+        data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch(index) {
+                    case 0 -> energyProduction?1:0;
+                    case 1 -> energyConsumption?1:0;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch(index) {
+                    case 0 -> CreativeBatteryBoxBlockEntity.this.energyProduction = value != 0;
+                    case 1 -> CreativeBatteryBoxBlockEntity.this.energyConsumption = value != 0;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("container.energizedpower.creative_battery_box");
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+        return new CreativeBatteryBoxMenu(id, inventory, this, data);
     }
 
     @Override
@@ -57,6 +121,9 @@ public class CreativeBatteryBoxBlockEntity extends BlockEntity implements Energy
     protected void saveAdditional(CompoundTag nbt) {
         nbt.put("energy", energyStorage.saveNBT());
 
+        nbt.putBoolean("energy_production", energyProduction);
+        nbt.putBoolean("energy_consumption", energyConsumption);
+
         super.saveAdditional(nbt);
     }
 
@@ -65,6 +132,30 @@ public class CreativeBatteryBoxBlockEntity extends BlockEntity implements Energy
         super.load(nbt);
 
         energyStorage.loadNBT(nbt.get("energy"));
+
+        energyProduction = !nbt.contains("energy_production") || nbt.getBoolean("energy_production");
+        energyConsumption = nbt.getBoolean("energy_consumption");
+    }
+
+    public void setEnergyProduction(boolean energyProduction) {
+        this.energyProduction = energyProduction;
+        setChanged(level, getBlockPos(), getBlockState());
+    }
+
+    public void setEnergyConsumption(boolean energyConsumption) {
+        this.energyConsumption = energyConsumption;
+        setChanged(level, getBlockPos(), getBlockState());
+    }
+
+    @Override
+    public void setCheckbox(int checkboxId, boolean checked) {
+        switch(checkboxId) {
+            //Energy Production
+            case 0 -> setEnergyProduction(checked);
+
+            //Energy Consumption
+            case 1 -> setEnergyConsumption(checked);
+        }
     }
 
     public static void tick(Level level, BlockPos blockPos, BlockState state, CreativeBatteryBoxBlockEntity blockEntity) {
@@ -76,6 +167,9 @@ public class CreativeBatteryBoxBlockEntity extends BlockEntity implements Energy
 
     private static void transferInfiniteEnergy(Level level, BlockPos blockPos, BlockState state, CreativeBatteryBoxBlockEntity blockEntity) {
         if(level.isClientSide)
+            return;
+
+        if(!blockEntity.energyProduction)
             return;
 
         for(Direction direction:Direction.values()) {
---
+++ src/main/java/me/jddev0/ep/screen/CreativeBatteryBoxScreen.java
@@ -0,0 +1,120 @@
+package me.jddev0.ep.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.SetCheckboxC2SPacket;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@OnlyIn(Dist.CLIENT)
+public class CreativeBatteryBoxScreen extends AbstractContainerScreen<CreativeBatteryBoxMenu> {
+    private final ResourceLocation TEXTURE;
+
+    public CreativeBatteryBoxScreen(CreativeBatteryBoxMenu menu, Inventory inventory, Component component) {
+        super(menu, inventory, component);
+
+        TEXTURE = new ResourceLocation(EnergizedPowerMod.MODID, "textures/gui/container/creative_battery_box.png");
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isHovering(10, 28, 11, 11, mouseX, mouseY)) {
+                //Energy Production checkbox
+
+                ModMessages.sendToServer(new SetCheckboxC2SPacket(menu.getBlockEntity().getBlockPos(), 0, !menu.isEnergyProduction()));
+                clicked = true;
+            }else if(isHovering(10, 46, 11, 11, mouseX, mouseY)) {
+                //Energy Consumption checkbox
+
+                ModMessages.sendToServer(new SetCheckboxC2SPacket(menu.getBlockEntity().getBlockPos(), 1, !menu.isEnergyConsumption()));
+                clicked = true;
+            }
+
+            if(clicked)
+                minecraft.getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    protected void renderBg(PoseStack poseStack, float partialTick, int mouseX, int mouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.f, 1.f, 1.f, 1.f);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        blit(poseStack, x, y, 0, 0, imageWidth, imageHeight);
+
+        renderCheckboxes(poseStack, x, y, mouseX, mouseY);
+        renderCheckboxLabels(poseStack, x, y, mouseX, mouseY);
+    }
+
+    private void renderCheckboxes(PoseStack poseStack, int x, int y, int mouseX, int mouseY) {
+        if(menu.isEnergyProduction()) {
+            //Energy Production checkbox
+
+            blit(poseStack, x + 10, y + 28, 176, 0, 11, 11);
+        }
+
+        if(menu.isEnergyConsumption()) {
+            //Energy Consumption checkbox
+
+            blit(poseStack, x + 10, y + 46, 176, 0, 11, 11);
+        }
+    }
+
+    private void renderCheckboxLabels(PoseStack poseStack, int x, int y, int mouseX, int mouseY) {
+        font.draw(poseStack, Component.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_production"),
+                x + 25, y + 30, 0);
+
+        font.draw(poseStack, Component.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_consumption"),
+                x + 25, y + 48, 0);
+    }
+
+    @Override
+    public void render(PoseStack poseStack, int mouseX, int mouseY, float delta) {
+        renderBackground(poseStack);
+
+        super.render(poseStack, mouseX, mouseY, delta);
+
+        renderTooltip(poseStack, mouseX, mouseY);
+    }
+
+    @Override
+    protected void renderTooltip(PoseStack poseStack, int mouseX, int mouseY) {
+        super.renderTooltip(poseStack, mouseX, mouseY);
+
+        if(isHovering(10, 28, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_production"));
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }else if(isHovering(10, 46, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Component> components = new ArrayList<>(2);
+            components.add(Component.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_consumption"));
+
+            renderTooltip(poseStack, components, Optional.empty(), mouseX, mouseY);
+        }
+    }
+}
