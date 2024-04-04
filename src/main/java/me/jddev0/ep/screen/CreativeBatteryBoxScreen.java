7a59a97f9363cc46867d3477b7829b1854495ef1
JDDev0 <jddev0@dorneger.at>
2024-04-04 17:34:28 +0200

    Improve Creative Battery Box: Add Energy Production and Energy Consumption checkboxes

---
+++ src/main/java/me/jddev0/ep/screen/CreativeBatteryBoxScreen.java
@@ -0,0 +1,126 @@
+package me.jddev0.ep.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.networking.ModMessages;
+import me.jddev0.ep.networking.packet.SetCheckboxC2SPacket;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Environment(EnvType.CLIENT)
+public class CreativeBatteryBoxScreen extends HandledScreen<CreativeBatteryBoxMenu> {
+    private final Identifier TEXTURE;
+
+    public CreativeBatteryBoxScreen(CreativeBatteryBoxMenu menu, PlayerInventory inventory, Text component) {
+        super(menu, inventory, component);
+
+        TEXTURE = new Identifier(EnergizedPowerMod.MODID, "textures/gui/container/creative_battery_box.png");
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+        if(mouseButton == 0) {
+            boolean clicked = false;
+            if(isPointWithinBounds(10, 28, 11, 11, mouseX, mouseY)) {
+                //Energy Production checkbox
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeBlockPos(handler.getBlockEntity().getPos());
+                buf.writeInt(0);
+                buf.writeBoolean(!handler.isEnergyProduction());
+                ClientPlayNetworking.send(ModMessages.SET_CHECKBOX_ID, buf);
+                clicked = true;
+            }else if(isPointWithinBounds(10, 46, 11, 11, mouseX, mouseY)) {
+                //Energy Consumption checkbox
+                PacketByteBuf buf = PacketByteBufs.create();
+                buf.writeBlockPos(handler.getBlockEntity().getPos());
+                buf.writeInt(1);
+                buf.writeBoolean(!handler.isEnergyConsumption());
+                ClientPlayNetworking.send(ModMessages.SET_CHECKBOX_ID, buf);
+                clicked = true;
+            }
+
+            if(clicked)
+                client.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.f));
+        }
+
+        return super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    protected void drawBackground(DrawContext drawContext, float partialTick, int mouseX, int mouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderColor(1.f, 1.f, 1.f, 1.f);
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+
+        drawContext.drawTexture(TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
+
+        renderCheckboxes(drawContext, x, y, mouseX, mouseY);
+        renderCheckboxLabels(drawContext, x, y, mouseX, mouseY);
+    }
+
+    private void renderCheckboxes(DrawContext drawContext, int x, int y, int mouseX, int mouseY) {
+        if(handler.isEnergyProduction()) {
+            //Energy Production checkbox
+
+            drawContext.drawTexture(TEXTURE, x + 10, y + 28, 176, 0, 11, 11);
+        }
+
+        if(handler.isEnergyConsumption()) {
+            //Energy Consumption checkbox
+
+            drawContext.drawTexture(TEXTURE, x + 10, y + 46, 176, 0, 11, 11);
+        }
+    }
+
+    private void renderCheckboxLabels(DrawContext drawContext, int x, int y, int mouseX, int mouseY) {
+        drawContext.drawText(textRenderer, Text.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_production"),
+                x + 25, y + 30, 0, false);
+
+        drawContext.drawText(textRenderer, Text.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_consumption"),
+                x + 25, y + 48, 0, false);
+    }
+
+    @Override
+    public void render(DrawContext drawContext, int mouseX, int mouseY, float delta) {
+        super.render(drawContext, mouseX, mouseY, delta);
+
+        drawMouseoverTooltip(drawContext, mouseX, mouseY);
+    }
+
+    @Override
+    protected void drawMouseoverTooltip(DrawContext drawContext, int mouseX, int mouseY) {
+        super.drawMouseoverTooltip(drawContext, mouseX, mouseY);
+
+        if(isPointWithinBounds(10, 28, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Text> components = new ArrayList<>(2);
+            components.add(Text.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_production"));
+
+            drawContext.drawTooltip(textRenderer, components, Optional.empty(), mouseX, mouseY);
+        }else if(isPointWithinBounds(10, 46, 11, 11, mouseX, mouseY)) {
+            //Ignore NBT checkbox
+
+            List<Text> components = new ArrayList<>(2);
+            components.add(Text.translatable("tooltip.energizedpower.creative_battery_box.cbx.energy_consumption"));
+
+            drawContext.drawTooltip(textRenderer, components, Optional.empty(), mouseX, mouseY);
+        }
+    }
+}
