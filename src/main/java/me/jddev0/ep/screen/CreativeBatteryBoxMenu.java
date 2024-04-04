9a4c12be7c21cffc16f633dfd9edd550481ef226
JDDev0 <jddev0@dorneger.at>
2024-04-04 17:11:52 +0200

    Improve Creative Battery Box: Add Energy Production and Energy Consumption checkboxes

---
+++ src/main/java/me/jddev0/ep/screen/CreativeBatteryBoxMenu.java
@@ -0,0 +1,71 @@
+package me.jddev0.ep.screen;
+
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.block.entity.CreativeBatteryBoxBlockEntity;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public class CreativeBatteryBoxMenu extends AbstractContainerMenu {
+    private final CreativeBatteryBoxBlockEntity blockEntity;
+    private final Level level;
+    private final ContainerData data;
+
+    public CreativeBatteryBoxMenu(int id, Inventory inv, FriendlyByteBuf buffer) {
+        this(id, inv, inv.player.level().getBlockEntity(buffer.readBlockPos()), new SimpleContainerData(2));
+    }
+
+    public CreativeBatteryBoxMenu(int id, Inventory inv, BlockEntity blockEntity, ContainerData data) {
+        super(ModMenuTypes.CREATIVE_BATTERY_BOX_MENU.get(), id);
+
+        checkContainerDataCount(data, 2);
+        this.blockEntity = (CreativeBatteryBoxBlockEntity)blockEntity;
+        this.level = inv.player.level();
+        this.data = data;
+
+        addPlayerInventory(inv);
+        addPlayerHotbar(inv);
+
+        addDataSlots(this.data);
+    }
+
+    public boolean isEnergyProduction() {
+        return data.get(0) != 0;
+    }
+
+    public boolean isEnergyConsumption() {
+        return data.get(1) != 0;
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int index) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return stillValid(ContainerLevelAccess.create(level, blockEntity.getBlockPos()), player, ModBlocks.CREATIVE_BATTERY_BOX.get());
+    }
+
+    private void addPlayerInventory(Inventory playerInventory) {
+        for(int i = 0;i < 3;i++) {
+            for(int j = 0;j < 9;j++) {
+                addSlot(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+    }
+
+    private void addPlayerHotbar(Inventory playerInventory) {
+        for(int i = 0;i < 9;i++) {
+            addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+    }
+
+    public BlockEntity getBlockEntity() {
+        return blockEntity;
+    }
+}
