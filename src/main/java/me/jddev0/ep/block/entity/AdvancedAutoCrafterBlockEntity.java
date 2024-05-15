20f9784f7d02804fc07a54404f146db07c1bd7c1
JDDev0 <jddev0@dorneger.at>
2024-05-15 21:31:34 +0200

    Fix missing "setChanged()" call in AutoCrafterBlockEntity and AdvancedAutoCrafterBlockEntity
    (cherry picked from commit 6381fc52cdb49030ef73a77bffb06b87003efcb8)

--- src/main/java/me/jddev0/ep/block/entity/AdvancedAutoCrafterBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/AdvancedAutoCrafterBlockEntity.java
@@ -381,6 +381,8 @@ public class AdvancedAutoCrafterBlockEntity
 
                 if(blockEntity.craftingRecipe[i] == null)
                     blockEntity.resetProgress(i);
+
+                setChanged(level, blockPos, state);
             }
 
             int itemCount = 0;
