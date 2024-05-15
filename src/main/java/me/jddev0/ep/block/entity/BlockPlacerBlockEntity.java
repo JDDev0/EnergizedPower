82c2e3b4c73239d6f2bb3c724f2f7839b110d37a
JDDev0 <jddev0@dorneger.at>
2024-05-15 19:03:09 +0200

    Add Worker Machine Block Entity base class

--- src/main/java/me/jddev0/ep/block/entity/BlockPlacerBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/BlockPlacerBlockEntity.java
@@ -1,10 +1,9 @@
 package me.jddev0.ep.block.entity;
 
 import me.jddev0.ep.block.BlockPlacerBlock;
-import me.jddev0.ep.block.entity.base.ConfigurableUpgradableInventoryEnergyStorageBlockEntity;
+import me.jddev0.ep.block.entity.base.WorkerMachineBlockEntity;
 import me.jddev0.ep.inventory.InputOutputItemHandler;
 import me.jddev0.ep.config.ModConfigs;
-import me.jddev0.ep.energy.ReceiveOnlyEnergyStorage;
 import me.jddev0.ep.machine.CheckboxUpdate;
 import me.jddev0.ep.machine.configuration.ComparatorMode;
 import me.jddev0.ep.machine.configuration.RedstoneMode;
@@ -16,7 +15,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.IntTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.*;
@@ -27,7 +25,6 @@ import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.DirectionalPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.energy.IEnergyStorage;
 import net.neoforged.neoforge.items.IItemHandler;
@@ -36,33 +33,29 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Optional;
+
+import static me.jddev0.ep.block.entity.base.WorkerMachineBlockEntity.NoWorkData;
 
 public class BlockPlacerBlockEntity
-        extends ConfigurableUpgradableInventoryEnergyStorageBlockEntity<ReceiveOnlyEnergyStorage, ItemStackHandler>
+        extends WorkerMachineBlockEntity<NoWorkData>
         implements MenuProvider, CheckboxUpdate {
     private static final List<@NotNull ResourceLocation> PLACEMENT_BLACKLIST = ModConfigs.COMMON_BLOCK_PLACER_PLACEMENT_BLACKLIST.getValue();
-
-    private static final int ENERGY_USAGE_PER_TICK = ModConfigs.COMMON_BLOCK_PLACER_ENERGY_CONSUMPTION_PER_TICK.getValue();
-
-    private static final int PLACEMENT_DURATION = ModConfigs.COMMON_BLOCK_PLACER_PLACEMENT_DURATION.getValue();
 
     private final IItemHandler itemHandlerSided = new InputOutputItemHandler(itemHandler, (i, stack) -> true, i -> false);
 
     protected final ContainerData data;
-    private int progress;
-    private int maxProgress;
-    private int energyConsumptionLeft = -1;
-    private boolean hasEnoughEnergy;
     private boolean inverseRotation;
 
     public BlockPlacerBlockEntity(BlockPos blockPos, BlockState blockState) {
         super(
                 ModBlockEntities.BLOCK_PLACER_ENTITY.get(), blockPos, blockState,
 
+                1, ModConfigs.COMMON_BLOCK_PLACER_PLACEMENT_DURATION.getValue(),
+
                 ModConfigs.COMMON_BLOCK_PLACER_CAPACITY.getValue(),
                 ModConfigs.COMMON_BLOCK_PLACER_TRANSFER_RATE.getValue(),
-
-                1,
+                ModConfigs.COMMON_BLOCK_PLACER_ENERGY_CONSUMPTION_PER_TICK.getValue(),
 
                 UpgradeModuleModifier.SPEED,
                 UpgradeModuleModifier.ENERGY_CONSUMPTION,
@@ -108,29 +101,6 @@ public class BlockPlacerBlockEntity
     }
 
     @Override
-    protected ReceiveOnlyEnergyStorage initEnergyStorage() {
-        return new ReceiveOnlyEnergyStorage(0, baseEnergyCapacity, baseEnergyTransferRate) {
-            @Override
-            public int getCapacity() {
-                return Math.max(1, (int)Math.ceil(capacity * upgradeModuleInventory.getModifierEffectProduct(
-                        UpgradeModuleModifier.ENERGY_CAPACITY)));
-            }
-
-            @Override
-            public int getMaxReceive() {
-                return Math.max(1, (int)Math.ceil(maxReceive * upgradeModuleInventory.getModifierEffectProduct(
-                        UpgradeModuleModifier.ENERGY_TRANSFER_RATE)));
-            }
-
-            @Override
-            protected void onChange() {
-                setChanged();
-                syncEnergyToPlayers(32);
-            }
-        };
-    }
-
-    @Override
     protected ItemStackHandler initInventoryStorage() {
         return new ItemStackHandler(slotCount) {
             @Override
@@ -152,7 +122,7 @@ public class BlockPlacerBlockEntity
                 if(slot == 0) {
                     ItemStack itemStack = getStackInSlot(slot);
                     if(level != null && !stack.isEmpty() && !itemStack.isEmpty() && !ItemStack.isSameItemSameComponents(stack, itemStack))
-                        resetProgress(worldPosition, level.getBlockState(worldPosition));
+                        resetProgress();
                 }
 
                 super.setStackInSlot(slot, stack);
@@ -193,10 +163,6 @@ public class BlockPlacerBlockEntity
     protected void saveAdditional(@NotNull CompoundTag nbt, @NotNull HolderLookup.Provider registries) {
         super.saveAdditional(nbt, registries);
 
-        nbt.put("recipe.progress", IntTag.valueOf(progress));
-        nbt.put("recipe.max_progress", IntTag.valueOf(maxProgress));
-        nbt.put("recipe.energy_consumption_left", IntTag.valueOf(energyConsumptionLeft));
-
         nbt.putBoolean("inverse_rotation", inverseRotation);
     }
 
@@ -204,151 +170,95 @@ public class BlockPlacerBlockEntity
     protected void loadAdditional(@NotNull CompoundTag nbt, @NotNull HolderLookup.Provider registries) {
         super.loadAdditional(nbt, registries);
 
-        progress = nbt.getInt("recipe.progress");
-        maxProgress = nbt.getInt("recipe.max_progress");
-        energyConsumptionLeft = nbt.getInt("recipe.energy_consumption_left");
-
         inverseRotation = nbt.getBoolean("inverse_rotation");
     }
 
-    public static void tick(Level level, BlockPos blockPos, BlockState state, BlockPlacerBlockEntity blockEntity) {
-        if(level.isClientSide)
-            return;
-
-        if(!blockEntity.redstoneMode.isActive(state.getValue(BlockPlacerBlock.POWERED)))
-            return;
-
-        if(hasRecipe(blockEntity)) {
-            if(blockEntity.maxProgress == 0)
-                blockEntity.maxProgress = Math.max(1, (int)Math.ceil(PLACEMENT_DURATION /
-                        blockEntity.upgradeModuleInventory.getModifierEffectProduct(UpgradeModuleModifier.SPEED)));
-
-            int energyConsumptionPerTick = Math.max(1, (int)Math.ceil(ENERGY_USAGE_PER_TICK *
-                    blockEntity.upgradeModuleInventory.getModifierEffectProduct(UpgradeModuleModifier.ENERGY_CONSUMPTION)));
-
-            if(blockEntity.energyConsumptionLeft < 0)
-                blockEntity.energyConsumptionLeft = energyConsumptionPerTick * blockEntity.maxProgress;
-
-            if(energyConsumptionPerTick <= blockEntity.energyStorage.getEnergy()) {
-                blockEntity.hasEnoughEnergy = true;
-
-                if(blockEntity.progress < 0 || blockEntity.maxProgress < 0 || blockEntity.energyConsumptionLeft < 0) {
-                    //Reset progress for invalid values
-
-                    blockEntity.resetProgress(blockPos, state);
-                    setChanged(level, blockPos, state);
-
-                    return;
-                }
-
-                blockEntity.energyStorage.setEnergy(blockEntity.energyStorage.getEnergy() - energyConsumptionPerTick);
-                blockEntity.energyConsumptionLeft -= energyConsumptionPerTick;
-
-                if(blockEntity.progress < blockEntity.maxProgress)
-                    blockEntity.progress++;
-
-                if(blockEntity.progress >= blockEntity.maxProgress) {
-                    ItemStack itemStack = blockEntity.itemHandler.getStackInSlot(0);
-                    if(itemStack.isEmpty()) {
-                        blockEntity.energyConsumptionLeft = energyConsumptionPerTick;
-                        setChanged(level, blockPos, state);
-
-                        return;
-                    }
-
-                    BlockPos blockPosPlacement = blockEntity.getBlockPos().relative(blockEntity.getBlockState().getValue(BlockPlacerBlock.FACING));
-
-                    BlockItem blockItem = (BlockItem)itemStack.getItem();
-                    final Direction direction;
-
-                    if(blockEntity.inverseRotation) {
-                        direction = switch(state.getValue(BlockPlacerBlock.FACING)) {
-                            case DOWN -> Direction.UP;
-                            case UP -> Direction.DOWN;
-                            case NORTH -> Direction.SOUTH;
-                            case SOUTH -> Direction.NORTH;
-                            case WEST -> Direction.EAST;
-                            case EAST -> Direction.WEST;
-                        };
-                    }else {
-                        direction = state.getValue(BlockPlacerBlock.FACING);
-                    }
-
-                    InteractionResult result = blockItem.place(new DirectionalPlaceContext(level, blockPosPlacement, direction, itemStack, direction) {
-                        @Override
-                        public @NotNull Direction getNearestLookingDirection() {
-                            return direction;
-                        }
-
-                        @Override
-                        public @NotNull Direction @NotNull [] getNearestLookingDirections() {
-                            return switch (direction) {
-                                case DOWN ->
-                                        new Direction[] { Direction.DOWN, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.UP };
-                                case UP ->
-                                        new Direction[] { Direction.UP, Direction.DOWN, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST };
-                                case NORTH ->
-                                        new Direction[] { Direction.NORTH, Direction.EAST, Direction.WEST, Direction.UP, Direction.DOWN, Direction.SOUTH };
-                                case SOUTH ->
-                                        new Direction[] { Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.UP, Direction.DOWN, Direction.NORTH };
-                                case WEST ->
-                                        new Direction[] { Direction.WEST, Direction.SOUTH, Direction.UP, Direction.DOWN, Direction.NORTH, Direction.EAST };
-                                case EAST ->
-                                        new Direction[] { Direction.EAST, Direction.SOUTH, Direction.UP, Direction.DOWN, Direction.NORTH, Direction.WEST };
-                            };
-                        }
-
-                        @Override
-                        public boolean replacingClickedOnBlock() {
-                            return false;
-                        }
-                    });
-
-                    if(result == InteractionResult.FAIL) {
-                        blockEntity.energyConsumptionLeft = energyConsumptionPerTick;
-                        setChanged(level, blockPos, state);
-
-                        return;
-                    }
-
-                    blockEntity.itemHandler.setStackInSlot(0, itemStack);
-                    blockEntity.resetProgress(blockPos, state);
-                }
-
-                setChanged(level, blockPos, state);
-            }else {
-                blockEntity.hasEnoughEnergy = false;
-                setChanged(level, blockPos, state);
-            }
-        }else {
-            blockEntity.resetProgress(blockPos, state);
-            setChanged(level, blockPos, state);
-        }
-    }
-
-    private void resetProgress(BlockPos blockPos, BlockState state) {
-        progress = 0;
-        maxProgress = 0;
-        energyConsumptionLeft = -1;
-        hasEnoughEnergy = true;
-    }
-
-    private static boolean hasRecipe(BlockPlacerBlockEntity blockEntity) {
-        ItemStack itemStack = blockEntity.itemHandler.getStackInSlot(0);
-        if(itemStack.isEmpty())
-            return false;
-
-        if(!(itemStack.getItem() instanceof BlockItem blockItemStack))
+    @Override
+    protected boolean hasWork() {
+        ItemStack itemStack = itemHandler.getStackInSlot(0);
+        if(itemStack.isEmpty() || !(itemStack.getItem() instanceof BlockItem blockItemStack))
             return false;
 
         return !PLACEMENT_BLACKLIST.contains(BuiltInRegistries.BLOCK.getKey(blockItemStack.getBlock()));
     }
 
     @Override
-    protected void updateUpgradeModules() {
-        resetProgress(getBlockPos(), getBlockState());
+    protected Optional<NoWorkData> getCurrentWorkData() {
+        return Optional.of(NoWorkData.INSTANCE);
+    }
 
-        super.updateUpgradeModules();
+    @Override
+    protected void onWorkStarted(NoWorkData workData) {}
+
+    @Override
+    protected void onWorkCompleted(NoWorkData workData) {
+        int energyConsumptionPerTick = getEnergyConsumptionFor(workData);
+
+        ItemStack itemStack = itemHandler.getStackInSlot(0);
+        if(itemStack.isEmpty()) {
+            energyConsumptionLeft = energyConsumptionPerTick;
+            setChanged();
+
+            return;
+        }
+
+        BlockPos blockPosPlacement = getBlockPos().relative(getBlockState().getValue(BlockPlacerBlock.FACING));
+
+        BlockItem blockItem = (BlockItem)itemStack.getItem();
+        final Direction direction;
+
+        if(inverseRotation) {
+            direction = switch(getBlockState().getValue(BlockPlacerBlock.FACING)) {
+                case DOWN -> Direction.UP;
+                case UP -> Direction.DOWN;
+                case NORTH -> Direction.SOUTH;
+                case SOUTH -> Direction.NORTH;
+                case WEST -> Direction.EAST;
+                case EAST -> Direction.WEST;
+            };
+        }else {
+            direction = getBlockState().getValue(BlockPlacerBlock.FACING);
+        }
+
+        InteractionResult result = blockItem.place(new DirectionalPlaceContext(level, blockPosPlacement, direction, itemStack, direction) {
+            @Override
+            public @NotNull Direction getNearestLookingDirection() {
+                return direction;
+            }
+
+            @Override
+            public @NotNull Direction @NotNull [] getNearestLookingDirections() {
+                return switch (direction) {
+                    case DOWN ->
+                            new Direction[] { Direction.DOWN, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.UP };
+                    case UP ->
+                            new Direction[] { Direction.UP, Direction.DOWN, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST };
+                    case NORTH ->
+                            new Direction[] { Direction.NORTH, Direction.EAST, Direction.WEST, Direction.UP, Direction.DOWN, Direction.SOUTH };
+                    case SOUTH ->
+                            new Direction[] { Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.UP, Direction.DOWN, Direction.NORTH };
+                    case WEST ->
+                            new Direction[] { Direction.WEST, Direction.SOUTH, Direction.UP, Direction.DOWN, Direction.NORTH, Direction.EAST };
+                    case EAST ->
+                            new Direction[] { Direction.EAST, Direction.SOUTH, Direction.UP, Direction.DOWN, Direction.NORTH, Direction.WEST };
+                };
+            }
+
+            @Override
+            public boolean replacingClickedOnBlock() {
+                return false;
+            }
+        });
+
+        if(result == InteractionResult.FAIL) {
+            energyConsumptionLeft = energyConsumptionPerTick;
+            setChanged();
+
+            return;
+        }
+
+        itemHandler.setStackInSlot(0, itemStack);
+        resetProgress();
     }
 
     public void setInverseRotation(boolean inverseRotation) {
