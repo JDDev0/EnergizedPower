332de5c7e501c88ba5dacd2c3c14f8367f8817a0
JDDev0 <jddev0@dorneger.at>
2024-05-16 19:43:19 +0200

    Add missing check for lava amount in the "canCraftRecipe()" method in the "StoneSolidiferBlockEntity" class

--- src/main/java/me/jddev0/ep/block/entity/StoneSolidifierBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/StoneSolidifierBlockEntity.java
@@ -152,6 +152,7 @@ public class StoneSolidifierBlockEntity
     protected boolean canCraftRecipe(SimpleInventory inventory, StoneSolidifierRecipe recipe) {
         return world != null &&
                 fluidStorage.parts.get(0).getAmount() >= FluidUtils.convertMilliBucketsToDroplets(recipe.getWaterAmount()) &&
+                fluidStorage.parts.get(1).getAmount() >= FluidUtils.convertMilliBucketsToDroplets(recipe.getLavaAmount()) &&
                 InventoryUtils.canInsertItemIntoSlot(inventory, 0, recipe.getOutput(world.getRegistryManager()));
     }
 }
