d623e23b46e22296f780d95baf7e05a49ccd710d
JDDev0 <jddev0@dorneger.at>
2024-05-16 19:29:20 +0200

    Fix "BuiltInRegistries" was used instead of "ForgeRegistries" in the "SyncCurrentRecipeS2CPacket" class

--- src/main/java/me/jddev0/ep/networking/packet/SyncCurrentRecipeS2CPacket.java
+++ src/main/java/me/jddev0/ep/networking/packet/SyncCurrentRecipeS2CPacket.java
@@ -3,13 +3,13 @@ package me.jddev0.ep.networking.packet;
 import me.jddev0.ep.recipe.CurrentRecipePacketUpdate;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
@@ -31,7 +31,7 @@ public final class SyncCurrentRecipeS2CPacket<R extends Recipe<?>> {
         pos = buffer.readBlockPos();
 
         ResourceLocation recipeSerializerId = buffer.readResourceLocation();
-        recipeSerializer = (RecipeSerializer<R>)BuiltInRegistries.RECIPE_SERIALIZER.get(recipeSerializerId);
+        recipeSerializer = (RecipeSerializer<R>)ForgeRegistries.RECIPE_SERIALIZERS.getValue(recipeSerializerId);
         if(recipeSerializer == null)
             throw new IllegalArgumentException("Recipe Serializer \"" + recipeSerializerId + "\" does not exist!");
 
@@ -42,7 +42,7 @@ public final class SyncCurrentRecipeS2CPacket<R extends Recipe<?>> {
      public void toBytes(FriendlyByteBuf buffer) {
         buffer.writeBlockPos(pos);
 
-        ResourceLocation recipeSerializerId = BuiltInRegistries.RECIPE_SERIALIZER.getKey(recipeSerializer);
+        ResourceLocation recipeSerializerId = ForgeRegistries.RECIPE_SERIALIZERS.getKey(recipeSerializer);
         if(recipeSerializerId == null)
             throw new IllegalArgumentException("The recipe serializer \"" + recipeSerializer.getClass().getCanonicalName() +
                     "\" is not registered!");
