37569caca9f7f2316722041d842fa66603171105
JDDev0 <jddev0@dorneger.at>
2024-05-16 17:52:40 +0200

    Fix Worker Machine Block Entity base classes

--- src/main/java/me/jddev0/ep/block/entity/base/WorkerFluidMachineBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/base/WorkerFluidMachineBlockEntity.java
@@ -195,7 +195,8 @@ public abstract class WorkerFluidMachineBlockEntity<F extends Storage<FluidVaria
         progress = 0;
         maxProgress = 0;
         energyConsumptionLeft = -1;
-        hasEnoughEnergy = true;
+        hasEnoughEnergy = false;
+        onHasNotEnoughEnergy();
     }
 
     @Override
