6ca453b46bd760f142afea4087aa53c3b2da763b
JDDev0 <jddev0@dorneger.at>
2024-05-31 21:24:55 +0200

    Swap position of next recipe and prev recipe buttons in selectable recipe machine screens

--- src/main/java/me/jddev0/ep/screen/base/SelectableRecipeMachineContainerScreen.java
+++ src/main/java/me/jddev0/ep/screen/base/SelectableRecipeMachineContainerScreen.java
@@ -28,17 +28,6 @@ public abstract class SelectableRecipeMachineContainerScreen
     protected int recipeSelectorTexturePosY = 70;
 
     public SelectableRecipeMachineContainerScreen(T menu, Inventory inventory, Component titleComponent,
-                                                  ResourceLocation upgradeViewTexture) {
-        super(menu, inventory, titleComponent, upgradeViewTexture);
-    }
-
-    public SelectableRecipeMachineContainerScreen(T menu, Inventory inventory, Component titleComponent,
-                                                  String energyIndicatorBarTooltipComponentID,
-                                                  ResourceLocation upgradeViewTexture) {
-        super(menu, inventory, titleComponent, energyIndicatorBarTooltipComponentID, upgradeViewTexture);
-    }
-
-    public SelectableRecipeMachineContainerScreen(T menu, Inventory inventory, Component titleComponent,
                                                   ResourceLocation texture,
                                                   ResourceLocation upgradeViewTexture) {
         super(menu, inventory, titleComponent, texture, upgradeViewTexture);
@@ -64,14 +53,14 @@ public abstract class SelectableRecipeMachineContainerScreen
         if(mouseButton == 0) {
             int diff = 0;
 
-            //Up button
+            //Down button
             if(isHovering(recipeSelectorPosX - 13, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
-                diff = 1;
+                diff = -1;
             }
 
-            //Down button
+            //Up button
             if(isHovering(recipeSelectorPosX + 18, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
-                diff = -1;
+                diff = 1;
             }
 
             if(diff != 0) {
@@ -115,13 +104,13 @@ public abstract class SelectableRecipeMachineContainerScreen
     }
 
     private void renderButtons(GuiGraphics guiGraphics, int x, int y, int mouseX, int mouseY) {
-        //Up button
+        //Down button
         if(isHovering(recipeSelectorPosX - 13, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
             guiGraphics.blit(TEXTURE, x + recipeSelectorPosX - 13, y + recipeSelectorPosY + 2,
                     recipeSelectorTexturePosX, recipeSelectorTexturePosY, 11, 12);
         }
 
-        //Down button
+        //Up button
         if(isHovering(recipeSelectorPosX + 18, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
             guiGraphics.blit(TEXTURE, x + recipeSelectorPosX + 18, y + recipeSelectorPosY + 2,
                     recipeSelectorTexturePosX + 11, recipeSelectorTexturePosY, 11, 12);
@@ -137,18 +126,18 @@ public abstract class SelectableRecipeMachineContainerScreen
         if(currentRecipe != null && isHovering(recipeSelectorPosX, recipeSelectorPosY, 16, 16, mouseX, mouseY))
             renderCurrentRecipeTooltip(guiGraphics, mouseX, mouseY, currentRecipe);
 
-        //Up button
+        //Down button
         if(isHovering(recipeSelectorPosX - 13, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
             List<Component> components = new ArrayList<>(2);
-            components.add(Component.translatable("tooltip.energizedpower.recipe.selector.next_recipe"));
+            components.add(Component.translatable("tooltip.energizedpower.recipe.selector.prev_recipe"));
 
             guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
         }
 
-        //Down button
+        //Up button
         if(isHovering(recipeSelectorPosX + 18, recipeSelectorPosY + 2, 11, 12, mouseX, mouseY)) {
             List<Component> components = new ArrayList<>(2);
-            components.add(Component.translatable("tooltip.energizedpower.recipe.selector.prev_recipe"));
+            components.add(Component.translatable("tooltip.energizedpower.recipe.selector.next_recipe"));
 
             guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
         }
