a3254c5edb17c1a2a6977f50877ce34ed51e2a54
JDDev0 <jddev0@dorneger.at>
2024-06-05 21:01:21 +0200

    Improve Energized Power recipes: Allow NBT tag in all item stacks

--- src/main/java/me/jddev0/ep/recipe/SawmillRecipe.java
+++ src/main/java/me/jddev0/ep/recipe/SawmillRecipe.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import me.jddev0.ep.EnergizedPowerMod;
 import me.jddev0.ep.block.ModBlocks;
 import me.jddev0.ep.item.ModItems;
+import me.jddev0.ep.util.ItemStackUtils;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
@@ -108,10 +109,10 @@ public class SawmillRecipe implements Recipe<SimpleInventory> {
         @Override
         public SawmillRecipe read(Identifier recipeID, JsonObject json) {
             Ingredient input = Ingredient.fromJson(json.get("ingredient"));
-            ItemStack output = ShapedRecipe.outputFromJson(JsonHelper.getObject(json, "output"));
+            ItemStack output = ItemStackUtils.fromJson(JsonHelper.getObject(json, "output"));
 
             if(json.has("secondaryOutput")) {
-                ItemStack secondaryOutput = ShapedRecipe.outputFromJson(JsonHelper.getObject(json, "secondaryOutput"));
+                ItemStack secondaryOutput = ItemStackUtils.fromJson(JsonHelper.getObject(json, "secondaryOutput"));
                 return new SawmillRecipe(recipeID, output, secondaryOutput, input);
             }
 
