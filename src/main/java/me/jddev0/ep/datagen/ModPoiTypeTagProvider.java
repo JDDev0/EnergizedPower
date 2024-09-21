73832a3d6642da0a73a46d9b37cce784586edcfc
JDDev0 <jddev0@dorneger.at>
2024-09-21 22:56:01 +0200

    Use Datagen for all blocks and items tags

---
+++ src/main/java/me/jddev0/ep/datagen/ModPoiTypeTagProvider.java
@@ -0,0 +1,26 @@
+package me.jddev0.ep.datagen;
+
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.villager.ModVillager;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.PoiTypeTagsProvider;
+import net.minecraft.tags.PoiTypeTags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+public class ModPoiTypeTagProvider extends PoiTypeTagsProvider {
+    public ModPoiTypeTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,
+                                 @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, EnergizedPowerMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider lookupProvider) {
+        tag(PoiTypeTags.ACQUIRABLE_JOB_SITE).
+                add(Objects.requireNonNull(ModVillager.BASIC_MACHINE_FRAME_POI.getKey()));
+    }
+}
