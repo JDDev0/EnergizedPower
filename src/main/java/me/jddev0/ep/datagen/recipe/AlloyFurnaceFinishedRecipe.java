30ab1ef25e500718459c2807d9f3ad86b3219306
JDDev0 <jddev0@dorneger.at>
2024-09-21 23:48:47 +0200

    Use Datagen for all alloy furnace recipes

---
+++ src/main/java/me/jddev0/ep/datagen/recipe/AlloyFurnaceFinishedRecipe.java
@@ -0,0 +1,79 @@
+package me.jddev0.ep.datagen.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import me.jddev0.ep.recipe.AlloyFurnaceRecipe;
+import me.jddev0.ep.recipe.ModRecipes;
+import me.jddev0.ep.util.ItemStackUtils;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.jetbrains.annotations.Nullable;
+
+public record AlloyFurnaceFinishedRecipe(
+        ResourceLocation id,
+        ItemStack output,
+        AlloyFurnaceRecipe.OutputItemStackWithPercentages secondaryOutput,
+        AlloyFurnaceRecipe.IngredientWithCount[] inputs,
+        int ticks
+) implements FinishedRecipe {
+    @Override
+    public void serializeRecipeData(JsonObject jsonObject) {
+        jsonObject.add("output", ItemStackUtils.toJson(output));
+
+        if(!secondaryOutput.output().isEmpty() && secondaryOutput.percentages().length != 0) {
+            JsonObject secondaryOutputJson = new JsonObject();
+
+            secondaryOutputJson.add("output", ItemStackUtils.toJson(output));
+
+            {
+                JsonArray percentagesJson = new JsonArray();
+
+                for(double percentage:secondaryOutput.percentages())
+                    percentagesJson.add(percentage);
+
+                secondaryOutputJson.add("percentages", percentagesJson);
+            }
+
+            jsonObject.add("secondaryOutput", secondaryOutputJson);
+        }
+
+        {
+            JsonArray inputsJson = new JsonArray();
+
+            for(AlloyFurnaceRecipe.IngredientWithCount input:inputs) {
+                JsonObject inputJson = new JsonObject();
+
+                inputJson.add("input", input.input().toJson());
+                inputJson.addProperty("count", input.count());
+
+                inputsJson.add(inputJson);
+            }
+
+            jsonObject.add("inputs", inputsJson);
+        }
+
+        jsonObject.addProperty("ticks", ticks);
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getType() {
+        return ModRecipes.ALLOY_FURNACE_SERIALIZER.get();
+    }
+
+    @Override
+    public @Nullable JsonObject serializeAdvancement() {
+        return null;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getAdvancementId() {
+        return null;
+    }
+}
