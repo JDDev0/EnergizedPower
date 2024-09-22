58095ff376d9645757068aa4b6bbfa9fb760f955
JDDev0 <jddev0@dorneger.at>
2024-09-22 12:01:47 +0200

    Use Datagen for all blasting and smelting recipes

--- src/main/java/me/jddev0/ep/datagen/ModRecipeProvider.java
+++ src/main/java/me/jddev0/ep/datagen/ModRecipeProvider.java
@@ -1,9 +1,29 @@
 package me.jddev0.ep.datagen;
 
+import me.jddev0.ep.EnergizedPowerMod;
+import me.jddev0.ep.block.ModBlocks;
+import me.jddev0.ep.datagen.recipe.AbstractCookingFinishedRecipe;
+import me.jddev0.ep.item.ModItems;
+import me.jddev0.ep.registry.tags.CommonItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementRewards;
+import net.minecraft.advancement.CriterionMerger;
+import net.minecraft.advancement.criterion.RecipeUnlockedCriterion;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.book.CookingRecipeCategory;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -12,7 +32,156 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(Consumer<RecipeJsonProvider> exporter) {
+    public void generate(Consumer<RecipeJsonProvider> output) {
+        buildCookingRecipes(output);
+    }
 
+    private void buildCookingRecipes(Consumer<RecipeJsonProvider> output) {
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.RAW_TIN_ORES, new ItemStack(ModItems.TIN_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "tin_ingot", "raw_tin");
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.TIN_ORES, new ItemStack(ModItems.TIN_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "tin_ingot", "tin_ores");
+
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.TIN_DUSTS, new ItemStack(ModItems.TIN_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "tin_ingot", "tin_dust");
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.COPPER_DUSTS, new ItemStack(Items.COPPER_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "copper_ingot", "copper_dust");
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.IRON_DUSTS, new ItemStack(Items.IRON_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "iron_ingot", "iron_dust");
+        addBlastingAndSmeltingRecipes(output, CommonItemTags.GOLD_DUSTS, new ItemStack(Items.GOLD_INGOT), CookingRecipeCategory.MISC,
+                100, .7f, "gold_ingot", "gold_dust");
+
+        addBlastingAndSmeltingRecipes(output, ModItems.IRON_HAMMER, new ItemStack(Items.IRON_NUGGET), CookingRecipeCategory.MISC,
+                100, .1f, "iron_nugget", "iron_hammer");
+        addBlastingAndSmeltingRecipes(output, ModItems.GOLDEN_HAMMER, new ItemStack(Items.GOLD_NUGGET), CookingRecipeCategory.MISC,
+                100, .1f, "gold_nugget", "golden_hammer");
+
+        addBlastingAndSmeltingRecipes(output, ConventionalItemTags.QUARTZ, new ItemStack(ModItems.SILICON), CookingRecipeCategory.MISC,
+                250, 4.1f, "silicon", "nether_quartz");
+
+        addSmeltingRecipe(output, ModBlocks.SAWDUST_BLOCK_ITEM, new ItemStack(Items.CHARCOAL), CookingRecipeCategory.MISC,
+                200, .15f, null, "sawdust_block");
+
+        addSmeltingRecipe(output, ModItems.RAW_GEAR_PRESS_MOLD, new ItemStack(ModItems.GEAR_PRESS_MOLD), CookingRecipeCategory.MISC,
+                200, .3f, null);
+        addSmeltingRecipe(output, ModItems.RAW_ROD_PRESS_MOLD, new ItemStack(ModItems.ROD_PRESS_MOLD), CookingRecipeCategory.MISC,
+                200, .3f, null);
+        addSmeltingRecipe(output, ModItems.RAW_WIRE_PRESS_MOLD, new ItemStack(ModItems.WIRE_PRESS_MOLD), CookingRecipeCategory.MISC,
+                200, .3f, null);
+    }
+
+    private static void addBlastingAndSmeltingRecipes(Consumer<RecipeJsonProvider> output, ItemConvertible ingredient, ItemStack result,
+                                                      CookingRecipeCategory category, int time, float xp, String group,
+                                                      String recipeIngredientName) {
+        addBlastingRecipe(output, ingredient, result, category, time, xp, group, recipeIngredientName);
+        addSmeltingRecipe(output, ingredient, result, category, 2 * time, xp, group, recipeIngredientName);
+    }
+
+    private static void addBlastingAndSmeltingRecipes(Consumer<RecipeJsonProvider> output, TagKey<Item> ingredient, ItemStack result,
+                                                      CookingRecipeCategory category, int time, float xp, String group,
+                                                      String recipeIngredientName) {
+        addBlastingRecipe(output, ingredient, result, category, time, xp, group, recipeIngredientName);
+        addSmeltingRecipe(output, ingredient, result, category, 2 * time, xp, group, recipeIngredientName);
+    }
+
+    private static void addSmeltingRecipe(Consumer<RecipeJsonProvider> output, ItemConvertible ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group) {
+        Identifier recipeId = Identifier.of(EnergizedPowerMod.MODID, "smelting/" +
+                getItemPath(result.getItem()));
+
+        addSmeltingRecipe(output, ingredient, result, category, time, xp, group, recipeId);
+    }
+
+    private static void addSmeltingRecipe(Consumer<RecipeJsonProvider> output, ItemConvertible ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group, String recipeIngredientName) {
+        Identifier recipeId = Identifier.of(EnergizedPowerMod.MODID, "smelting/" +
+                getItemPath(result.getItem()) + "_from_smelting_" + recipeIngredientName);
+
+        addSmeltingRecipe(output, ingredient, result, category, time, xp, group, recipeId);
+    }
+
+    private static void addSmeltingRecipe(Consumer<RecipeJsonProvider> output, ItemConvertible ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group, Identifier recipeId) {
+        Advancement.Builder advancementBuilder = Advancement.Builder.create()
+                .criterion("has_the_recipe", RecipeUnlockedCriterion.create(recipeId))
+                .criterion("has_the_ingredient", conditionsFromItem(ingredient))
+                .rewards(AdvancementRewards.Builder.recipe(recipeId))
+                .criteriaMerger(CriterionMerger.OR);
+        RecipeJsonProvider recipe = new AbstractCookingFinishedRecipe(
+                recipeId,
+                Objects.requireNonNullElse(group, ""), category,
+                Ingredient.ofItems(ingredient), result.getItem(),
+                xp, time,
+                advancementBuilder,
+                recipeId.withPrefixedPath("recipes/"),
+                RecipeSerializer.SMELTING
+        );
+        output.accept(recipe);
+    }
+
+    private static void addSmeltingRecipe(Consumer<RecipeJsonProvider> output, TagKey<Item> ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group, String recipeIngredientName) {
+        Identifier recipeId = Identifier.of(EnergizedPowerMod.MODID, "smelting/" +
+                getItemPath(result.getItem()) + "_from_smelting_" + recipeIngredientName);
+
+        Advancement.Builder advancementBuilder = Advancement.Builder.create()
+                .criterion("has_the_recipe", RecipeUnlockedCriterion.create(recipeId))
+                .criterion("has_the_ingredient", conditionsFromTag(ingredient))
+                .rewards(AdvancementRewards.Builder.recipe(recipeId))
+                .criteriaMerger(CriterionMerger.OR);
+        RecipeJsonProvider recipe = new AbstractCookingFinishedRecipe(
+                recipeId,
+                Objects.requireNonNullElse(group, ""), category,
+                Ingredient.fromTag(ingredient), result.getItem(),
+                xp, time,
+                advancementBuilder,
+                recipeId.withPrefixedPath("recipes/"),
+                RecipeSerializer.SMELTING
+        );
+        output.accept(recipe);
+    }
+
+    private static void addBlastingRecipe(Consumer<RecipeJsonProvider> output, ItemConvertible ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group, String recipeIngredientName) {
+        Identifier recipeId = Identifier.of(EnergizedPowerMod.MODID, "blasting/" +
+                getItemPath(result.getItem()) + "_from_blasting_" + recipeIngredientName);
+
+        Advancement.Builder advancementBuilder = Advancement.Builder.create()
+                .criterion("has_the_recipe", RecipeUnlockedCriterion.create(recipeId))
+                .criterion("has_the_ingredient", conditionsFromItem(ingredient))
+                .rewards(AdvancementRewards.Builder.recipe(recipeId))
+                .criteriaMerger(CriterionMerger.OR);
+        RecipeJsonProvider recipe = new AbstractCookingFinishedRecipe(
+                recipeId,
+                Objects.requireNonNullElse(group, ""), category,
+                Ingredient.ofItems(ingredient), result.getItem(),
+                xp, time,
+                advancementBuilder,
+                recipeId.withPrefixedPath("recipes/"),
+                RecipeSerializer.BLASTING
+        );
+        output.accept(recipe);
+    }
+
+    private static void addBlastingRecipe(Consumer<RecipeJsonProvider> output, TagKey<Item> ingredient, ItemStack result, CookingRecipeCategory category,
+                                          int time, float xp, String group, String recipeIngredientName) {
+        Identifier recipeId = Identifier.of(EnergizedPowerMod.MODID, "blasting/" +
+                getItemPath(result.getItem()) + "_from_blasting_" + recipeIngredientName);
+
+        Advancement.Builder advancementBuilder = Advancement.Builder.create()
+                .criterion("has_the_recipe", RecipeUnlockedCriterion.create(recipeId))
+                .criterion("has_the_ingredient", conditionsFromTag(ingredient))
+                .rewards(AdvancementRewards.Builder.recipe(recipeId))
+                .criteriaMerger(CriterionMerger.OR);
+        RecipeJsonProvider recipe = new AbstractCookingFinishedRecipe(
+                recipeId,
+                Objects.requireNonNullElse(group, ""), category,
+                Ingredient.fromTag(ingredient), result.getItem(),
+                xp, time,
+                advancementBuilder,
+                recipeId.withPrefixedPath("recipes/"),
+                RecipeSerializer.BLASTING
+        );
+        output.accept(recipe);
     }
 }
