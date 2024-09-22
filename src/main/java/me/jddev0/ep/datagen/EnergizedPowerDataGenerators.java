07c6d45300973145f69efc672602afc8e559eb0c
JDDev0 <jddev0@dorneger.at>
2024-09-22 13:39:09 +0200

    Use Datagen for book pages

--- src/main/java/me/jddev0/ep/datagen/EnergizedPowerDataGenerators.java
+++ src/main/java/me/jddev0/ep/datagen/EnergizedPowerDataGenerators.java
@@ -14,11 +14,12 @@ public class EnergizedPowerDataGenerators implements DataGeneratorEntrypoint {
 
         pack.addProvider(ModRegistriesProvider::new);
 
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModBookPageContentProvider::new);
+
         pack.addProvider(ModRecipeProvider::new);
         pack.addProvider(ModBlockLootTables::new);
         ModAdvancementProvider.create(pack);
-
-        pack.addProvider(ModModelProvider::new);
 
         pack.addProvider(ModBlockTagProvider::new);
         pack.addProvider(ModItemTagProvider::new);
