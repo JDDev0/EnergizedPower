5afe1d492aacbc2eed2d544b6d58431e36c9ee72
JDDev0 <jddev0@dorneger.at>
2024-09-22 16:48:45 +0200

    Use Datagen for some block models

--- src/main/java/me/jddev0/ep/datagen/model/ModModels.java
+++ src/main/java/me/jddev0/ep/datagen/model/ModModels.java
@@ -8,16 +8,31 @@ import net.minecraft.util.Identifier;
 import java.util.Optional;
 
 public final class ModModels {
-    public static final Model CABLE_CORE_TEMPLATE = block("cable_core_template", TextureKey.PARTICLE, ModTextureKey.CABLE);
-    public static final Model CABLE_SIDE_TEMPLATE = block("cable_side_template", TextureKey.PARTICLE, ModTextureKey.CABLE);
+    public static final Model FLUID_PIPE_CORE_TEMPLATE = block("fluid_pipe_core_template",
+            TextureKey.PARTICLE, ModTextureKey.FLUID_PIPE_CORE);
+    public static final Model FLUID_PIPE_SIDE_CONNECTED_TEMPLATE = block("fluid_pipe_side_connected_template",
+            TextureKey.PARTICLE, ModTextureKey.FLUID_PIPE_SIDE);
+    public static final Model FLUID_PIPE_SIDE_EXTRACT_TEMPLATE = block("fluid_pipe_side_extract_template",
+            TextureKey.PARTICLE, ModTextureKey.FLUID_PIPE_SIDE_INNER, ModTextureKey.FLUID_PIPE_SIDE_OUTER);
+
+    public static final Model FLUID_TANK_TEMPLATE = block("fluid_tank_template",
+            TextureKey.PARTICLE, TextureKey.FRONT, TextureKey.SIDE, TextureKey.UP, ModTextureKey.INTERIOR);
+
+    public static final Model CABLE_CORE_TEMPLATE = block("cable_core_template",
+            TextureKey.PARTICLE, ModTextureKey.CABLE);
+    public static final Model CABLE_SIDE_TEMPLATE = block("cable_side_template",
+            TextureKey.PARTICLE, ModTextureKey.CABLE);
+
+    public static final Model SOLAR_PANEL_TEMPLATE = block("solar_panel_template",
+            TextureKey.PARTICLE, TextureKey.TOP, TextureKey.SIDE);
 
     private ModModels() {}
 
     private static Model block(String parent, TextureKey ... requiredTextureKeys) {
-        return new Model(Optional.of(Identifier.of(EnergizedPowerMod.MODID, "block/" + parent)), Optional.empty(), requiredTextureKeys);
+        return new Model(Optional.of(new Identifier(EnergizedPowerMod.MODID, "block/" + parent)), Optional.empty(), requiredTextureKeys);
     }
 
     private static Model block(String parent, String variant, TextureKey... requiredTextureKeys) {
-        return new Model(Optional.of(Identifier.of(EnergizedPowerMod.MODID, "block/" + parent)), Optional.of(variant), requiredTextureKeys);
+        return new Model(Optional.of(new Identifier(EnergizedPowerMod.MODID, "block/" + parent)), Optional.of(variant), requiredTextureKeys);
     }
 }
