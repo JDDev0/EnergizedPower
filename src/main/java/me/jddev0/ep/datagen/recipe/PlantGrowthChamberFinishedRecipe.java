84741c4ec0ad3d2293d3cb0b5561403b34e02da4
JDDev0 <jddev0@dorneger.at>
2024-09-22 15:00:58 +0200

    Use Datagen for all plant growth chamber, plant growth chmaber fertilizer, and stone solidifier recipes

---
+++ src/main/java/me/jddev0/ep/datagen/recipe/PlantGrowthChamberFinishedRecipe.java
@@ -0,0 +1,69 @@
+package me.jddev0.ep.datagen.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import me.jddev0.ep.recipe.ModRecipes;
+import me.jddev0.ep.recipe.OutputItemStackWithPercentages;
+import me.jddev0.ep.util.ItemStackUtils;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.jetbrains.annotations.Nullable;
+
+public record PlantGrowthChamberFinishedRecipe(
+        ResourceLocation id,
+        OutputItemStackWithPercentages[] outputs,
+        Ingredient input,
+        int ticks
+) implements FinishedRecipe {
+    @Override
+    public void serializeRecipeData(JsonObject jsonObject) {
+        {
+            JsonArray outputsJson = new JsonArray();
+
+            for(OutputItemStackWithPercentages output:outputs) {
+                JsonObject outputJson = new JsonObject();
+
+                outputJson.add("output", ItemStackUtils.toJson(output.output()));
+
+                {
+                    JsonArray percentagesJson = new JsonArray();
+
+                    for(double percentage:output.percentages())
+                        percentagesJson.add(percentage);
+
+                    outputJson.add("percentages", percentagesJson);
+                }
+
+                outputsJson.add(outputJson);
+            }
+
+            jsonObject.add("outputs", outputsJson);
+        }
+
+        jsonObject.add("ingredient", input.toJson());
+
+        jsonObject.addProperty("ticks", ticks);
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getType() {
+        return ModRecipes.PLANT_GROWTH_CHAMBER_SERIALIZER.get();
+    }
+
+    @Override
+    public @Nullable JsonObject serializeAdvancement() {
+        return null;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getAdvancementId() {
+        return null;
+    }
+}
---
+++ src/main/java/me/jddev0/ep/datagen/recipe/StoneSolidifierFinishedRecipe.java
@@ -0,0 +1,44 @@
+package me.jddev0.ep.datagen.recipe;
+
+import com.google.gson.JsonObject;
+import me.jddev0.ep.recipe.ModRecipes;
+import me.jddev0.ep.util.ItemStackUtils;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.jetbrains.annotations.Nullable;
+
+public record StoneSolidifierFinishedRecipe(
+        ResourceLocation id,
+        ItemStack output,
+        int waterAmount,
+        int lavaAmount
+) implements FinishedRecipe {
+    @Override
+    public void serializeRecipeData(JsonObject jsonObject) {
+        jsonObject.add("output", ItemStackUtils.toJson(output));
+        jsonObject.addProperty("waterAmount", waterAmount);
+        jsonObject.addProperty("lavaAmount", lavaAmount);
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getType() {
+        return ModRecipes.STONE_SOLIDIFIER_SERIALIZER.get();
+    }
+
+    @Override
+    public @Nullable JsonObject serializeAdvancement() {
+        return null;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getAdvancementId() {
+        return null;
+    }
+}
