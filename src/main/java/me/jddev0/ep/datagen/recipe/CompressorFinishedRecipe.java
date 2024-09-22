ba49edbb1d39b041ffe7a132162202f576c362e5
JDDev0 <jddev0@dorneger.at>
2024-09-22 17:05:23 +0200

    Use Datagen for all charger and compressor recipes

---
+++ src/main/java/me/jddev0/ep/datagen/recipe/CompressorFinishedRecipe.java
@@ -0,0 +1,45 @@
+package me.jddev0.ep.datagen.recipe;
+
+import com.google.gson.JsonObject;
+import me.jddev0.ep.recipe.ModRecipes;
+import me.jddev0.ep.util.ItemStackUtils;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+public record CompressorFinishedRecipe(
+        Identifier id,
+        ItemStack output,
+        Ingredient input,
+        int inputCount
+) implements RecipeJsonProvider {
+    @Override
+    public void serialize(JsonObject jsonObject) {
+        jsonObject.add("output", ItemStackUtils.toJson(output));
+        jsonObject.add("ingredient", input.toJson());
+        jsonObject.addProperty("inputCount", inputCount);
+    }
+
+    @Override
+    public Identifier getRecipeId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return ModRecipes.COMPRESSOR_SERIALIZER;
+    }
+
+    @Override
+    public @Nullable JsonObject toAdvancementJson() {
+        return null;
+    }
+
+    @Override
+    public @Nullable Identifier getAdvancementId() {
+        return null;
+    }
+}
