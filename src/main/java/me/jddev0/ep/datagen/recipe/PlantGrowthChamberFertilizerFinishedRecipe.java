9e8a08a604eba7a1191016185e604aa33907d185
JDDev0 <jddev0@dorneger.at>
2024-09-22 10:26:02 +0200

    Use Datagen for all plant growth chamber, plant growth chmaber fertilizer, and stone solidifier recipes

---
+++ src/main/java/me/jddev0/ep/datagen/recipe/PlantGrowthChamberFertilizerFinishedRecipe.java
@@ -0,0 +1,44 @@
+package me.jddev0.ep.datagen.recipe;
+
+import com.google.gson.JsonObject;
+import me.jddev0.ep.recipe.ModRecipes;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import org.jetbrains.annotations.Nullable;
+
+public record PlantGrowthChamberFertilizerFinishedRecipe(
+        ResourceLocation id,
+        Ingredient input,
+        double speedMultiplier,
+        double energyConsumptionMultiplier
+) implements FinishedRecipe {
+    @Override
+    public void serializeRecipeData(JsonObject jsonObject) {
+        jsonObject.add("ingredient", input.toJson());
+
+        jsonObject.addProperty("speedMultiplier", speedMultiplier);
+        jsonObject.addProperty("energyConsumptionMultiplier", energyConsumptionMultiplier);
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getType() {
+        return ModRecipes.PLANT_GROWTH_CHAMBER_FERTILIZER_SERIALIZER.get();
+    }
+
+    @Override
+    public @Nullable JsonObject serializeAdvancement() {
+        return null;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getAdvancementId() {
+        return null;
+    }
+}
