54e049e4a8ac34207aa4731f85b45162d68b66c6
JDDev0 <jddev0@dorneger.at>
2026-04-14 18:06:20 +0200

    Update credits: Add Japanese translation

--- src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
+++ src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
@@ -166,6 +166,12 @@ public class ModBookPageContentProvider extends PageContentProvider {
                             withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).
                             append(Component.translatable("book.energizedpower.page.credits.10")).append(": ").
                             append(Component.literal("sebbyrenewed [Discord]"))
+            ).append(
+                    Component.literal("\n• ").
+                            withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).
+                            append(Component.translatable("book.energizedpower.page.credits.11")).append(": ").
+                            append(addLinkToComponent(Component.literal("hamutaromaru"),
+                                    "https://github.com/hamu6251ren0725-hue"))
             ), changePageIntToId);
         }
 
