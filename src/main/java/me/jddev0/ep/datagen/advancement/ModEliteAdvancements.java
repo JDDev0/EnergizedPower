e96d10ceab59697bf1f199e8709d6234fb3cf985
JDDev0 <jddev0@dorneger.at>
2026-06-07 13:29:15 +0200

    Fix typo in Quantum Processing Unit advancement

--- src/main/java/me/jddev0/ep/datagen/advancement/ModEliteAdvancements.java
+++ src/main/java/me/jddev0/ep/datagen/advancement/ModEliteAdvancements.java
@@ -89,7 +89,7 @@ public class ModEliteAdvancements extends FabricAdvancementProvider {
 
         AdvancementHolder quantumProcessingUnit = addAdvancement(
                 advancementOutput, superconductor,
-                EPItems.QUANTUM_PROCESSING_UNIT, "quantum_prcessing_unit", AdvancementType.TASK
+                EPItems.QUANTUM_PROCESSING_UNIT, "quantum_processing_unit", AdvancementType.TASK
         );
 
         AdvancementHolder eliteUpgradeModule = addAdvancement(
