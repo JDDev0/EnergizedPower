a5a5187614232be091489df9624ce313d3ddbbd6
JDDev0 <jddev0@dorneger.at>
2026-06-10 22:07:24 +0200

    Rename SoilType (stream) codec to direct (stream) codec and add holder-based (stream) codec

--- src/main/java/me/jddev0/ep/soil/SoilType.java
+++ src/main/java/me/jddev0/ep/soil/SoilType.java
@@ -2,19 +2,28 @@ package me.jddev0.ep.soil;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import me.jddev0.ep.registry.EPRegistries;
+import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.RegistryFixedCodec;
 
 public record SoilType(Component displayName) {
-    public static final Codec<SoilType> CODEC = RecordCodecBuilder.create(instance -> {
+    public static final Codec<SoilType> DIRECT_CODEC = RecordCodecBuilder.create(instance -> {
         return instance.group(ComponentSerialization.CODEC.fieldOf("display_name").forGetter(soilType -> {
             return soilType.displayName;
         })).apply(instance, SoilType::new);
     });
 
-    public static final StreamCodec<RegistryFriendlyByteBuf, SoilType> STREAM_CODEC = StreamCodec.ofMember(SoilType::write, SoilType::new);
+    public static final StreamCodec<RegistryFriendlyByteBuf, SoilType> DIRECT_STREAM_CODEC = StreamCodec.ofMember(SoilType::write, SoilType::new);
+
+    public static final Codec<Holder<SoilType>> CODEC = RegistryFixedCodec.create(EPRegistries.SOIL_TYPE);
+    public static final StreamCodec<RegistryFriendlyByteBuf, Holder<SoilType>> STREAM_CODEC = ByteBufCodecs.holder(
+            EPRegistries.SOIL_TYPE, DIRECT_STREAM_CODEC
+    );
 
     private SoilType(RegistryFriendlyByteBuf buffer) {
         this(ComponentSerialization.TRUSTED_STREAM_CODEC.decode(buffer));
