edb49992454f32275f6b8828d7e3d260c9e31bb4
JDDev0 <jddev0@dorneger.at>
2026-06-12 15:47:05 +0200

    Add text color to display name of all Soil Types

--- src/main/java/me/jddev0/ep/soil/EPSoilTypes.java
+++ src/main/java/me/jddev0/ep/soil/EPSoilTypes.java
@@ -24,19 +24,19 @@ public final class EPSoilTypes {
     public static final ResourceKey<SoilType> END_STONE = registerKey("end_stone");
 
     public static void bootstrap(BootstrapContext<SoilType> context) {
-        context.register(FARMLAND, new SoilType(Component.translatable("block.minecraft.farmland")));
-        context.register(DIRT, new SoilType(Component.translatable("block.minecraft.dirt")));
-        context.register(COARSE_DIRT, new SoilType(Component.translatable("block.minecraft.coarse_dirt")));
-        context.register(GRASS, new SoilType(Component.translatable("soil_type.energizedpower.grass")));
-        context.register(PODZOL, new SoilType(Component.translatable("block.minecraft.podzol")));
-        context.register(MYCELIUM, new SoilType(Component.translatable("block.minecraft.mycelium")));
-        context.register(MUD, new SoilType(Component.translatable("block.minecraft.mud")));
-        context.register(MOSS, new SoilType(Component.translatable("soil_type.energizedpower.moss")));
-        context.register(SAND, new SoilType(Component.translatable("block.minecraft.sand")));
-        context.register(GRAVEL, new SoilType(Component.translatable("block.minecraft.gravel")));
-        context.register(STONE, new SoilType(Component.translatable("block.minecraft.stone")));
-        context.register(SOUL_SAND, new SoilType(Component.translatable("block.minecraft.soul_sand")));
-        context.register(END_STONE, new SoilType(Component.translatable("block.minecraft.end_stone")));
+        context.register(FARMLAND, new SoilType(Component.translatable("block.minecraft.farmland").withColor(0x593d29)));
+        context.register(DIRT, new SoilType(Component.translatable("block.minecraft.dirt").withColor(0x362519)));
+        context.register(COARSE_DIRT, new SoilType(Component.translatable("block.minecraft.coarse_dirt").withColor(0x593d29)));
+        context.register(GRASS, new SoilType(Component.translatable("soil_type.energizedpower.grass").withColor(0x3e5f36)));
+        context.register(PODZOL, new SoilType(Component.translatable("block.minecraft.podzol").withColor(0x6a4418)));
+        context.register(MYCELIUM, new SoilType(Component.translatable("block.minecraft.mycelium").withColor(0x50474b)));
+        context.register(MUD, new SoilType(Component.translatable("block.minecraft.mud").withColor(0x3a383d)));
+        context.register(MOSS, new SoilType(Component.translatable("soil_type.energizedpower.moss").withColor(0x495e27)));
+        context.register(SAND, new SoilType(Component.translatable("block.minecraft.sand").withColor(0x8c8462)));
+        context.register(GRAVEL, new SoilType(Component.translatable("block.minecraft.gravel").withColor(0x817f7f)));
+        context.register(STONE, new SoilType(Component.translatable("block.minecraft.stone").withColor(0x7f7f7f)));
+        context.register(SOUL_SAND, new SoilType(Component.translatable("block.minecraft.soul_sand").withColor(0x6a5244)));
+        context.register(END_STONE, new SoilType(Component.translatable("block.minecraft.end_stone").withColor(0xdee6a4)));
     }
 
     public static ResourceKey<SoilType> registerKey(String name) {
