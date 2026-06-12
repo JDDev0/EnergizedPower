d7479ce1f2fac2aeaff480287e81784573ce41c3
JDDev0 <jddev0@dorneger.at>
2026-06-12 22:06:06 +0200

    Add fluidConsumptionMultiplier parameter to Plant Growth Chamber Fertilizer recipes

--- src/main/java/me/jddev0/ep/integration/jei/PlantGrowthChamberFertilizerCategory.java
+++ src/main/java/me/jddev0/ep/integration/jei/PlantGrowthChamberFertilizerCategory.java
@@ -24,13 +24,13 @@ public class PlantGrowthChamberFertilizerCategory implements IRecipeCategory<Rec
     public static final RecipeType<RecipeHolder<PlantGrowthChamberFertilizerRecipe>> TYPE = RecipeType.createFromVanilla(PlantGrowthChamberFertilizerRecipe.Type.INSTANCE);
 
     private final IDrawable background;
-    private final IDrawable fertlizerSlot;
+    private final IDrawable fertilizerSlot;
     private final IDrawable icon;
 
     public PlantGrowthChamberFertilizerCategory(IGuiHelper helper) {
         ResourceLocation texture = EPAPI.id("textures/gui/container/plant_growth_chamber.png");
-        fertlizerSlot = helper.createDrawable(texture, 34, 34, 18, 18);
-        background = helper.createBlankDrawable(144, 30);
+        fertilizerSlot = helper.createDrawable(texture, 34, 34, 18, 18);
+        background = helper.createBlankDrawable(144, 60);
 
         icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(EPBlocks.PLANT_GROWTH_CHAMBER_ITEM));
     }
@@ -62,17 +62,17 @@ public class PlantGrowthChamberFertilizerCategory implements IRecipeCategory<Rec
 
     @Override
     public void draw(RecipeHolder<PlantGrowthChamberFertilizerRecipe> recipe, IRecipeSlotsView iRecipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        fertlizerSlot.draw(guiGraphics, 0, 0);
+        fertilizerSlot.draw(guiGraphics, 0, 0);
 
         Font font = Minecraft.getInstance().font;
-        Component component = Component.translatable("recipes.energizedpower.plant_growth_chamber.speed_multiplier", recipe.value().getSpeedMultiplier());
-        int textWidth = font.width(component);
 
-        guiGraphics.drawString(font, component, 144 - textWidth, 5, 0xFFFFFFFF, true);
+        Component component = Component.translatable("recipes.energizedpower.plant_growth_chamber.speed_multiplier", recipe.value().getSpeedMultiplier());
+        guiGraphics.drawString(font, component, 1, 22, 0xFFFFFFFF, true);
+
+        component = Component.translatable("recipes.energizedpower.plant_growth_chamber.fluid_consumption_multiplier", recipe.value().getFluidConsumptionMultiplier());
+        guiGraphics.drawString(font, component, 1, 37, 0xFFFFFFFF, true);
 
         component = Component.translatable("recipes.energizedpower.plant_growth_chamber.energy_consumption_multiplier", recipe.value().getEnergyConsumptionMultiplier());
-        textWidth = font.width(component);
-
-        guiGraphics.drawString(font, component, 144 - textWidth, 22, 0xFFFFFFFF, true);
+        guiGraphics.drawString(font, component, 1, 52, 0xFFFFFFFF, true);
     }
 }
