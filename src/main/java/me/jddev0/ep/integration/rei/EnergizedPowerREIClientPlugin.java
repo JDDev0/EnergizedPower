e386f76537cf3a3f1cdacc87cabb6a9e415ae1bf
JDDev0 <jddev0@dorneger.at>
2026-06-15 21:51:53 +0200

    Implement Fluid Freezer REI integration

--- src/main/java/me/jddev0/ep/integration/rei/EnergizedPowerREIClientPlugin.java
+++ src/main/java/me/jddev0/ep/integration/rei/EnergizedPowerREIClientPlugin.java
@@ -93,6 +93,9 @@ public class EnergizedPowerREIClientPlugin implements REIClientPlugin {
         registry.addWorkstations(AlloyFurnaceDisplay.CATEGORY, EntryStacks.of(EPBlocks.ALLOY_FURNACE_ITEM));
         registry.addWorkstations(AlloyFurnaceDisplay.CATEGORY, EntryStacks.of(EPBlocks.INDUCTION_SMELTER_ITEM));
 
+        registry.add(new FluidFreezerCategory());
+        registry.addWorkstations(FluidFreezerDisplay.CATEGORY, EntryStacks.of(EPBlocks.FLUID_FREEZER_ITEM));
+
         registry.add(new StoneLiquefierCategory());
         registry.addWorkstations(StoneLiquefierDisplay.CATEGORY, EntryStacks.of(EPBlocks.STONE_LIQUEFIER_ITEM));
 
@@ -204,6 +207,8 @@ public class EnergizedPowerREIClientPlugin implements REIClientPlugin {
                 AlloyFurnaceScreen.class, AlloyFurnaceDisplay.CATEGORY);
         registerRecipeClickArea(registry, new Rectangle(104, 34, 24, 17),
                 InductionSmelterScreen.class, AlloyFurnaceDisplay.CATEGORY);
+        registerRecipeClickArea(registry, new Rectangle(83, 43, 24, 17),
+                FluidFreezerScreen.class, FluidFreezerDisplay.CATEGORY);
         registerRecipeClickArea(registry, new Rectangle(112, 34, 24, 17),
                 StoneLiquefierScreen.class, StoneLiquefierDisplay.CATEGORY);
         registerRecipeClickArea(registry, new Rectangle(69, 45, 20, 14),
--- src/main/java/me/jddev0/ep/integration/rei/EnergizedPowerREIPlugin.java
+++ src/main/java/me/jddev0/ep/integration/rei/EnergizedPowerREIPlugin.java
@@ -27,6 +27,7 @@ public class EnergizedPowerREIPlugin implements REICommonPlugin {
         registry.beginRecipeFiller(CrystalGrowthChamberRecipe.class).filterType(EPRecipes.CRYSTAL_GROWTH_CHAMBER_TYPE).fill(CrystalGrowthChamberDisplay::new);
         registry.beginRecipeFiller(PressMoldMakerRecipe.class).filterType(EPRecipes.PRESS_MOLD_MAKER_TYPE).fill(PressMoldMakerDisplay::new);
         registry.beginRecipeFiller(AlloyFurnaceRecipe.class).filterType(EPRecipes.ALLOY_FURNACE_TYPE).fill(AlloyFurnaceDisplay::new);
+        registry.beginRecipeFiller(FluidFreezerRecipe.class).filterType(EPRecipes.FLUID_FREEZER_TYPE).fill(FluidFreezerDisplay::new);
         registry.beginRecipeFiller(StoneLiquefierRecipe.class).filterType(EPRecipes.STONE_LIQUEFIER_TYPE).fill(StoneLiquefierDisplay::new);
         registry.beginRecipeFiller(StoneSolidifierRecipe.class).filterType(EPRecipes.STONE_SOLIDIFIER_TYPE).fill(StoneSolidifierDisplay::new);
         registry.beginRecipeFiller(FiltrationPlantRecipe.class).filterType(EPRecipes.FILTRATION_PLANT_TYPE).fill(FiltrationPlantDisplay::new);
@@ -49,6 +50,7 @@ public class EnergizedPowerREIPlugin implements REICommonPlugin {
         registry.register(CrystalGrowthChamberDisplay.CATEGORY.getIdentifier(), CrystalGrowthChamberDisplay.SERIALIZER);
         registry.register(PressMoldMakerDisplay.CATEGORY.getIdentifier(), PressMoldMakerDisplay.SERIALIZER);
         registry.register(AlloyFurnaceDisplay.CATEGORY.getIdentifier(), AlloyFurnaceDisplay.SERIALIZER);
+        registry.register(FluidFreezerDisplay.CATEGORY.getIdentifier(), FluidFreezerDisplay.SERIALIZER);
         registry.register(StoneLiquefierDisplay.CATEGORY.getIdentifier(), StoneLiquefierDisplay.SERIALIZER);
         registry.register(StoneSolidifierDisplay.CATEGORY.getIdentifier(), StoneSolidifierDisplay.SERIALIZER);
         registry.register(FiltrationPlantDisplay.CATEGORY.getIdentifier(), FiltrationPlantDisplay.SERIALIZER);
