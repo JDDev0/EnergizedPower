d5508897c928fcc6cdcfaf389ae0a104dcf927b0
JDDev0 <jddev0@dorneger.at>
2026-06-24 19:29:46 +0200

    Refactor from SimpleContainer/SimpleFluidStorage to Storage<ItemVariant>/EnergizedPowerFluidStorage (Fluid Pump and Advanced Fluid Pump)

--- src/main/java/me/jddev0/ep/screen/AdvancedFluidPumpMenu.java
+++ src/main/java/me/jddev0/ep/screen/AdvancedFluidPumpMenu.java
@@ -3,7 +3,8 @@ package me.jddev0.ep.screen;
 import me.jddev0.ep.block.EPBlocks;
 import me.jddev0.ep.block.entity.AdvancedFluidPumpBlockEntity;
 import me.jddev0.ep.fluid.FluidStack;
-import me.jddev0.ep.inventory.ConstraintInsertSlot;
+import me.jddev0.ep.inventory.ItemCapabilityMenuHelper;
+import me.jddev0.ep.inventory.ResourceHandlerSlot;
 import me.jddev0.ep.inventory.UpgradeModuleSlot;
 import me.jddev0.ep.inventory.data.*;
 import me.jddev0.ep.inventory.upgrade.UpgradeModuleInventory;
@@ -14,14 +15,11 @@ import me.jddev0.ep.screen.base.IConfigurableMenu;
 import me.jddev0.ep.screen.base.IEnergyStorageConsumerIndicatorBarMenu;
 import me.jddev0.ep.screen.base.UpgradableEnergyStorageMenu;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class AdvancedFluidPumpMenu extends UpgradableEnergyStorageMenu<AdvancedFluidPumpBlockEntity>
@@ -39,16 +37,7 @@ public class AdvancedFluidPumpMenu extends UpgradableEnergyStorageMenu<AdvancedF
     private final SimpleComparatorModeValueContainerData comparatorModeData = new SimpleComparatorModeValueContainerData();
 
     public AdvancedFluidPumpMenu(int id, Inventory inv, BlockPos pos) {
-        this(id, inv.player.level().getBlockEntity(pos), inv, new SimpleContainer(1) {
-            @Override
-            public boolean canPlaceItem(int slot, ItemStack stack) {
-                if(slot == 0) {
-                    return stack.is(Items.COBBLESTONE);
-                }
-
-                return super.canPlaceItem(slot, stack);
-            }
-        }, new UpgradeModuleInventory(
+        this(id, inv, inv.player.level().getBlockEntity(pos), new UpgradeModuleInventory(
                 UpgradeModuleModifier.SPEED,
                 UpgradeModuleModifier.ENERGY_CONSUMPTION,
                 UpgradeModuleModifier.ENERGY_CAPACITY,
@@ -58,25 +47,25 @@ public class AdvancedFluidPumpMenu extends UpgradableEnergyStorageMenu<AdvancedF
         ), null);
     }
 
-    public AdvancedFluidPumpMenu(int id, BlockEntity blockEntity, Inventory playerInventory, Container inv,
-                                 UpgradeModuleInventory upgradeModuleInventory, ContainerData data) {
+    public AdvancedFluidPumpMenu(int id, Inventory inv, BlockEntity blockEntity, UpgradeModuleInventory upgradeModuleInventory,
+                                 ContainerData data) {
         super(
                 EPMenuTypes.ADVANCED_FLUID_PUMP_MENU, id,
 
-                playerInventory, blockEntity,
+                inv, blockEntity,
                 EPBlocks.ADVANCED_FLUID_PUMP,
                 35, 84,
 
                 upgradeModuleInventory, 6
         );
 
-        checkContainerSize(inv, 1);
-
-        addSlot(new ConstraintInsertSlot(inv, 0, 107, 35) {
-            @Override
-            public boolean isActive() {
-                return super.isActive() && !isInUpgradeModuleView();
-            }
+        ItemCapabilityMenuHelper.getEnergizedPowerItemStackHandlerCapability(this.level, this.blockEntity).ifPresent(itemHandler -> {
+            addSlot(new ResourceHandlerSlot(itemHandler, itemHandler::set, 0, 107, 35) {
+                @Override
+                public boolean isActive() {
+                    return super.isActive() && !isInUpgradeModuleView();
+                }
+            });
         });
 
         for(int i = 0;i < upgradeModuleInventory.getContainerSize();i++)
@@ -171,7 +160,7 @@ public class AdvancedFluidPumpMenu extends UpgradableEnergyStorageMenu<AdvancedF
         }
 
         if(sourceItem.getCount() == 0)
-            sourceSlot.setByPlayer(ItemStack.EMPTY);
+            sourceSlot.set(ItemStack.EMPTY);
         else
             sourceSlot.setChanged();
 
