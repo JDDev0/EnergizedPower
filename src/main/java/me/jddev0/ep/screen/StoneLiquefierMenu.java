5bc8d4c97bc7de830e518ba1ad4a33b187ca4831
JDDev0 <jddev0@dorneger.at>
2026-06-24 20:16:20 +0200

    Refactor from SimpleContainer/SimpleFluidStorage to Storage<ItemVariant>/EnergizedPowerFluidStorage (Fluid Transposer and Stone Liquefier)

--- src/main/java/me/jddev0/ep/screen/StoneLiquefierMenu.java
+++ src/main/java/me/jddev0/ep/screen/StoneLiquefierMenu.java
@@ -3,7 +3,8 @@ package me.jddev0.ep.screen;
 import me.jddev0.ep.block.EPBlocks;
 import me.jddev0.ep.block.entity.StoneLiquefierBlockEntity;
 import me.jddev0.ep.fluid.FluidStack;
-import me.jddev0.ep.inventory.ConstraintInsertSlot;
+import me.jddev0.ep.inventory.ItemCapabilityMenuHelper;
+import me.jddev0.ep.inventory.ResourceHandlerSlot;
 import me.jddev0.ep.inventory.UpgradeModuleSlot;
 import me.jddev0.ep.inventory.data.*;
 import me.jddev0.ep.inventory.upgrade.UpgradeModuleInventory;
@@ -13,10 +14,7 @@ import me.jddev0.ep.machine.upgrade.UpgradeModuleModifier;
 import me.jddev0.ep.screen.base.IConfigurableMenu;
 import me.jddev0.ep.screen.base.IEnergyStorageConsumerIndicatorBarMenu;
 import me.jddev0.ep.screen.base.UpgradableEnergyStorageMenu;
-import me.jddev0.ep.util.RecipeUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Container;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerData;
@@ -35,13 +33,7 @@ public class StoneLiquefierMenu extends UpgradableEnergyStorageMenu<StoneLiquefi
     private final SimpleComparatorModeValueContainerData comparatorModeData = new SimpleComparatorModeValueContainerData();
 
     public StoneLiquefierMenu(int id, Inventory inv, BlockPos pos) {
-        this(id, inv.player.level().getBlockEntity(pos), inv, new SimpleContainer(1) {
-            @Override
-            public boolean canPlaceItem(int slot, ItemStack stack) {
-                return slot == 0 && RecipeUtils.isIngredientOfAny(((StoneLiquefierBlockEntity)inv.player.level().
-                            getBlockEntity(pos)).getIngredientsOfRecipes(), stack);
-            }
-        }, new UpgradeModuleInventory(
+        this(id, inv, inv.player.level().getBlockEntity(pos), new UpgradeModuleInventory(
                 UpgradeModuleModifier.SPEED,
                 UpgradeModuleModifier.ENERGY_CONSUMPTION,
                 UpgradeModuleModifier.ENERGY_CAPACITY,
@@ -49,24 +41,24 @@ public class StoneLiquefierMenu extends UpgradableEnergyStorageMenu<StoneLiquefi
         ), null);
     }
 
-    public StoneLiquefierMenu(int id, BlockEntity blockEntity, Inventory playerInventory, Container inv, UpgradeModuleInventory upgradeModuleInventory,
+    public StoneLiquefierMenu(int id, Inventory inv, BlockEntity blockEntity, UpgradeModuleInventory upgradeModuleInventory,
                               ContainerData data) {
         super(
                 EPMenuTypes.STONE_LIQUEFIER_MENU, id,
 
-                playerInventory, blockEntity,
+                inv, blockEntity,
                 EPBlocks.STONE_LIQUEFIER,
 
                 upgradeModuleInventory, 4
         );
 
-        checkContainerSize(inv, 1);
-
-        addSlot(new ConstraintInsertSlot(inv, 0, 80, 35) {
-            @Override
-            public boolean isActive() {
-                return super.isActive() && !isInUpgradeModuleView();
-            }
+        ItemCapabilityMenuHelper.getEnergizedPowerItemStackHandlerCapability(this.level, this.blockEntity).ifPresent(itemHandler -> {
+            addSlot(new ResourceHandlerSlot(itemHandler, itemHandler::set, 0, 80, 35) {
+                @Override
+                public boolean isActive() {
+                    return super.isActive() && !isInUpgradeModuleView();
+                }
+            });
         });
 
         for(int i = 0;i < upgradeModuleInventory.getContainerSize();i++)
@@ -157,7 +149,7 @@ public class StoneLiquefierMenu extends UpgradableEnergyStorageMenu<StoneLiquefi
         }
 
         if(sourceItem.getCount() == 0)
-            sourceSlot.setByPlayer(ItemStack.EMPTY);
+            sourceSlot.set(ItemStack.EMPTY);
         else
             sourceSlot.setChanged();
 
