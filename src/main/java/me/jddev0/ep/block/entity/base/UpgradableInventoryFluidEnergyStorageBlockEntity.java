c3a796a81b4dedb7b9e003ace5b08242df8d509e
JDDev0 <jddev0@dorneger.at>
2026-06-24 19:17:11 +0200

    Refactor from SimpleContainer/SimpleFluidStorage to Storage<ItemVariant>/EnergizedPowerFluidStorage (Fluid Drainer and Fluid Filler)

---
+++ src/main/java/me/jddev0/ep/block/entity/base/UpgradableInventoryFluidEnergyStorageBlockEntity.java
@@ -0,0 +1,64 @@
+package me.jddev0.ep.block.entity.base;
+
+import me.jddev0.ep.energy.IEnergizedPowerEnergyStorage;
+import me.jddev0.ep.fluid.IEnergizedPowerFluidStorage;
+import me.jddev0.ep.inventory.ContainerListener;
+import me.jddev0.ep.inventory.IEnergizedPowerItemStackHandler;
+import me.jddev0.ep.inventory.upgrade.UpgradeModuleInventory;
+import me.jddev0.ep.machine.upgrade.UpgradeModuleModifier;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+
+public abstract class UpgradableInventoryFluidEnergyStorageBlockEntity
+        <E extends IEnergizedPowerEnergyStorage, I extends IEnergizedPowerItemStackHandler, F extends IEnergizedPowerFluidStorage>
+        extends MenuInventoryFluidEnergyStorageBlockEntity<E, I, F> {
+    protected final UpgradeModuleInventory upgradeModuleInventory;
+    protected final ContainerListener updateUpgradeModuleListener = container -> updateUpgradeModules();
+
+    public UpgradableInventoryFluidEnergyStorageBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState,
+                                                            String machineName,
+                                                            long baseEnergyCapacity, long baseEnergyTransferRate,
+                                                            int slotCount,
+                                                            long baseTankCapacity,
+                                                            UpgradeModuleModifier... upgradeModifierSlots) {
+        super(type, blockPos, blockState, machineName, baseEnergyCapacity, baseEnergyTransferRate, slotCount, baseTankCapacity);
+
+        this.upgradeModuleInventory = new UpgradeModuleInventory(upgradeModifierSlots);
+        upgradeModuleInventory.addListener(updateUpgradeModuleListener);
+    }
+
+    @Override
+    protected void saveAdditional(ValueOutput view) {
+        //Save Upgrade Module Inventory first
+        upgradeModuleInventory.saveData(view.child("upgrade_module_inventory"));
+
+        super.saveAdditional(view);
+    }
+
+    @Override
+    protected void loadAdditional(ValueInput view) {
+        //Load Upgrade Module Inventory first
+        upgradeModuleInventory.removeListener(updateUpgradeModuleListener);
+        upgradeModuleInventory.readData(view.childOrEmpty("upgrade_module_inventory"));
+        upgradeModuleInventory.addListener(updateUpgradeModuleListener);
+
+        super.loadAdditional(view);
+    }
+
+    @Override
+    public void preRemoveSideEffects(BlockPos pos, BlockState oldState) {
+        super.preRemoveSideEffects(pos, oldState);
+
+        if(level != null)
+            Containers.dropContents(level, pos, upgradeModuleInventory);
+    }
+
+    protected void updateUpgradeModules() {
+        setChanged();
+        syncEnergyToPlayers(32);
+    }
+}
