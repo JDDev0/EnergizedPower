dd6b2ba1d2b06bca1d0f3bd49c330836ff021a00
JDDev0 <jddev0@dorneger.at>
2026-07-09 11:23:14 +0200

    Add Add XP Extraction Upgrades (8 tiers)

--- src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
+++ src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
@@ -633,6 +633,29 @@ public class ModBookPageContentProvider extends PageContentProvider {
                     EPAPI.id("textures/item/item_pulling_upgrade_module_6.png")
             }, changePageIntToId);
         }
+
+        {
+            Map<Integer, Identifier> changePageIntToId = new HashMap<>();
+            addSimplePage(pageId("xp_extraction_upgrades"), Component.empty().append(
+                    Component.translatable("book.energizedpower.page.xp_extraction_upgrades.1").
+                            withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).append("\n")
+            ).append(
+                    Component.translatable("book.energizedpower.page.xp_extraction_upgrades.2").
+                            withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).append("\n")
+            ).append(
+                    Component.translatable("book.energizedpower.page.xp_extraction_upgrades.3").
+                            withStyle(ChatFormatting.BLACK, ChatFormatting.ITALIC).withStyle(UNIFORM)
+            ), new Identifier[] {
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_1.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_2.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_3.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_4.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_5.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_6.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_7.png"),
+                    EPAPI.id("textures/item/xp_extraction_upgrade_module_8.png")
+            }, changePageIntToId);
+        }
     }
     private void registerToolsChapter() {
         chapterId("tools");
