9eeb8b91847b26a652f19d721387d5bc528e4d4b
JDDev0 <jddev0@dorneger.at>
2026-07-12 13:12:39 +0200

    Fix Energizer progress would not reset for invalid max progress values
    (cherry picked from commit cb776b57c8f96f30bf1eae8f19c0227719a21ef5)

--- src/main/java/me/jddev0/ep/block/entity/EnergizerBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/EnergizerBlockEntity.java
@@ -222,8 +222,7 @@ public class EnergizerBlockEntity
                     level.setBlock(blockPos, state.setValue(EPBlockStateProperties.WORKING, true), 3);
                 }
 
-                if(blockEntity.progress < 0 || blockEntity.maxProgress < 0 || blockEntity.energyConsumptionLeft < 0 ||
-                        energyConsumptionPerTick < 0) {
+                if(blockEntity.progress < 0 || blockEntity.energyConsumptionLeft < 0 || energyConsumptionPerTick < 0) {
                     //Reset progress for invalid values
 
                     blockEntity.resetProgress(blockPos, state);
@@ -251,7 +250,7 @@ public class EnergizerBlockEntity
                 setChanged(level, blockPos, state);
             }
 
-            if(blockEntity.maxProgress == 0) {
+            if(blockEntity.maxProgress <= 0) {
                 blockEntity.maxProgress = Math.max(1, (int)Math.ceil(RECIPE_DURATION /
                         blockEntity.upgradeModuleInventory.getModifierEffectProduct(UpgradeModuleModifier.ENERGIZING_SPEED)));
 
