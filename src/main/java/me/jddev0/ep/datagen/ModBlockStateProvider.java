bb25daf31eee51892b374de88aca3bc04aee7474
JDDev0 <jddev0@dorneger.at>
2026-07-12 11:18:10 +0200

    Make front texture of (Advanced) (Un-)Charger, Fluid Drainer, and Fluid Filler unique

--- src/main/java/me/jddev0/ep/datagen/ModBlockStateProvider.java
+++ src/main/java/me/jddev0/ep/datagen/ModBlockStateProvider.java
@@ -203,7 +203,8 @@ public class ModBlockStateProvider {
 
         activatableOrientableMachineBlockWithItem(EPBlocks.INDUCTION_SMELTER, false);
 
-        horizontalBlockWithItem(EPBlocks.FLUID_FILLER, true);
+        orientableBlockWithItem(EPBlocks.FLUID_FILLER,
+                orientableBlockModel(EPBlocks.FLUID_FILLER, true));
 
         orientableBlockWithItem(EPBlocks.FLUID_FREEZER,
                 orientableBlockModel(EPBlocks.FLUID_FREEZER, false));
@@ -219,7 +220,8 @@ public class ModBlockStateProvider {
 
         horizontalTwoSideBlockWithItem(EPBlocks.FILTRATION_PLANT, false);
 
-        horizontalBlockWithItem(EPBlocks.FLUID_DRAINER, true);
+        orientableBlockWithItem(EPBlocks.FLUID_DRAINER,
+                orientableBlockModel(EPBlocks.FLUID_DRAINER, true));
 
         orientableBlockWithItem(EPBlocks.FLUID_PUMP,
                 orientableBlockModel(EPBlocks.FLUID_PUMP, false));
@@ -228,11 +230,15 @@ public class ModBlockStateProvider {
 
         horizontalBlockWithItem(EPBlocks.DRAIN, true);
 
-        horizontalBlockWithItem(EPBlocks.CHARGER, true);
-        horizontalBlockWithItem(EPBlocks.ADVANCED_CHARGER, true);
+        orientableBlockWithItem(EPBlocks.CHARGER,
+                orientableBlockModel(EPBlocks.CHARGER, true));
+        orientableBlockWithItem(EPBlocks.ADVANCED_CHARGER,
+                orientableBlockModel(EPBlocks.ADVANCED_CHARGER, true));
 
-        horizontalBlockWithItem(EPBlocks.UNCHARGER, true);
-        horizontalBlockWithItem(EPBlocks.ADVANCED_UNCHARGER, true);
+        orientableBlockWithItem(EPBlocks.UNCHARGER,
+                orientableBlockModel(EPBlocks.UNCHARGER, true));
+        orientableBlockWithItem(EPBlocks.ADVANCED_UNCHARGER,
+                orientableBlockModel(EPBlocks.ADVANCED_UNCHARGER, true));
 
         orientableSixDirsBlockWithItem(EPBlocks.MINECART_CHARGER, true);
         orientableSixDirsBlockWithItem(EPBlocks.ADVANCED_MINECART_CHARGER, true);
