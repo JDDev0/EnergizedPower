19ed3f722d074bfc331d86ec397f15db5c6dc4e0
JDDev0 <jddev0@dorneger.at>
2026-07-15 23:01:56 +0200

    Fix flickering in most machines when higher tier Speed upgrades are installed

--- src/main/java/me/jddev0/ep/screen/PulverizerMenu.java
+++ src/main/java/me/jddev0/ep/screen/PulverizerMenu.java
@@ -104,11 +104,11 @@ public class PulverizerMenu extends UpgradableEnergyStorageMenu<PulverizerBlockE
      * @return Same as isCrafting but energy requirements are ignored
      */
     public boolean isCraftingActive() {
-        return progressData.getValue() > 0;
+        return maxProgressData.getValue() > 0;
     }
 
     public boolean isCrafting() {
-        return progressData.getValue() > 0 && hasEnoughEnergyData.getValue();
+        return maxProgressData.getValue() > 0 && hasEnoughEnergyData.getValue();
     }
 
     public int getScaledProgressArrowSize() {
--- src/main/java/me/jddev0/ep/screen/StoneSolidifierMenu.java
+++ src/main/java/me/jddev0/ep/screen/StoneSolidifierMenu.java
@@ -103,11 +103,11 @@ public class StoneSolidifierMenu extends UpgradableEnergyStorageMenu<StoneSolidi
      * @return Same as isCrafting but energy requirements are ignored
      */
     public boolean isCraftingActive() {
-        return progressData.getValue() > 0;
+        return maxProgressData.getValue() > 0;
     }
 
     public boolean isCrafting() {
-        return progressData.getValue() > 0 && hasEnoughEnergyData.getValue();
+        return maxProgressData.getValue() > 0 && hasEnoughEnergyData.getValue();
     }
 
     public int getScaledProgressArrowSize() {
