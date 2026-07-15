d127bbe203fe00a1ea6c0e3420356daa5a9b3355
JDDev0 <jddev0@dorneger.at>
2026-07-15 23:38:40 +0200

    Add IO Configuration support to the (Advanced) Powered Furnace

--- src/main/java/me/jddev0/ep/screen/PoweredFurnaceMenu.java
+++ src/main/java/me/jddev0/ep/screen/PoweredFurnaceMenu.java
@@ -12,9 +12,9 @@ import me.jddev0.ep.item.upgrade.UpgradeModuleItem;
 import me.jddev0.ep.machine.configuration.ComparatorMode;
 import me.jddev0.ep.machine.configuration.RedstoneMode;
 import me.jddev0.ep.machine.upgrade.UpgradeModuleModifier;
+import me.jddev0.ep.screen.base.ConfigurableIOUpgradableEnergyStorageMenu;
 import me.jddev0.ep.screen.base.IConfigurableMenu;
 import me.jddev0.ep.screen.base.IEnergyStorageConsumerIndicatorBarMenu;
-import me.jddev0.ep.screen.base.UpgradableEnergyStorageMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
@@ -23,7 +23,7 @@ import net.minecraft.world.inventory.*;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-public class PoweredFurnaceMenu extends UpgradableEnergyStorageMenu<PoweredFurnaceBlockEntity>
+public class PoweredFurnaceMenu extends ConfigurableIOUpgradableEnergyStorageMenu<PoweredFurnaceBlockEntity>
         implements IEnergyStorageConsumerIndicatorBarMenu, IConfigurableMenu {
     private final SimpleProgressValueContainerData progressData = new SimpleProgressValueContainerData();
     private final SimpleProgressValueContainerData maxProgressData = new SimpleProgressValueContainerData();
