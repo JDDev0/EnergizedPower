79a0c1d579d646ba0f3db5add7eebf061d397d1c
JDDev0 <jddev0@dorneger.at>
2026-07-17 18:18:05 +0200

    Add IO Configuration support to the Advanced Crusher and Fluid Transposer

--- src/main/java/me/jddev0/ep/screen/AdvancedCrusherScreen.java
+++ src/main/java/me/jddev0/ep/screen/AdvancedCrusherScreen.java
@@ -1,7 +1,7 @@
 package me.jddev0.ep.screen;
 
 import me.jddev0.ep.api.EPAPI;
-import me.jddev0.ep.screen.base.ConfigurableUpgradableEnergyStorageContainerScreen;
+import me.jddev0.ep.screen.base.ConfigurableIOUpgradableEnergyStorageContainerScreen;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
@@ -10,7 +10,7 @@ import net.neoforged.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class AdvancedCrusherScreen
-        extends ConfigurableUpgradableEnergyStorageContainerScreen<AdvancedCrusherMenu> {
+        extends ConfigurableIOUpgradableEnergyStorageContainerScreen<AdvancedCrusherMenu> {
     public AdvancedCrusherScreen(AdvancedCrusherMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component,
                 "tooltip.energizedpower.recipe.energy_required_to_finish.txt",
@@ -51,5 +51,15 @@ public class AdvancedCrusherScreen
                 renderFluidMeterContentTooltip(guiGraphics, menu.getFluid(i), menu.getTankCapacity(i), mouseX, mouseY);
             }
         }
+    }
+
+    @Override
+    protected Rect getTankCords(int tank) {
+        if(tank == 0)
+            return new Rect(44, 17, 16, 52);
+        else if(tank == 1)
+            return new Rect(152, 17, 16, 52);
+
+        return super.getTankCords(tank);
     }
 }
--- src/main/java/me/jddev0/ep/screen/FluidTransposerScreen.java
+++ src/main/java/me/jddev0/ep/screen/FluidTransposerScreen.java
@@ -4,7 +4,7 @@ import me.jddev0.ep.api.EPAPI;
 import me.jddev0.ep.block.entity.FluidTransposerBlockEntity;
 import me.jddev0.ep.networking.ModMessages;
 import me.jddev0.ep.networking.packet.SetCheckboxC2SPacket;
-import me.jddev0.ep.screen.base.ConfigurableUpgradableEnergyStorageContainerScreen;
+import me.jddev0.ep.screen.base.ConfigurableIOUpgradableEnergyStorageContainerScreen;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @OnlyIn(Dist.CLIENT)
 public class FluidTransposerScreen
-        extends ConfigurableUpgradableEnergyStorageContainerScreen<FluidTransposerMenu> {
+        extends ConfigurableIOUpgradableEnergyStorageContainerScreen<FluidTransposerMenu> {
     public FluidTransposerScreen(FluidTransposerMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component,
                 "tooltip.energizedpower.recipe.energy_required_to_finish.txt",
@@ -107,5 +107,13 @@ public class FluidTransposerScreen
 
             guiGraphics.renderTooltip(font, components, Optional.empty(), mouseX, mouseY);
         }
+    }
+
+    @Override
+    protected Rect getTankCords(int tank) {
+        if(tank == 0)
+            return new Rect(152, 17, 16, 52);
+
+        return super.getTankCords(tank);
     }
 }
