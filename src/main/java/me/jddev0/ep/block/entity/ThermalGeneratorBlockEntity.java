b82a6d21d2b7de4dc038e99521de2a6e983775ad
JDDev0 <jddev0@dorneger.at>
2026-07-17 21:20:39 +0200

    Change default fluid slot configuration of the Plant Growth Chamber to input only and allow fluid input and output at once for Fluid Freezer, Thermal Generator, and Fluid Filler
    (cherry picked from commit a6d360b819461000ed3b60ea7b39d788ba057674)

--- src/main/java/me/jddev0/ep/block/entity/ThermalGeneratorBlockEntity.java
+++ src/main/java/me/jddev0/ep/block/entity/ThermalGeneratorBlockEntity.java
@@ -178,7 +178,9 @@ public class ThermalGeneratorBlockEntity
                     SlotGroup.of(SlotEntry.ofInput(FLUID_SLOT_INPUT)),
 
                     //Output all tanks (Every EP fluid tank should support output to allow draining fluids without losing them)
-                    SlotGroup.of(SlotEntry.ofOutput(FLUID_SLOT_INPUT))
+                    SlotGroup.of(SlotEntry.ofOutput(FLUID_SLOT_INPUT)),
+                    //Fluid input and output at once should also work, because input tank supports different fluid types
+                    SlotGroup.of(SlotEntry.ofBoth(FLUID_SLOT_INPUT))
             );
         }
 
