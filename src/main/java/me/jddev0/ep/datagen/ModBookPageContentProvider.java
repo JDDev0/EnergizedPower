c49a09c4293012f559029490855b60c56567e0fe
JDDev0 <jddev0@dorneger.at>
2026-07-25 13:47:23 +0200

    Fix solar cells book page was missing

--- src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
+++ src/main/java/me/jddev0/ep/datagen/ModBookPageContentProvider.java
@@ -927,6 +927,18 @@ public class ModBookPageContentProvider extends PageContentProvider {
                 ));
         tableOfContentsEntries.get(energyBlocksTableOfContentPageId).add(Pair.of(energyBlocksEnergyProductionChapterTitle, energyBlocksEnergyProductionChapterPage));
 
+        addSimplePage(pageId("solar_cells"), Component.empty().append(
+                Component.translatable("book.energizedpower.page.solar_cells.1").
+                        withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).append("\n")
+        ).append(
+                Component.translatable("book.energizedpower.page.solar_cells.2").
+                        withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM)
+        ), new ResourceLocation[] {
+                EPAPI.id("textures/item/basic_solar_cell.png"),
+                EPAPI.id("textures/item/advanced_solar_cell.png"),
+                EPAPI.id("textures/item/reinforced_advanced_solar_cell.png"),
+                EPAPI.id("textures/item/elite_solar_cell.png")
+        });
 
         addSimplePage(pageId("solar_panels"), Component.empty().append(
                 Component.translatable("book.energizedpower.page.solar_panels.1").
@@ -942,21 +954,6 @@ public class ModBookPageContentProvider extends PageContentProvider {
                 EPBlocks.SOLAR_PANEL_5,
                 EPBlocks.SOLAR_PANEL_6,
                 EPBlocks.SOLAR_PANEL_7
-        });
-
-        addSimplePage(pageId("solar_panels"), Component.empty().append(
-                Component.translatable("book.energizedpower.page.solar_panels.1").
-                        withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM).append("\n")
-        ).append(
-                Component.translatable("book.energizedpower.page.solar_panels.2").
-                        withStyle(ChatFormatting.BLACK, ChatFormatting.BOLD).withStyle(UNIFORM)
-        ), new Block[] {
-                EPBlocks.SOLAR_PANEL_1,
-                EPBlocks.SOLAR_PANEL_2,
-                EPBlocks.SOLAR_PANEL_3,
-                EPBlocks.SOLAR_PANEL_4,
-                EPBlocks.SOLAR_PANEL_5,
-                EPBlocks.SOLAR_PANEL_6
         });
 
         addSimplePage(pageId("coal_engine"), Component.empty().append(
