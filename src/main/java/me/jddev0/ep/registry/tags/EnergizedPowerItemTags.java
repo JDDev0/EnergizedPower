1d08f74e0d524253aab01dbaf29786a8971b460e
JDDev0 <jddev0@dorneger.at>
2026-07-29 23:31:24 +0200

    Change Metal Press recipes to require an ingredient for the press mold and add press mold sub tags for all press mold types

--- src/main/java/me/jddev0/ep/registry/tags/EnergizedPowerItemTags.java
+++ src/main/java/me/jddev0/ep/registry/tags/EnergizedPowerItemTags.java
@@ -8,7 +8,16 @@ import net.minecraft.world.item.Item;
 public final class EnergizedPowerItemTags {
     private EnergizedPowerItemTags() {}
 
-    public static final TagKey<Item> RAW_METAL_PRESS_MOLDS = TagKey.create(Registries.ITEM, EPAPI.id("metal_press/raw_press_molds"));
+    public static final TagKey<Item> RAW_METAL_PRESS_MOLDS = TagKey.create(Registries.ITEM,
+            EPAPI.id("metal_press/raw_press_molds"));
 
-    public static final TagKey<Item> METAL_PRESS_MOLDS = TagKey.create(Registries.ITEM, EPAPI.id("metal_press/press_molds"));
+    public static final TagKey<Item> METAL_PRESS_MOLDS = TagKey.create(Registries.ITEM,
+            EPAPI.id("metal_press/press_molds"));
+
+    public static final TagKey<Item> METAL_PRESS_MOLDS_GEAR = TagKey.create(Registries.ITEM,
+            EPAPI.id("metal_press/press_molds/gear"));
+    public static final TagKey<Item> METAL_PRESS_MOLDS_ROD = TagKey.create(Registries.ITEM,
+            EPAPI.id("metal_press/press_molds/rod"));
+    public static final TagKey<Item> METAL_PRESS_MOLDS_WIRE = TagKey.create(Registries.ITEM,
+            EPAPI.id("metal_press/press_molds/wire"));
 }
