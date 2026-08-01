bed383d1c44a32ab71e502091a57690001ecb17d
JDDev0 <jddev0@dorneger.at>
2026-08-01 09:42:09 +0200

    Add `AbstractAutoCrafterBlockEntity` base class

--- src/main/java/me/jddev0/ep/block/entity/EPBlockEntities.java
+++ src/main/java/me/jddev0/ep/block/entity/EPBlockEntities.java
@@ -195,8 +195,8 @@ public final class EPBlockEntities {
     public static final BlockEntityType<AutoCrafterBlockEntity> AUTO_CRAFTER_ENTITY = createBlockEntity("auto_crafter",
             EPBlocks.AUTO_CRAFTER, AutoCrafterBlockEntity::new);
 
-    public static final BlockEntityType<LegacyAdvancedAutoCrafterBlockEntity> ADVANCED_AUTO_CRAFTER_ENTITY = createBlockEntity("advanced_auto_crafter",
-            EPBlocks.ADVANCED_AUTO_CRAFTER, LegacyAdvancedAutoCrafterBlockEntity::new);
+    public static final BlockEntityType<AdvancedAutoCrafterBlockEntity> ADVANCED_AUTO_CRAFTER_ENTITY = createBlockEntity("advanced_auto_crafter",
+            EPBlocks.ADVANCED_AUTO_CRAFTER, AdvancedAutoCrafterBlockEntity::new);
 
     public static final BlockEntityType<PressMoldMakerBlockEntity> PRESS_MOLD_MAKER_ENTITY = createBlockEntity("press_mold_maker",
             EPBlocks.PRESS_MOLD_MAKER, PressMoldMakerBlockEntity::new);
@@ -549,9 +549,9 @@ public final class EPBlockEntities {
                 AUTO_CRAFTER_ENTITY, AutoCrafterBlockEntity::getEnergyStorageCapability);
 
         event.registerBlockEntity(Capabilities.Item.BLOCK,
-                ADVANCED_AUTO_CRAFTER_ENTITY, LegacyAdvancedAutoCrafterBlockEntity::getItemHandlerCapability);
+                ADVANCED_AUTO_CRAFTER_ENTITY, AdvancedAutoCrafterBlockEntity::getItemHandlerCapability);
         event.registerBlockEntity(Capabilities.Energy.BLOCK,
-                ADVANCED_AUTO_CRAFTER_ENTITY, LegacyAdvancedAutoCrafterBlockEntity::getEnergyStorageCapability);
+                ADVANCED_AUTO_CRAFTER_ENTITY, AdvancedAutoCrafterBlockEntity::getEnergyStorageCapability);
 
         event.registerBlockEntity(Capabilities.Item.BLOCK,
                 PRESS_MOLD_MAKER_ENTITY, PressMoldMakerBlockEntity::getItemHandlerCapability);
